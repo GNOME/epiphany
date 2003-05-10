e7fd761bd7a2820298025394951e48281867e0cc
Xan Lopez <xan@src.gnome.org>
2003-05-10 17:28:05 +0000

    Alphabetical sort of bookmarks menu elements, fixes #111315. Also create
    Alphabetical sort of bookmarks menu elements, fixes #111315.
    Also create ephy-node-common.h with some general enums.

---
+++ lib/ephy-node-common.h
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (C) 2003 Xan Lopez <xan@masilla.org>
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ */
+
+#ifndef __EPHY_NODE_COMMON_H
+#define __EPHY_NODE_COMMON_H
+
+/* Ids of the root nodes of history,
+ * bookmarks and favicon cache */
+enum
+{
+	BOOKMARKS_NODE_ID = 0,
+	KEYWORDS_NODE_ID = 1,
+	FAVORITES_NODE_ID = 2,
+	BMKS_NOTCATEGORIZED_NODE_ID = 3,
+	STATES_NODE_ID = 4,
+	HOSTS_NODE_ID = 5,
+	PAGES_NODE_ID = 6,
+	ICONS_NODE_ID = 9
+};
+
+typedef enum
+{
+	EPHY_NODE_ALL_PRIORITY,
+	EPHY_NODE_SPECIAL_PRIORITY,
+	EPHY_NODE_NORMAL_PRIORITY
+} EphyNodePriority;
+
+#endif /* EPHY_NODE_COMMON_H */
