45799945c19414094a767dbe3d8dd0815f555014
Marco Pesenti Gritti <marco@gnome.org>
2003-09-16 19:08:27 +0000

    Make sure to remove show_timeout also when the window is still not
    2003-09-16  Marco Pesenti Gritti  <marco@gnome.org>
    
            * lib/widgets/ephy-location-entry.c:
            (ephy_location_entry_autocompletion_hide_alternatives),
            (insert_text_cb),
            (ephy_location_entry_autocompletion_window_hidden_cb):
    
            Make sure to remove show_timeout also when
            the window is still not visible.

--- lib/widgets/ephy-location-entry.c
+++ lib/widgets/ephy-location-entry.c
@@ -44,7 +44,6 @@ struct _EphyLocationEntryPrivate {
 	EphyAutocompletion *autocompletion;
 	EphyAutocompletionWindow *autocompletion_window;
 	gboolean autocompletion_window_visible;
-	gint autocompletion_timeout;
 	gint show_alternatives_timeout;
 	gboolean block_set_autocompletion_key;
 	gboolean going_to_site;
@@ -379,6 +378,14 @@ ephy_location_entry_autocompletion_hide_alternatives (EphyLocationEntry *w)
 		ephy_autocompletion_window_hide (p->autocompletion_window);
 		p->autocompletion_window_visible = FALSE;
 	}
+
+	p->autocompletion_window_visible = FALSE;
+
+	if (p->show_alternatives_timeout)
+	{
+		g_source_remove (p->show_alternatives_timeout);
+		p->show_alternatives_timeout = 0;
+	}
 }
 
 static void
@@ -454,12 +461,6 @@ insert_text_cb (GtkWidget *editable,
 	if (!GTK_WINDOW (window)->has_focus) return;
 
 	if (p->going_to_site) return;
-
-        if (p->autocompletion_timeout != 0)
-	{
-                g_source_remove (p->autocompletion_timeout);
-		p->autocompletion_timeout = 0;
-	}
 
         if (p->show_alternatives_timeout != 0)
 	{
@@ -703,12 +704,6 @@ ephy_location_entry_autocompletion_window_hidden_cb (EphyAutocompletionWindow *a
 	{
 		g_source_remove (p->show_alternatives_timeout);
 		p->show_alternatives_timeout = 0;
-	}
-
-	if (p->autocompletion_timeout)
-	{
-		g_source_remove (p->autocompletion_timeout);
-		p->autocompletion_timeout = 0;
 	}
 }
 
