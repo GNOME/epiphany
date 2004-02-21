b3d1f30aea03119819b34418c900ca5141ba233c
Christian Persch <chpe@src.gnome.org>
2004-02-21 19:59:01 +0000

    Add $ markers

--- embed/mozilla/EventContext.cpp
+++ embed/mozilla/EventContext.cpp
@@ -1,6 +1,7 @@
 /*
  *  Copyright (C) 2000-2004 Marco Pesenti Gritti
  *  Copyright (C) 2003, 2004 Christian Persch
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2, or (at your option)
