3fcff840fcb4b969d6b679c2a9418b717a64eb67
Christian Persch <chpe@cvs.gnome.org>
2004-09-15 17:23:37 +0000

    More fix for mozilla 1.7.3 API-non-change.
    2004-09-15  Christian Persch  <chpe@cvs.gnome.org>
    
    	* embed/mozilla/mozilla-embed.cpp:
    
    	More fix for mozilla 1.7.3 API-non-change.

--- embed/mozilla/mozilla-embed.cpp
+++ embed/mozilla/mozilla-embed.cpp
@@ -503,7 +503,7 @@ static void
 impl_reload (EphyEmbed *embed, 
              gboolean force)
 {
-#if (MOZILLA_IS_BRANCH (1,7) && MOZILLA_CHECK_VERSION3 (1, 7, 3)) || MOZILLA_CHECK_VERSION4 (1, 8, MOZILLA_ALPHA, 3)
+#if (MOZILLA_IS_BRANCH (1,7) && MOZILLA_CHECK_VERSION3 (1, 7, 4)) || MOZILLA_CHECK_VERSION4 (1, 8, MOZILLA_ALPHA, 3)
 	guint32 mflags;
 
 	mflags = GTK_MOZ_EMBED_FLAG_RELOADNORMAL;
@@ -711,7 +711,7 @@ impl_set_encoding (EphyEmbed *embed,
 		if (NS_FAILED (rv)) return;
 	}
 
-#if (MOZILLA_IS_BRANCH (1,7) && MOZILLA_CHECK_VERSION3 (1, 7, 3)) || MOZILLA_CHECK_VERSION4 (1, 8, MOZILLA_ALPHA, 3)
+#if (MOZILLA_IS_BRANCH (1,7) && MOZILLA_CHECK_VERSION3 (1, 7, 4)) || MOZILLA_CHECK_VERSION4 (1, 8, MOZILLA_ALPHA, 3)
 	gtk_moz_embed_reload (GTK_MOZ_EMBED (embed),
 			      GTK_MOZ_EMBED_FLAG_RELOADCHARSETCHANGE);
 #else
