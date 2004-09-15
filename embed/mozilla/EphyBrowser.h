e7cd1f3559f0b0f8ad8d2ccdf3dbd4180b505ae6
Christian Persch <chpe@cvs.gnome.org>
2004-09-15 11:02:06 +0000

    Fix for mozilla 1.7.3 API-non-change.
    2004-09-15  Christian Persch  <chpe@cvs.gnome.org>
    
    	* embed/mozilla/EphyBrowser.cpp:
    	* embed/mozilla/EphyBrowser.h:
    	* embed/mozilla/GlobalHistory.cpp:
    
    	Fix for mozilla 1.7.3 API-non-change.

--- embed/mozilla/EphyBrowser.h
+++ embed/mozilla/EphyBrowser.h
@@ -112,7 +112,7 @@ public:
 	nsresult GetSHUrlAtIndex (PRInt32 index, nsACString &url);
 	nsresult GoToHistoryIndex (PRInt16 index);
 
-#if (!MOZILLA_IS_BRANCH (1, 7) || !MOZILLA_CHECK_VERSION3 (1, 7, 3)) && !MOZILLA_CHECK_VERSION4 (1, 8, MOZILLA_ALPHA, 3)
+#if (!MOZILLA_IS_BRANCH (1, 7) || !MOZILLA_CHECK_VERSION3 (1, 7, 4)) && !MOZILLA_CHECK_VERSION4 (1, 8, MOZILLA_ALPHA, 3)
 	enum ReloadType { RELOAD_NORMAL		 = 0,
 			  RELOAD_FORCE		 = 1,
 			  RELOAD_ENCODING_CHANGE = 2 };
