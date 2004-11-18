715b70c954ac694a0027039cea87bd33e0986679
Christian Persch <chpe@cvs.gnome.org>
2004-11-18 13:52:05 +0000

    Fix opening of content when not downloading automatically. Fixes bug
    2004-11-18  Christian Persch  <chpe@cvs.gnome.org>
    
    	* embed/mozilla/ContentHandler.cpp:
    
    	Fix opening of content when not downloading automatically.
    	Fixes bug #158466.

--- embed/mozilla/ContentHandler.cpp
+++ embed/mozilla/ContentHandler.cpp
@@ -325,7 +325,7 @@ NS_METHOD GContentHandler::MIMEDoAction (void)
 		mAction = CONTENT_ACTION_OPEN_TMP;
 	}
 
-	if (!mHelperApp || mPermission == EPHY_MIME_PERMISSION_UNSAFE)
+	if (!mHelperApp || mPermission != EPHY_MIME_PERMISSION_SAFE)
 	{
 		mAction = CONTENT_ACTION_DOWNLOAD;
 	}
@@ -339,7 +339,8 @@ NS_METHOD GContentHandler::MIMEDoAction (void)
 	mLauncher->GetMIMEInfo(getter_AddRefs(mimeInfo));
 	NS_ENSURE_TRUE (mimeInfo, NS_ERROR_FAILURE);
 
-	if (mAction == CONTENT_ACTION_OPEN)
+	if (mAction == CONTENT_ACTION_OPEN ||
+	    mAction == CONTENT_ACTION_OPEN_TMP)
 	{
 		nsEmbedString desc;
 
