0912cf63ef6b9462f6677a59a1d0c99d14c39e93
Christian Persch <chpe@cvs.gnome.org>
2004-12-12 22:22:19 +0000

    Back out the fix for bug #158466, it's wrong.
    2004-12-12  Christian Persch  <chpe@cvs.gnome.org>
    
    	* embed/mozilla/ContentHandler.cpp:
    
    	Back out the fix for bug #158466, it's wrong.
    
    	* embed/mozilla/EphyBrowser.h:
    
    	Fix compile in !MOZILLA_HAVE_PSM case.

--- embed/mozilla/EphyBrowser.h
+++ embed/mozilla/EphyBrowser.h
@@ -156,11 +156,13 @@ private:
 	nsCOMPtr<nsIDOMDocument> mTargetDocument;
 	nsCOMPtr<nsIDOMEventTarget> mEventTarget;
 	nsCOMPtr<nsIDOMWindow> mDOMWindow;
-	nsCOMPtr<nsISecureBrowserUI> mSecurityInfo;
 	EphyFaviconEventListener *mFaviconEventListener;
 	EphyPopupBlockEventListener *mPopupBlockEventListener;
 	EphyModalAlertEventListener *mModalAlertListener;
 	PRBool mInitialized;
+#ifdef HAVE_MOZILLA_PSM
+        nsCOMPtr<nsISecureBrowserUI> mSecurityInfo;
+#endif
 
 	nsresult GetListener (void);
 	nsresult AttachListeners (void);
