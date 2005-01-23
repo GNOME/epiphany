eb9df565b9b379c28ee7b2b941c7e7b9ad07919e
Christian Persch <chpe@cvs.gnome.org>
2005-01-23 13:34:31 +0000

    Default typeaheadfindsea to on. From bug #157435.
    2005-01-23  Christian Persch  <chpe@cvs.gnome.org>
    
    	* data/default-prefs-toolkit.js:
    
    	Default typeaheadfindsea to on. From bug #157435.

--- data/default-prefs-toolkit.js
+++ data/default-prefs-toolkit.js
@@ -4,3 +4,6 @@ pref("plugin.default_plugin_disabled", true);
 // enable locale matching
 pref("intl.locale.matchOS", true);
 
+// enable fixed-up typeaheadfind extension
+pref("accessibility.typeaheadfindsea", true);
+
