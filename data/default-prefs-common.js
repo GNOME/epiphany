bc7484e902d2d363cd9238b53d62d9e6cbec7f85
Christian Persch <chpe@cvs.gnome.org>
2005-01-24 20:38:11 +0000

    Default security.warn_submit_insecure.show_once to false. Avoids us
    2005-01-24  Christian Persch,,,  <chpe@cvs.gnome.org>
    
    	* data/default-prefs-common.js:
    
    	Default security.warn_submit_insecure.show_once to false.
    	Avoids us displaying the submit-insecure warning, ever.

--- data/default-prefs-common.js
+++ data/default-prefs-common.js
@@ -39,6 +39,7 @@ pref("security.warn_entering_secure", false);
 pref("security.warn_entering_secure.show_once", true);
 pref("security.warn_leaving_secure", false);
 pref("security.warn_submit_insecure", false);
+pref("security.warn_submit_insecure.show_once", false);
 
 // fonts
 pref("font.size.unit", "pt");
