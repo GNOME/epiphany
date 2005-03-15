fda46e4fcc5a21c2f69e74860c4e99b3703243b1
Christian Persch <chpe@cvs.gnome.org>
2005-03-15 23:01:34 +0000

    Default to links only with the fixed-up typeaheadfind too.
    2005-03-15  Christian Persch  <chpe@cvs.gnome.org>
    
    	* data/default-prefs-toolkit.js:
    
    	Default to links only with the fixed-up typeaheadfind too.

--- data/default-prefs-toolkit.js
+++ data/default-prefs-toolkit.js
@@ -7,6 +7,7 @@ pref("intl.locale.matchOS", true);
 // enable fixed-up typeaheadfind extension
 pref("accessibility.typeaheadfindsea", true);
 pref("accessibility.typeaheadfindsea.autostart", true);
+pref("accessibility.typeaheadfindsea.linksonly", true);
 
 // disable image resizing
 pref("browser.enable_automatic_image_resizing", false);
