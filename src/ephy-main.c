fb3620c3bc82a24cd51baaa52e1789036fae1813
Christian Persch <chpe@cvs.gnome.org>
2005-05-11 11:30:37 +0000

    Change string passed to gnome_program_init() as human readable program
    2005-05-11  Christian Persch  <chpe@cvs.gnome.org>
    
    	* src/ephy-main.c: (main):
    
    	Change string passed to gnome_program_init() as human readable
    	program name.

--- src/ephy-main.c
+++ src/ephy-main.c
@@ -223,7 +223,7 @@ main (int argc, char *argv[])
 	program = gnome_program_init (PACKAGE, VERSION,
                                       LIBGNOMEUI_MODULE, argc, argv,
                                       GNOME_PARAM_POPT_TABLE, popt_options,
-                                      GNOME_PARAM_HUMAN_READABLE_NAME, _("Ephy"),
+                                      GNOME_PARAM_HUMAN_READABLE_NAME, _("Epiphany Web Browser"),
 				      GNOME_PARAM_APP_DATADIR, DATADIR,
                                       NULL);
 
