d1122c3e8d957de40cb25965cb2593ea34d74ae1
Christian Persch <chpe@cvs.gnome.org>
2005-07-17 15:50:21 +0000

    Use $^ in the default-prefs.js rule.
    2005-07-17  Christian Persch  <chpe@cvs.gnome.org>
    
    	* data/Makefile.am:
    
    	Use $^ in the default-prefs.js rule.
    
    	* data/default-prefs-gecko-1.8.js:
    
    	All these are already set by default, no need to repeat them.

--- data/default-prefs-gecko-1-8.js
+++
@@ -1,6 +0,0 @@
-// enable error pages
-pref("browser.xul.error_pages.enabled", true);
-pref("browser.xul.error_pages.location", "epiphany:error");
-
-// use fast-back
-//pref("browser.sessionhistory.max_viewers", 3);
