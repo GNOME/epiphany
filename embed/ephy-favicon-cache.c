08477cd1a9ef1958f993bb626339d6a96b85d0b8
Christian Persch <chpe@cvs.gnome.org>
2005-08-13 10:49:03 +0000

    More gcc4 fixes.
    2005-08-13  Christian Persch  <chpe@cvs.gnome.org>
    
    	* embed/ephy-favicon-cache.c: (ephy_favicon_cache_get):
    	* lib/ephy-dnd.c: (ephy_dnd_drag_data_get):
    
    	More gcc4 fixes.

--- embed/ephy-favicon-cache.c
+++ embed/ephy-favicon-cache.c
@@ -749,7 +749,7 @@ ephy_favicon_cache_get (EphyFaviconCache *cache,
 			{
 				if (g_file_get_contents (pix_file, &buf, &count, NULL))
 				{
-					gdk_pixbuf_loader_write (loader, buf, count, NULL);
+					gdk_pixbuf_loader_write (loader, (const guchar *) buf, count, NULL);
 					g_free (buf);
 				}
 				
