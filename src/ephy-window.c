3ed2c5710fa0bbb4e0c22b9a0212a6bbed5f74fc
Christian Persch <chpe@cvs.gnome.org>
2006-01-13 12:15:56 +0000

    Fix zoom buttons order on default toolbar. Set short-label property for
    2006-01-13  Christian Persch  <chpe@cvs.gnome.org>
    
            * data/ui/epiphany-toolbar.xml:
    	* src/ephy-window.c:
    
    	Fix zoom buttons order on default toolbar. Set short-label
    	property for zoom actions.

--- src/ephy-window.c
+++ src/ephy-window.c
@@ -1297,6 +1297,10 @@ setup_ui_manager (EphyWindow *window)
 
 	action = gtk_action_group_get_action (action_group, "ViewEncoding");
 	g_object_set (action, "hide_if_empty", FALSE, NULL);
+	action = gtk_action_group_get_action (action_group, "ViewZoomIn");
+	g_object_set (action, "short-label", _("Larger"), NULL);
+	action = gtk_action_group_get_action (action_group, "ViewZoomOut");
+	g_object_set (action, "short-label", _("Smaller"), NULL);
 
 	action_group = gtk_action_group_new ("PopupsActions");
 	gtk_action_group_set_translation_domain (action_group, NULL);
