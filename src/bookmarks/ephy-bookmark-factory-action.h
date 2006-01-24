871e074093b6780eefed2214a0aad890ab402e1d
Christian Persch <chpe@cvs.gnome.org>
2006-01-24 23:21:31 +0000

    Add cvs version markers.
    2006-01-25  Christian Persch  <chpe@cvs.gnome.org>
    
    	* src/bookmarks/ephy-bookmark-action-group.c:
    	* src/bookmarks/ephy-bookmark-action-group.h:
    	* src/bookmarks/ephy-bookmark-factory-action.c:
    	* src/bookmarks/ephy-bookmark-factory-action.h:
    	* src/bookmarks/ephy-bookmarks-ui.c:
    	* src/bookmarks/ephy-bookmarks-ui.h:
    	* src/bookmarks/ephy-nodes-cover.c:
    	* src/bookmarks/ephy-nodes-cover.h:
    	* src/bookmarks/ephy-open-tabs-action.c:
    	* src/bookmarks/ephy-open-tabs-action.h:
    	* src/bookmarks/ephy-related-action.c:
    	* src/bookmarks/ephy-topic-action-group.c:
    	* src/bookmarks/ephy-topic-action-group.h:
    	* src/bookmarks/ephy-topic-factory-action.c:
    	* src/bookmarks/ephy-topics-palette.c:
    
    	Add cvs version markers.

--- src/bookmarks/ephy-bookmark-factory-action.h
+++ src/bookmarks/ephy-bookmark-factory-action.h
@@ -15,6 +15,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
+ *  $Id$
  */
 
 #ifndef EPHY_BOOKMARK_FACTORY_ACTION_H
