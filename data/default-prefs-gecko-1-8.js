65da02dab6fa525114f1e9449177c86cf6f52c63
Christian Persch <chpe@cvs.gnome.org>
2006-02-22 20:00:05 +0000

    Unset general.useragent.extra.simple pref from xulrunner, we don't want
    2006-02-22  Christian Persch  <chpe@cvs.gnome.org>
    
    	* data/default-prefs-gecko-1-8.js:
    
    	Unset general.useragent.extra.simple pref from xulrunner,
    	we don't want that in our UA string.

--- data/default-prefs-gecko-1-8.js
+++ data/default-prefs-gecko-1-8.js
@@ -1,5 +1,5 @@
 // explicitly enable error pages (xulrunner is missing this pref)
 pref("browser.xul.error_pages.enabled", true);
 
-// explicitly enable error pages (xulrunner is missing this pref)
-pref("browser.xul.error_pages.enabled", true);
+// unset weird xulrunner default UA string
+pref("general.useragent.extra.simple", "");
