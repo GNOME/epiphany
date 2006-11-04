b5c7b53bff9372ff4166e9779c5f89644bd4e521
Christian Persch <chpe@src.gnome.org>
2006-11-04 22:39:01 +0000

    This is entirely my code, not marco's

--- lib/ephy-print-utils.c
+++ lib/ephy-print-utils.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright © 2002 Marco Pesenti Gritti
+ *  Copyright © 2006 Christian Persch
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
