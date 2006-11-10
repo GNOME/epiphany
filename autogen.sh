a56baf39ca79fd012b1e592e80352324219791a5
Wouter Bolsterlee <wbolster@cvs.gnome.org>
2006-11-10 21:39:17 +0000

    Add useful warning when gnome-autogen.sh couldn't be found.
    2006-11-10  Wouter Bolsterlee  <wbolster@cvs.gnome.org>
    
    	* autogen.sh: Add useful warning when gnome-autogen.sh
    	couldn't be found.

--- autogen.sh
+++ autogen.sh
@@ -5,16 +5,19 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="epiphany"
+REQUIRED_AUTOMAKE_VERSION=1.9
 
-(test -f $srcdir/src/ephy-window.c) || {
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/src/ephy-window.c) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
 }
 
 which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
+    echo "You need to install gnome-common from GNOME CVS and make"
+    echo "sure the gnome-autogen.sh script is in your \$PATH."
     exit 1
 }
 
-REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 . gnome-autogen.sh
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
