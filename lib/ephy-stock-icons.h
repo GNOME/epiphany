a468fea36d6cdcd22bb23365c777d2f6968b7f85
Luca Ferretti <elle.uca@libero.it>
2006-12-14 09:21:46 +0000

    Add new "bookmark-view" icon from Andreas Nilsson, replacing
    2006-12-14  Luca Ferretti  <elle.uca@libero.it>
    
    	* data/icons/16x16/actions/Makefile.am:
    	* data/icons/16x16/actions/bookmark-view.png:
    	* data/icons/16x16/actions/bookmark-view.xcf:
    	* data/icons/16x16/actions/download.xcf:
    	* data/icons/16x16/actions/epiphany-download.xcf.bz2:
    	* data/icons/16x16/status/Makefile.am:
    	* data/icons/16x16/status/popup-hidden.xcf:
    	* data/icons/16x16/status/popup-hidden.xcf.bz2:
    	* data/icons/22x22/actions/Makefile.am:
    	* data/icons/22x22/actions/bookmark-view.png:
    	* data/icons/22x22/actions/bookmark-view.xcf:
    	* data/icons/22x22/actions/download.xcf:
    	* data/icons/22x22/actions/epiphany-download.xcf.bz2:
    	* data/icons/22x22/status/Makefile.am:
    	* data/icons/22x22/status/popup-hidden.xcf:
    	* data/icons/22x22/status/popup-hidden.xcf.bz2:
    	* data/icons/24x24/actions/Makefile.am:
    	* data/icons/24x24/actions/README:
    	* data/icons/24x24/actions/bookmark-view.png:
    	* data/icons/24x24/actions/web-bookmarks.png:
    	* data/icons/32x32/actions/Makefile.am:
    	* data/icons/32x32/actions/download.svg:
    	* data/icons/32x32/actions/epiphany-download.svg:
    	* data/icons/scalable/status/Makefile.am:
    
    	Add new "bookmark-view" icon from Andreas Nilsson, replacing
    	"web-bookmarks". Move images source from xcf.bz2 to xcf and
    	use the same name of the named icon, including them in dist.
    
    	* lib/ephy-stock-icons.h:
    
    	Use "bookmark-view" named icon for EPHY_STOCK_BOOKMARKS.

--- lib/ephy-stock-icons.h
+++ lib/ephy-stock-icons.h
@@ -25,7 +25,7 @@ G_BEGIN_DECLS
 
 #define EPHY_STOCK_POPUPS	   "popup-hidden"
 #define EPHY_STOCK_HISTORY         "history-view"
-#define EPHY_STOCK_BOOKMARKS       "web-bookmarks"
+#define EPHY_STOCK_BOOKMARKS       "bookmark-view"
 #define EPHY_STOCK_ENTRY	   "location-entry"
 #define EPHY_STOCK_DOWNLOAD	   "download"
 
