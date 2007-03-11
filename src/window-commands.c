cc36c80482fff1d4d240e359e571bd3cab6591fa
Christian Persch <chpe@gnome.org>
2007-03-11 19:42:02 +0000

    Correct FSF address. Bug #409075, patch by John Millikin.
    2007-03-11  Christian Persch  <chpe@gnome.org>
    
    	* *:
    
    	Correct FSF address. Bug #409075, patch by John Millikin.
    
    svn path=/trunk/; revision=6952

--- src/window-commands.c
+++ src/window-commands.c
@@ -13,7 +13,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  *  $Id$
  */
@@ -738,7 +738,7 @@ window_cmd_help_about (GtkAction *action,
 		   "GNU General Public License for more details."),
 		N_("You should have received a copy of the GNU General Public License "
 		   "along with the GNOME Web Browser; if not, write to the Free Software Foundation, Inc., "
-		   "59 Temple Place, Suite 330, Boston, MA  02111-1307  USA")
+		   "51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA")
 	};
 
 	EphyEmbedShell *shell;
