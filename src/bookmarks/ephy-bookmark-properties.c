7e9557a4ec83055168bebb4244446816e0e88e50
Cosimo Cecchi <cosimoc@src.gnome.org>
2007-09-23 17:59:09 +0000

    Change the hint of the bookmarking properties dialog to DIALOG, instead of NORMAL. Make bookmarking work in fullscreen mode and does not put bookmark properties windows in the taskbar. Fixes bug #478928.
    svn path=/trunk/; revision=7482

--- src/bookmarks/ephy-bookmark-properties.c
+++ src/bookmarks/ephy-bookmark-properties.c
@@ -515,7 +515,7 @@ ephy_bookmark_properties_constructor (GType type,
 	priv = properties->priv;
 
 	gtk_window_set_icon_name (window, EPHY_STOCK_BOOKMARK);
-	gtk_window_set_type_hint (window, GDK_WINDOW_TYPE_HINT_NORMAL);
+	gtk_window_set_type_hint (window, GDK_WINDOW_TYPE_HINT_DIALOG);
 
 	g_signal_connect (properties, "response",
 			  G_CALLBACK (bookmark_properties_response_cb), properties);
