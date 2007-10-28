5eaacd5bd34687ad7dc63f3bddf8b8f140bf2101
Christian Persch <chpe@src.gnome.org>
2007-10-28 14:16:50 +0000

    Can't use an interface type in signal registration
    svn path=/trunk/; revision=7606

--- src/ephy-link.c
+++ src/ephy-link.c
@@ -50,10 +50,10 @@ ephy_link_base_init (gpointer g_class)
 			 G_STRUCT_OFFSET (EphyLinkIface, open_link),
 			 ephy_signal_accumulator_object, ephy_embed_get_type,
 			 ephy_marshal_OBJECT__STRING_OBJECT_FLAGS,
-			 EPHY_TYPE_EMBED,
+			 GTK_TYPE_WIDGET /* Can't use an interface type here */,
 			 3,
 			 G_TYPE_STRING,
-			 EPHY_TYPE_EMBED,
+			 GTK_TYPE_WIDGET /* Can't use an interface type here */,
 			 EPHY_TYPE_LINK_FLAGS);
 
 		initialised = TRUE;
