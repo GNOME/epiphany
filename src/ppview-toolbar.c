21da984c078527c6eb2dce85b308b8685d1123d6
Christian Persch <chpe@src.gnome.org>
2007-10-28 14:07:43 +0000

    Remove EphyTab
    svn path=/trunk/; revision=7602

--- src/ppview-toolbar.c
+++ src/ppview-toolbar.c
@@ -206,7 +206,7 @@ toolbar_update_sensitivity (PPViewToolbar *t)
 	GtkAction *action;
 	GtkActionGroup *action_group = t->priv->action_group;
 
-	embed = ephy_window_get_active_embed (window);
+	embed = ephy_window_get_active_tab (window);
 	g_return_if_fail (embed != NULL);
 
 	pages = ephy_embed_print_preview_n_pages (embed);
@@ -301,7 +301,7 @@ toolbar_cmd_ppv_goto_first (GtkUIManager *merge,
 	EphyWindow *window = t->priv->window;
 	EphyEmbed *embed;
 
-	embed = ephy_window_get_active_embed (window);
+	embed = ephy_window_get_active_tab (window);
 	g_return_if_fail (EPHY_IS_EMBED (embed));
 
 	ephy_embed_print_preview_navigate (embed, EPHY_EMBED_PRINTPREVIEW_HOME, 0);
@@ -318,7 +318,7 @@ toolbar_cmd_ppv_goto_last  (GtkUIManager *merge,
 	EphyWindow *window = t->priv->window;
 	EphyEmbed *embed;
 
-	embed = ephy_window_get_active_embed (window);
+	embed = ephy_window_get_active_tab (window);
 	g_return_if_fail (embed != NULL);
 
 	ephy_embed_print_preview_navigate (embed,
@@ -337,7 +337,7 @@ clamp_page_limits (PPViewToolbar *t, int page)
 	EphyEmbed *embed;
 	int pages;
 
-	embed = ephy_window_get_active_embed (window);
+	embed = ephy_window_get_active_tab (window);
 	g_return_val_if_fail (embed != NULL, -1);
 
 	pages = ephy_embed_print_preview_n_pages (embed);
@@ -352,7 +352,7 @@ toolbar_cmd_ppv_go_back  (GtkUIManager *merge,
 	EphyWindow *window = t->priv->window;
 	EphyEmbed *embed;
 
-	embed = ephy_window_get_active_embed (window);
+	embed = ephy_window_get_active_tab (window);
 	g_return_if_fail (EPHY_IS_EMBED (embed));
 
 	t->priv->current_page = clamp_page_limits (t, t->priv->current_page - 1);
@@ -371,7 +371,7 @@ toolbar_cmd_ppv_go_forward (GtkUIManager *merge,
 	EphyWindow *window = t->priv->window;
 	EphyEmbed *embed;
 
-	embed = ephy_window_get_active_embed (window);
+	embed = ephy_window_get_active_tab (window);
 	g_return_if_fail (EPHY_IS_EMBED (embed));
 
 	t->priv->current_page = clamp_page_limits (t, t->priv->current_page + 1);
@@ -395,7 +395,7 @@ toolbar_cmd_ppv_close (GtkUIManager *merge,
 	window = t->priv->window;
 	g_return_if_fail (EPHY_IS_WINDOW (window));
 
-	embed = ephy_window_get_active_embed (window);
+	embed = ephy_window_get_active_tab (window);
 	g_return_if_fail (EPHY_IS_EMBED (embed));
 
 	ephy_window_set_print_preview (window, FALSE);
