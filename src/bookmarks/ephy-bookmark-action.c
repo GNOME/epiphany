b2087f41f64679f3f300ed5af74aef285327d6b8
Reinout van Schouwen <reinouts@src.gnome.org>
2007-11-22 21:17:26 +0000

    2007-11-22 Reinout van Schouwen <reinouts@gnome.org>
    * 2007-11-22 Reinout van Schouwen <reinouts@gnome.org>
    
    	- bookmarks/ephy-bookmark-action.c: 
              unbreak the build. Use g_strdup_printf.
    
    
    svn path=/trunk/; revision=7725

--- src/bookmarks/ephy-bookmark-action.c
+++ src/bookmarks/ephy-bookmark-action.c
@@ -436,7 +436,7 @@ query_tooltip_cb (GtkWidget *proxy,
 	
 	if (g_str_has_prefix (location, "javascript:"))
 	{
-		text = g_strdup (_("Executes the script “%s”", title));
+		text = g_strdup_printf (_("Executes the script “%s”"), title);
 	}
 	else
 	{
