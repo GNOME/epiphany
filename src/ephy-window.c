e0ede04cf118f740b76adce7bfea8c1e8b5c1e81
Frédéric Péters <fpeters@0d.be>
2009-10-09 20:07:20 +0200

    Pass mailto: URI to gtk_show_uri()
    (Part of) Bug #584537
    
    Signed-off-by: Xan Lopez <xan@gnome.org>

--- src/ephy-window.c
+++ src/ephy-window.c
@@ -2555,10 +2555,18 @@ policy_decision_required_cb (WebKitWebView *web_view,
 	WebKitWebNavigationReason reason;
 	gint button;
 	gint state;
+	const char *uri;
 
 	reason = webkit_web_navigation_action_get_reason (action);
 	button = webkit_web_navigation_action_get_button (action);
 	state = webkit_web_navigation_action_get_modifier_state (action);
+	uri = webkit_network_request_get_uri (request);
+
+	if (g_str_has_prefix (uri, "mailto:")) {
+		webkit_web_policy_decision_ignore (decision);
+		gtk_show_uri (NULL, uri, GDK_CURRENT_TIME, NULL);
+		return TRUE;
+	}
 
 	/* Open in new tab for middle click or ctrl+click */
 	if (reason == WEBKIT_WEB_NAVIGATION_REASON_LINK_CLICKED &&
