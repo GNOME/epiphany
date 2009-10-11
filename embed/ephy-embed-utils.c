437e08e9303dfe186d14b1cd3b2e9226c61feb4e
Chris Lamb <lamby@debian.org>
2009-10-11 16:44:44 +0100

    Execute "javascript:" URIs instead of browsing for them.
    Signed-off-by: Xan Lopez <xan@gnome.org>

--- embed/ephy-embed-utils.c
+++ embed/ephy-embed-utils.c
@@ -91,6 +91,7 @@ ephy_embed_utils_address_has_web_scheme (const char *address)
 			  g_str_has_prefix (address, "file:") ||
 			  g_str_has_prefix (address, "data:") ||
 			  g_str_has_prefix (address, "about:") ||
+			  g_str_has_prefix (address, "javascript:") ||
 			  g_str_has_prefix (address, "gopher:"));
 
 	return has_web_scheme;
