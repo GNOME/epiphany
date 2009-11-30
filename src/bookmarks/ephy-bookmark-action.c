167e60123c1024d539dc02f4a16f7de994bd1c29
Xan Lopez <xan@gnome.org>
2009-11-30 14:28:42 +0200

    Do not use deprecated GtkButton APIs

--- src/bookmarks/ephy-bookmark-action.c
+++ src/bookmarks/ephy-bookmark-action.c
@@ -431,7 +431,7 @@ button_press_cb (GtkWidget *widget,
 {
 	if (event->button == 2)	
 	{
-		gtk_button_pressed (GTK_BUTTON (widget));
+		g_signal_emit_by_name (widget, "button-press-event");
 	}
 
 	return FALSE;
@@ -444,7 +444,7 @@ button_release_cb (GtkWidget *widget,
 {
 	if (event->button == 2)	
 	{
-		gtk_button_released (GTK_BUTTON (widget));
+		g_signal_emit_by_name (widget, "button-release-event");
 	}
 
 	return FALSE;
--- src/bookmarks/ephy-bookmark-properties.c
+++ src/bookmarks/ephy-bookmark-properties.c
@@ -222,7 +222,7 @@ similar_deactivate_cb (GtkMenuShell *ms,
 		       GtkWidget *button)
 {
 	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (button), FALSE);
-	gtk_button_released (GTK_BUTTON (button));
+	g_signal_emit_by_name (button, "button-release-event");
 }
 
 static void
