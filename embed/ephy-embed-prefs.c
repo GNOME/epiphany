dd6a43a539a51b16e434209f13750e456ef0aa8b
Gustavo Noronha Silva <gns@gnome.org>
2010-01-21 18:23:40 -0200

    Enable page cache for real, now.
    All critical bugs have been fixed, we already depend on a new enough
    version of WebKit, and this feature needs testing.

--- embed/ephy-embed-prefs.c
+++ embed/ephy-embed-prefs.c
@@ -396,6 +396,7 @@ ephy_embed_prefs_init (void)
                 "auto-shrink-images", FALSE,
                 "enable-default-context-menu", FALSE,
                 "enable-site-specific-quirks", TRUE,
+                "enable-page-cache", TRUE,
                 NULL);
 
   /* Connections */
