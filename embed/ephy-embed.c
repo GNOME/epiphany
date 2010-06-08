531ae385c42e7fb624ab3969804bbde76b67a71d
Diego Escalante Urrelo <descalante@igalia.com>
2010-06-08 02:55:27 -0500

    ephy-embed: don't create Save As button manually
    Bug #621050

--- embed/ephy-embed.c
+++ embed/ephy-embed.c
@@ -552,7 +552,7 @@ confirm_action_from_mime (WebKitWebView *web_view,
                           DownloadAction action)
 {
   GtkWidget *parent_window;
-  GtkWidget *dialog, *button, *image;
+  GtkWidget *dialog;
   const char *action_label;
   char *mime_description;
   EphyMimePermission mime_permission;
@@ -646,15 +646,8 @@ confirm_action_from_mime (WebKitWebView *web_view,
 
   g_free (mime_description);
 
-  button = gtk_button_new_with_mnemonic (_("_Save As..."));
-  image = gtk_image_new_from_stock (GTK_STOCK_SAVE_AS, GTK_ICON_SIZE_BUTTON);
-  gtk_button_set_image (GTK_BUTTON (button), image);
-  gtk_widget_set_can_default (button, TRUE);
-  /* don't show the image! see bug #307818 */
-  gtk_widget_show (button);
-  gtk_dialog_add_action_widget (GTK_DIALOG (dialog),
-                                button, DOWNLOAD_ACTION_DOWNLOAD);
-
+  gtk_dialog_add_button (GTK_DIALOG (dialog),
+                         GTK_STOCK_SAVE_AS, DOWNLOAD_ACTION_DOWNLOAD);
   gtk_dialog_add_button (GTK_DIALOG (dialog),
                          GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL);
   gtk_dialog_add_button (GTK_DIALOG (dialog),
