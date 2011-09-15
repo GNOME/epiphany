6acf22c4e1e00ece79604561b2f6a1e29f85d994
Claudio Saavedra <csaavedra@igalia.com>
2011-09-16 00:03:05 +0300

    Add translation context for a string
    https://bugzilla.gnome.org/show_bug.cgi?id=658900

--- embed/ephy-request-about.c
+++ embed/ephy-request-about.c
@@ -186,7 +186,9 @@ ephy_request_about_send (SoupRequest          *request,
         img_data_base64 = g_base64_encode ((guchar*)img_data, data_length);
       g_string_append_printf (data_str, "<tbody><tr><td class=\"icon\"><img width=64 height=64 src=\"data:image/png;base64,%s\">" \
                               " </img></td><td class=\"data\"><div class=\"appname\">%s</div><div class=\"appurl\">%s</div></td><td class=\"input\"><input type=\"submit\" value=\"Delete\" id=\"%s\"></td><td class=\"date\">%s <br /> %s</td></tr>",
-                              img_data_base64, app->name, app->url, app->name, _("Installed on:"), app->install_date);
+                              img_data_base64, app->name, app->url, app->name,
+                              /* Note for translators: this refers to the installation date. */
+                              _("Installed on:"), app->install_date);
       g_free (img_data_base64);
       g_free (img_data);
     }
