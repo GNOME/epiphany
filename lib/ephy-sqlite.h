8bce65b5bfd06d29bcc57891303c6e655ee373b8
Xan Lopez <xlopez@igalia.com>
2011-11-29 12:57:41 +0100

    ephy-sqlite: and another forgotten file!

---
+++ lib/ephy-sqlite.h
@@ -0,0 +1,34 @@
+/*
+ *  Copyright © 2011 Igalia S.L.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef EPHY_SQLITE_H
+#define EPHY_SQLITE_H
+
+G_BEGIN_DECLS
+
+typedef enum {
+  EPHY_SQLITE_COLUMN_TYPE_NULL,
+  EPHY_SQLITE_COLUMN_TYPE_INT,
+  EPHY_SQLITE_COLUMN_TYPE_FLOAT,
+  EPHY_SQLITE_COLUMN_TYPE_STRING,
+  EPHY_SQLITE_COLUMN_TYPE_BLOB
+} EphySQLiteColumnType;
+
+G_END_DECLS
+
+#endif /* EPHY_SQLITE_H */
