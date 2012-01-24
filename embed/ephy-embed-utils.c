5efccdb2132bbc33ac4907db2c1cdfc638eeac9f
Xan Lopez <xan@igalia.com>
2012-01-24 13:44:57 +0100

    ephy-embed-utils: consider 'ephy-about' as a web scheme
    Otherwise we'll fail to open about: pages in our policy decision
    mechanism.

--- embed/ephy-embed-utils.c
+++ embed/ephy-embed-utils.c
@@ -97,6 +97,7 @@ ephy_embed_utils_address_has_web_scheme (const char *address)
 			   g_ascii_strncasecmp (address, "javascript", colonpos) &&
 			   g_ascii_strncasecmp (address, "data", colonpos) &&
 			   g_ascii_strncasecmp (address, "about", colonpos) &&
+			   g_ascii_strncasecmp (address, "ephy-about", colonpos) &&
 			   g_ascii_strncasecmp (address, "gopher", colonpos));
 
 	return has_web_scheme;
