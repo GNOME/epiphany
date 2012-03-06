236ee3549644cb97bfac9696e5794a3c5dc2d285
Xan Lopez <xan@igalia.com>
2012-03-06 22:56:14 +0100

    ephy-history-window: and also handle "Ever" now that we have infinite history storage

--- src/ephy-history-window.c
+++ src/ephy-history-window.c
@@ -825,6 +825,8 @@ build_search_box (EphyHistoryWindow *editor)
 	gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (combo), str);
 	g_free (str);
 
+	gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (combo), _("Ever"));
+
 	g_settings_bind (EPHY_SETTINGS_STATE,
 			 EPHY_PREFS_STATE_HISTORY_DATE_FILTER,
 			 combo, "active",
