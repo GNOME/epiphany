9d22581c243c4dbbb124a92331475eb1d12a1e6c
Xan Lopez <xan@igalia.com>
2012-04-11 15:10:42 +0200

    web-apps: s/StartupNotification/StartupNotify/ in desktop files
    That's the correct name for the entry.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=673865

--- embed/ephy-web-app-utils.c
+++ embed/ephy-web-app-utils.c
@@ -211,7 +211,7 @@ create_desktop_file (EphyWebView *view,
                                  webkit_web_view_get_uri (WEBKIT_WEB_VIEW (view)));
   g_key_file_set_value (file, "Desktop Entry", "Exec", exec_string);
   g_free (exec_string);
-  g_key_file_set_value (file, "Desktop Entry", "StartupNotification", "true");
+  g_key_file_set_value (file, "Desktop Entry", "StartupNotify", "true");
   g_key_file_set_value (file, "Desktop Entry", "Terminal", "false");
   g_key_file_set_value (file, "Desktop Entry", "Type", "Application");
 
