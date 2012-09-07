dfb243b5951298d745bb1b670205f8d9a186758d
Xan Lopez <xan@igalia.com>
2012-09-07 23:34:49 +0200

    tests: some simple EphyBookmarks tests

---
+++ tests/ephy-bookmarks-test.c
@@ -0,0 +1,78 @@
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * Copyright © 2012 Igalia S.L.
+ *
+ * Epiphany is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Epiphany is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Epiphany; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+#include "config.h"
+#include "ephy-bookmarks.h"
+
+#include "ephy-debug.h"
+#include "ephy-file-helpers.h"
+
+static void
+test_ephy_bookmarks_create ()
+{
+  EphyBookmarks *bookmarks;
+
+  bookmarks = ephy_bookmarks_new ();
+  g_assert (bookmarks);
+  g_object_unref (bookmarks);
+}
+
+static void
+test_ephy_bookmarks_add ()
+{
+  EphyBookmarks *bookmarks;
+  EphyNode *node, *result;
+
+  bookmarks = ephy_bookmarks_new ();
+  g_assert (bookmarks);
+  
+  node = ephy_bookmarks_add (bookmarks, "GNOME", "http://www.gnome.org");
+  g_assert (node);
+  result = ephy_bookmarks_find_bookmark (bookmarks, "http://www.gnome.org");
+  g_assert (node == result);
+  
+  g_object_unref (bookmarks);
+}
+
+int
+main (int argc, char *argv[])
+{
+  gboolean ret;
+
+  gtk_test_init (&argc, &argv);
+  ephy_debug_init ();
+
+  if (!ephy_file_helpers_init (NULL,
+                               EPHY_FILE_HELPERS_PRIVATE_PROFILE | EPHY_FILE_HELPERS_ENSURE_EXISTS,
+                               NULL)) {
+    g_debug ("Something wrong happened with ephy_file_helpers_init()");
+    return -1;
+  }
+
+  g_test_add_func ("/src/bookmarks/ephy-bookmarks/create",
+                   test_ephy_bookmarks_create);
+
+  g_test_add_func ("/src/bookmarks/ephy-bookmarks/add",
+                   test_ephy_bookmarks_add);
+
+  ret = g_test_run ();
+
+  return ret;
+}
