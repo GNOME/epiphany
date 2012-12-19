4441ac86a00e4e01f96153e4af6e86310eac2394
Xan Lopez <xan@igalia.com>
2012-12-19 13:36:10 +0100

    Revert "ephy-toolbar: set url entry's box style class to .linked"
    This reverts commit 6626beaf15f04824066af937d3fa277e8bd46aa6.
    
    We won't get the theming right in time for the release, so revert for now.

--- src/ephy-toolbar.c
+++ src/ephy-toolbar.c
@@ -149,7 +149,7 @@ ephy_toolbar_constructed (GObject *object)
   gtk_box_pack_start (GTK_BOX (box), location,
                       TRUE, TRUE, 0);
   gtk_style_context_add_class (gtk_widget_get_style_context (box),
-                               "linked");
+                               "location-entry");
 
   /* Reload/Stop */
   tool_button = gtk_button_new ();
