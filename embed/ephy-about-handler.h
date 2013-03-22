739c65404072de313e6cdb39e6b6caa4f276804d
Xan Lopez <xan@igalia.com>
2013-03-22 09:43:03 +0100

    ephy-about-handler: add license, G_{BEGIN,END}_DECLS, etc

--- embed/ephy-about-handler.h
+++ embed/ephy-about-handler.h
@@ -1,6 +1,21 @@
 /* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /*
- * Copyright (C) 2012, Igalia S.L.
+ *  Copyright © 2012 Igalia S.L.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
  */
 
 #ifndef EPHY_ABOUT_HANDLER_H
@@ -8,12 +23,14 @@
 
 #include <glib.h>
 
+G_BEGIN_DECLS
+
 #define EPHY_ABOUT_SCHEME "ephy-about"
 #define EPHY_ABOUT_SCHEME_LEN 10
 
-GString *ephy_about_handler_handle (const char *about);
-
-void _ephy_about_handler_handle_plugins (GString *data_str,
-                                         GList   *plugin_list);
+GString * ephy_about_handler_handle          (const char *about);
+void      _ephy_about_handler_handle_plugins (GString    *data_str,
+                                              GList      *plugin_list);
+G_END_DECLS
 
 #endif /* EPHY_ABOUT_HANDLER_H */
