92a0c59e302f75d46160a1f00bebdd48ebaa5372
Claudio Saavedra <csaavedra@igalia.com>
2013-05-20 18:30:33 +0300

    ephy-web-view: thaw history when load is finished
    Otherwise we end up creating snapshots for error pages.

--- embed/ephy-web-view.c
+++ embed/ephy-web-view.c
@@ -1621,8 +1621,6 @@ load_changed_cb (WebKitWebView *web_view,
       g_free (history_uri);
     }
 
-    ephy_web_view_thaw_history (view);
-
     break;
   }
   case WEBKIT_LOAD_FINISHED:
@@ -1645,6 +1643,8 @@ load_changed_cb (WebKitWebView *web_view,
         g_source_remove (priv->snapshot_idle_id);
       priv->snapshot_idle_id = g_idle_add_full (G_PRIORITY_LOW, (GSourceFunc)web_view_check_snapshot, web_view, NULL);
     }
+
+    ephy_web_view_thaw_history (view);
 
     break;
   }
