bcbc459693e4ea830d6787519dfc92c84ba9acd6
William Jon McCann <william.jon.mccann@gmail.com>
2014-02-06 15:59:37 -0500

    Use header bar on preferences dialog
    https://bugzilla.gnome.org/show_bug.cgi?id=723803

--- src/ephy-shell.c
+++ src/ephy-shell.c
@@ -928,7 +928,9 @@ GObject *
 ephy_shell_get_prefs_dialog (EphyShell *shell)
 {
   if (shell->priv->prefs_dialog == NULL) {
-    shell->priv->prefs_dialog = g_object_new (EPHY_TYPE_PREFS_DIALOG, NULL);
+    shell->priv->prefs_dialog = g_object_new (EPHY_TYPE_PREFS_DIALOG,
+                                              "use-header-bar", TRUE,
+                                              NULL);
     g_signal_connect (shell->priv->prefs_dialog,
                       "destroy",
                       G_CALLBACK (gtk_widget_destroyed),
