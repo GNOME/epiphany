76cd91aa585cf62b3966ec8dfee2c7cdd52e24f1
Carlos Garcia Campos <cgarcia@igalia.com>
2014-10-10 12:49:29 +0200

    window: Simplify the code to open external uris in default browser when in app mode
    Use ephy_embed_utils_urls_have_same_origin() to check if the target URL
    is external or not and ephy_file_open_uri_in_default_browser() to open
    the URL in the default browser if it's external.

--- src/ephy-window.c
+++ src/ephy-window.c
@@ -2183,42 +2183,16 @@ decide_policy_cb (WebKitWebView *web_view,
 	if (navigation_type == WEBKIT_NAVIGATION_TYPE_LINK_CLICKED &&
 	    ephy_embed_shell_get_mode (ephy_embed_shell_get_default ()) == EPHY_EMBED_SHELL_MODE_APPLICATION)
 	{
-		/* The only thing we allow here is to either navigate
-		 * in the same window and tab to the current domain,
-		 * or launch a new (non app mode) epiphany instance
-		 * for all the other cases. */
-		gboolean return_value;
-		SoupURI *soup_uri = soup_uri_new (uri);
-		SoupURI *current_soup_uri = soup_uri_new (webkit_web_view_get_uri (web_view));
-
-		if (g_str_equal (soup_uri->host, current_soup_uri->host))
+		if (ephy_embed_utils_urls_have_same_origin (uri, webkit_web_view_get_uri (web_view)))
 		{
-			return_value = FALSE;
-		}
-		else
-		{
-			GError *error = NULL;
-			const char *const child_argv[] = {"gvfs-open", uri, NULL};
-
-			return_value = TRUE;
-
-			g_spawn_async (NULL, (char**)child_argv, NULL,
-				       G_SPAWN_SEARCH_PATH, NULL, NULL, NULL,
-				       &error);
-
-			if (error)
-			{
-				g_debug ("Error opening %s: %s", uri, error->message);
-				g_error_free (error);
-			}
-
-			webkit_policy_decision_ignore (decision);
+			return FALSE;
 		}
 
-		soup_uri_free (soup_uri);
-		soup_uri_free (current_soup_uri);
+		ephy_file_open_uri_in_default_browser (uri, GDK_CURRENT_TIME,
+						       gtk_window_get_screen (GTK_WINDOW (window)));
+		webkit_policy_decision_ignore (decision);
 
-		return return_value;
+		return TRUE;
 	}
 
 	if (navigation_type == WEBKIT_NAVIGATION_TYPE_LINK_CLICKED)
