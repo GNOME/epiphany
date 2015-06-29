d483ea45db53b4326434a8d46eb0f88069a9d20b
Michael Catanzaro <mcatanzaro@igalia.com>
2015-06-29 16:19:19 -0500

    Stop using gnome-autogen
    It's deprecated.
    
    This incidentally fixes the following warning:
    
    ***Warning*** PKG_NAME is deprecated, you may remove it from autogen.sh
    
    https://wiki.gnome.org/Projects/GnomeCommon/Migration

--- autogen.sh
+++ autogen.sh
@@ -1,24 +1,36 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
-
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="epiphany"
-
-(test -f $srcdir/src/ephy-window.c) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
+(test -f $srcdir/configure.ac) || {
+        echo "**Error**: Directory "\`$srcdir\'" does not look like the top-level project directory"
+        exit 1
 }
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from GNOME git and make"
-    echo "sure the gnome-autogen.sh script is in your \$PATH."
-    exit 1
-}
+PKG_NAME=`autoconf --trace 'AC_INIT:$1' "$srcdir/configure.ac"`
 
-REQUIRED_AUTOMAKE_VERSION=1.11
-REQUIRED_LIBTOOL_VERSION=2.2
-REQUIRED_INTLTOOL_VERSION=0.40.4
-. gnome-autogen.sh
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+        echo "**Warning**: I am going to run \`configure' with no arguments." >&2
+        echo "If you wish to pass any to it, please specify them on the" >&2
+        echo \`$0\'" command line." >&2
+        echo "" >&2
+fi
+
+set -x
+aclocal --install || exit 1
+intltoolize --force --copy --automake || exit 1
+autoreconf --verbose --force --install -Wno-portability || exit 1
+{ set +x; } 2>/dev/null
+
+if [ "$NOCONFIGURE" = "" ]; then
+        set -x
+        $srcdir/configure "$@" || exit 1
+        { set +x; } 2>/dev/null
+
+        if [ "$1" = "--help" ]; then exit 0 else
+                echo "Now type \`make\' to compile $PKG_NAME" || exit 1
+        fi
+else
+        echo "Skipping configure process."
+fi
