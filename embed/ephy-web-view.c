6e5d64aeac48493c201f7fec611840510168e8d1
Michael Catanzaro <mcatanzaro@igalia.com>
2015-07-30 09:57:02 -0500

    Reorder options in remember password prompt
    I messed this up replacing the close button with don't save: affirmative
    is always on the right in GNOME. Just like it already is for
    geolocation/notification requests.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=753061

--- embed/ephy-web-view.c
+++ embed/ephy-web-view.c
@@ -490,8 +490,8 @@ ephy_web_view_create_form_auth_save_confirmation_info_bar (EphyWebView *web_view
 
   LOG ("Going to show infobar about %s", webkit_web_view_get_uri (WEBKIT_WEB_VIEW (web_view)));
 
-  info_bar = gtk_info_bar_new_with_buttons (_("_Save"), GTK_RESPONSE_YES,
-                                            _("_Don’t Save"), GTK_RESPONSE_CLOSE,
+  info_bar = gtk_info_bar_new_with_buttons (_("_Don’t Save"), GTK_RESPONSE_CLOSE,
+                                            _("_Save"), GTK_RESPONSE_YES,
                                             NULL);
 
   action_area = gtk_info_bar_get_action_area (GTK_INFO_BAR (info_bar));
