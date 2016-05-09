GNOME/epiphany

commit 0433ac9dafd91664134ae1c300c6841e86a58d91

overview: Remove the overview model from the UI process
Now we query the history and snapshot services directly and the model is
used only in the web process. This fixes a race condition when the
overview page is created before the history query has completed leaving
an empty overview.

https://bugzilla.gnome.org/show_bug.cgi?id=725393

embed/ephy-embed-shell.c
@@ -54,8 +54,9 @@ struct _EphyEmbedShellPrivate
   GtkPageSetup *page_setup;
   GtkPrintSettings *print_settings;
   EphyEmbedShellMode mode;
-  EphyFrecentStore *frecent_store;
   EphyAboutHandler *about_handler;
+  guint update_overview_timeout_id;
+  guint hiding_overview_item;
   GDBusConnection *bus;
   GList *web_extensions;
   guint web_extensions_page_created_signal_id;
@@ -93,10 +94,14 @@ ephy_embed_shell_dispose (GObject *object)
 {
   EphyEmbedShellPrivate *priv = EPHY_EMBED_SHELL (object)->priv;
 
+  if (priv->update_overview_timeout_id > 0) {
+    g_source_remove (priv->update_overview_timeout_id);
+    priv->update_overview_timeout_id = 0;
+  }
+
   g_clear_object (&priv->encodings);
   g_clear_object (&priv->page_setup);
   g_clear_object (&priv->print_settings);
-  g_clear_object (&priv->frecent_store);
   g_clear_object (&priv->global_history_service);
   g_clear_object (&priv->about_handler);
 
@@ -188,7 +193,7 @@ ephy_embed_shell_update_overview_urls (EphyEmbedShell *shell)
 
   query = ephy_history_query_new ();
   query->sort_type = EPHY_HISTORY_SORT_MOST_VISITED;
-  query->limit = ephy_frecent_store_get_history_length (ephy_embed_shell_get_frecent_store (shell));
+  query->limit = EPHY_ABOUT_OVERVIEW_MAX_ITEMS;
   query->ignore_hidden = TRUE;
 
   ephy_history_service_query_urls (shell->priv->global_history_service, query, NULL,
@@ -205,10 +210,33 @@ history_service_urls_visited_cb (EphyHistoryService *history,
 }
 
 static gboolean
-ephy_embed_shell_update_overview (EphyEmbedShell *shell)
+ephy_embed_shell_update_overview_timeout_cb (EphyEmbedShell *shell)
 {
-    ephy_embed_shell_update_overview_urls (shell);
+  shell->priv->update_overview_timeout_id = 0;
+
+  if (shell->priv->hiding_overview_item > 0)
     return FALSE;
+
+  ephy_embed_shell_update_overview_urls (shell);
+
+  return FALSE;
+}
+
+static void
+history_set_url_hidden_cb (EphyHistoryService *service,
+                           gboolean success,
+                           gpointer result_data,
+                           EphyEmbedShell *shell)
+{
+  shell->priv->hiding_overview_item--;
+
+  if (!success)
+    return;
+
+  if (shell->priv->update_overview_timeout_id > 0)
+    return;
+
+  ephy_embed_shell_update_overview_urls (shell);
 }
 
 static void
@@ -220,20 +248,22 @@ web_extension_remove_from_overview (GDBusConnection *connection,
                                     GVariant *parameters,
                                     EphyEmbedShell *shell)
 {
-  GtkTreeIter iter;
-  EphyFrecentStore *store;
   const char *url_to_remove;
 
   g_variant_get (parameters, "(&s)", &url_to_remove);
-  store = ephy_embed_shell_get_frecent_store (shell);
 
-  if (ephy_overview_store_find_url (EPHY_OVERVIEW_STORE(store), url_to_remove, &iter)) {
-	  ephy_frecent_store_set_hidden (store, &iter);
+  shell->priv->hiding_overview_item++;
+  ephy_history_service_set_url_hidden (shell->priv->global_history_service,
+                                       url_to_remove, TRUE, NULL,
+                                       (EphyHistoryJobCallback) history_set_url_hidden_cb,
+                                       shell);
 
-	  /* Wait for the CSS animations to finish before refreshing */
-	  g_timeout_add (OVERVIEW_RELOAD_DELAY, (GSourceFunc) ephy_embed_shell_update_overview, shell);
+  if (shell->priv->update_overview_timeout_id > 0)
+    g_source_remove (shell->priv->update_overview_timeout_id);
 
-  }
+  /* Wait for the CSS animations to finish before refreshing */
+  shell->priv->update_overview_timeout_id =
+    g_timeout_add (OVERVIEW_RELOAD_DELAY, (GSourceFunc) ephy_embed_shell_update_overview_timeout_cb, shell);
 }
 
 static void
@@ -328,13 +358,17 @@ history_service_cleared_cb (EphyHistoryService *service,
   }
 }
 
-static void
-frecent_store_snapshot_saved_cb (EphyOverviewStore *store,
-                                 const char *url,
-                                 const char *path,
-                                 EphyEmbedShell *shell)
+void
+ephy_embed_shell_set_thumbanil_path (EphyEmbedShell *shell,
+                                     const char *url,
+                                     time_t mtime,
+                                     const char *path)
 {
   GList *l;
+
+  ephy_history_service_set_url_thumbnail_time (shell->priv->global_history_service,
+                                               url, mtime,
+                                               NULL, NULL, NULL);
 
   for (l = shell->priv->web_extensions; l; l = g_list_next (l)) {
     EphyWebExtensionProxy *web_extension = (EphyWebExtensionProxy *)l->data;
@@ -376,38 +410,9 @@ ephy_embed_shell_get_global_history_service (EphyEmbedShell *shell)
     g_signal_connect (shell->priv->global_history_service, "cleared",
                       G_CALLBACK (history_service_cleared_cb),
                       shell);
-    g_signal_connect (ephy_embed_shell_get_frecent_store (shell), "snapshot-saved",
-                      G_CALLBACK (frecent_store_snapshot_saved_cb),
-                      shell);
   }
 
   return G_OBJECT (shell->priv->global_history_service);
-}
-
-/**
- * ephy_embed_shell_get_frecent_store:
- * @shell: a #EphyEmbedShell
- *
- * Gets the #EphyFrecentStore in the shell. This can be used
- * by EphyOverview implementors.
- *
- * Returns: (transfer none): a #EphyFrecentStore
- **/
-EphyFrecentStore *
-ephy_embed_shell_get_frecent_store (EphyEmbedShell *shell)
-{
-  g_return_val_if_fail (EPHY_IS_EMBED_SHELL (shell), NULL);
-
-  if (shell->priv->frecent_store == NULL) {
-    shell->priv->frecent_store = ephy_frecent_store_new ();
-    g_object_set (shell->priv->frecent_store,
-                  "history-service",
-                  ephy_embed_shell_get_global_history_service (shell),
-                  "history-length", 10,
-                  NULL);
-  }
-
-  return shell->priv->frecent_store;
 }
 
 /**

lib/ephy-snapshot-service.c
@@ -159,6 +159,68 @@ snapshot_async_data_free (SnapshotAsyncData *data)
   g_slice_free (SnapshotAsyncData, data);
 }
 
+static GdkPixbuf *
+ephy_snapshot_service_prepare_snapshot (cairo_surface_t *surface,
+                                        cairo_surface_t *favicon)
+{
+  GdkPixbuf *snapshot, *scaled;
+  int orig_width, orig_height;
+  float orig_aspect_ratio, dest_aspect_ratio;
+  int x_offset, new_width = 0, new_height;
+
+  orig_width = cairo_image_surface_get_width (surface);
+  orig_height = cairo_image_surface_get_height (surface);
+
+  if (orig_width < EPHY_THUMBNAIL_WIDTH ||
+      orig_height < EPHY_THUMBNAIL_HEIGHT) {
+    snapshot = gdk_pixbuf_get_from_surface (surface,
+                                            0, 0,
+                                            orig_width, orig_height);
+    scaled = gdk_pixbuf_scale_simple (snapshot,
+                                      EPHY_THUMBNAIL_WIDTH,
+                                      EPHY_THUMBNAIL_HEIGHT,
+                                      GDK_INTERP_TILES);
+  } else {
+    orig_aspect_ratio = orig_width / (float)orig_height;
+    dest_aspect_ratio = EPHY_THUMBNAIL_WIDTH / (float)EPHY_THUMBNAIL_HEIGHT;
+
+    if (orig_aspect_ratio > dest_aspect_ratio) {
+      /* Wider than taller, crop the sides. */
+      new_width = orig_height * dest_aspect_ratio;
+      new_height = orig_height;
+      x_offset = (orig_width - new_width) / 2;
+    } else {
+      /* Crop the bottom otherwise. */
+      new_width = orig_width;
+      new_height = orig_width / (float)dest_aspect_ratio;
+      x_offset = 0;
+    }
+
+    snapshot = gdk_pixbuf_get_from_surface (surface, x_offset, 0, new_width, new_height);
+    scaled = gnome_desktop_thumbnail_scale_down_pixbuf (snapshot,
+                                                        EPHY_THUMBNAIL_WIDTH,
+                                                        EPHY_THUMBNAIL_HEIGHT);
+  }
+
+  g_object_unref (snapshot);
+
+  if (favicon) {
+    GdkPixbuf* fav_pixbuf;
+    int favicon_size = 16;
+    int x_offset = 6;
+    int y_offset = gdk_pixbuf_get_height (scaled) - favicon_size - x_offset;
+
+    fav_pixbuf = ephy_pixbuf_get_from_surface_scaled (favicon, favicon_size, favicon_size);
+    gdk_pixbuf_composite (fav_pixbuf, scaled,
+                          x_offset, y_offset, favicon_size, favicon_size,
+                          x_offset, y_offset, 1, 1,
+                          GDK_INTERP_NEAREST, 255);
+    g_object_unref (fav_pixbuf);
+  }
+
+  return scaled;
+}
+
 static void
 snapshot_saved (EphySnapshotService *service,
                 GAsyncResult *result,
@@ -586,67 +648,6 @@ ephy_snapshot_service_save_snapshot_finish (EphySnapshotService *service,
   data->path = NULL;
 
   return retval;
-}
-
-GdkPixbuf *
-ephy_snapshot_service_prepare_snapshot (cairo_surface_t *surface,
-                                        cairo_surface_t *favicon)
-{
-  GdkPixbuf *snapshot, *scaled;
-  int orig_width, orig_height;
-  float orig_aspect_ratio, dest_aspect_ratio;
-  int x_offset, new_width = 0, new_height;
-
-  orig_width = cairo_image_surface_get_width (surface);
-  orig_height = cairo_image_surface_get_height (surface);
-
-  if (orig_width < EPHY_THUMBNAIL_WIDTH ||
-      orig_height < EPHY_THUMBNAIL_HEIGHT) {
-    snapshot = gdk_pixbuf_get_from_surface (surface,
-                                            0, 0,
-                                            orig_width, orig_height);
-    scaled = gdk_pixbuf_scale_simple (snapshot,
-                                      EPHY_THUMBNAIL_WIDTH,
-                                      EPHY_THUMBNAIL_HEIGHT,
-                                      GDK_INTERP_TILES);
-  } else {
-    orig_aspect_ratio = orig_width / (float)orig_height;
-    dest_aspect_ratio = EPHY_THUMBNAIL_WIDTH / (float)EPHY_THUMBNAIL_HEIGHT;
-
-    if (orig_aspect_ratio > dest_aspect_ratio) {
-      /* Wider than taller, crop the sides. */
-      new_width = orig_height * dest_aspect_ratio;
-      new_height = orig_height;
-      x_offset = (orig_width - new_width) / 2;
-    } else {
-      /* Crop the bottom otherwise. */
-      new_width = orig_width;
-      new_height = orig_width / (float)dest_aspect_ratio;
-      x_offset = 0;
-    }
-
-    snapshot = gdk_pixbuf_get_from_surface (surface, x_offset, 0, new_width, new_height);
-    scaled = gnome_desktop_thumbnail_scale_down_pixbuf (snapshot,
-                                                        EPHY_THUMBNAIL_WIDTH,
-                                                        EPHY_THUMBNAIL_HEIGHT);
-  }
-
-  g_object_unref (snapshot);
-
-  if (favicon) {
-    GdkPixbuf* fav_pixbuf;
-    int favicon_size = 16;
-    int x_offset = 6;
-    int y_offset = gdk_pixbuf_get_height (scaled) - favicon_size - x_offset;
-    fav_pixbuf = ephy_pixbuf_get_from_surface_scaled (favicon, favicon_size, favicon_size);
-    gdk_pixbuf_composite (fav_pixbuf, scaled,
-                          x_offset, y_offset, favicon_size, favicon_size,
-                          x_offset, y_offset, 1, 1,
-                          GDK_INTERP_NEAREST, 255);
-    g_object_unref (fav_pixbuf);
-  }
-
-  return scaled;
 }
 
 const char *

embed/ephy-about-handler.c
@@ -24,7 +24,9 @@
 #include "ephy-embed-shell.h"
 #include "ephy-embed-prefs.h"
 #include "ephy-file-helpers.h"
+#include "ephy-history-service.h"
 #include "ephy-smaps.h"
+#include "ephy-snapshot-service.h"
 #include "ephy-web-app-utils.h"
 #include "ephy-embed-private.h"
 
@@ -397,64 +399,78 @@ ephy_about_handler_handle_applications (EphyAboutHandler *handler,
   return TRUE;
 }
 
-static GString *
-ephy_about_handler_generate_overview_html (EphyOverviewStore *store)
+typedef struct {
+  char *url;
+  time_t mtime;
+} GetSnapshotPathAsyncData;
+
+static void
+got_snapshot_path_for_url_cb (EphySnapshotService *service,
+                              GAsyncResult *result,
+                              GetSnapshotPathAsyncData *data)
 {
-  GtkTreeIter iter;
-  GString *data_str;
-  char *row_url, *row_title;
-  char *row_snapshot;
-  char *thumbnail_style;
-  gboolean valid;
+  char *snapshot;
 
-  valid = gtk_tree_model_get_iter_first (GTK_TREE_MODEL (store), &iter);
-  data_str = g_string_new (NULL);
-
-  while (valid) {
-    gtk_tree_model_get (GTK_TREE_MODEL (store), &iter,
-      EPHY_OVERVIEW_STORE_URI, &row_url,
-      EPHY_OVERVIEW_STORE_TITLE, &row_title,
-      EPHY_OVERVIEW_STORE_SNAPSHOT_PATH, &row_snapshot,
-      -1);
-
-    thumbnail_style = row_snapshot ? g_strdup_printf (" style=\"background: url(file://%s) no-repeat;\"", row_snapshot) : NULL;
-    g_free (row_snapshot);
-
-    g_string_append_printf (data_str,
-      "<li>" \
-      "  <a class=\"overview-item\" title=\"%s\" href=\"%s\">" \
-      "    <div class=\"close-button\" onclick=\"removeFromOverview(this.parentNode,event)\" title=\"%s\">&#10006;</div>" \
-      "    <span class=\"thumbnail\"%s></span>" \
-      "    <span class=\"title\">%s</span>" \
-      "  </a>" \
-      "</li>",
-      g_markup_escape_text (row_title,-1), row_url, _("Remove from overview"),
-                            thumbnail_style ? thumbnail_style : "", row_title);
-
-    g_free (row_title);
-    g_free (row_url);
-    g_free (thumbnail_style);
-
-    valid = gtk_tree_model_iter_next (GTK_TREE_MODEL (store), &iter);
+  snapshot = ephy_snapshot_service_get_snapshot_path_for_url_finish (service, result, NULL);
+  if (snapshot) {
+    ephy_embed_shell_set_thumbanil_path (ephy_embed_shell_get_default (), data->url, data->mtime, snapshot);
+    g_free (snapshot);
   }
-  return data_str;
+  g_free (data->url);
+  g_free (data);
 }
 
-
-static gboolean
-ephy_about_handler_handle_html_overview (EphyAboutHandler *handler,
-                                        WebKitURISchemeRequest *request)
+static void
+history_service_query_urls_cb (EphyHistoryService *history,
+                               gboolean success,
+                               GList *urls,
+                               WebKitURISchemeRequest *request)
 {
   GString *data_str;
   GBytes *html_file;
   GString *html = g_string_new ("");
   gsize data_length;
-  EphyOverviewStore *store;
   char *lang;
+  GList *l;
 
-  store = EPHY_OVERVIEW_STORE (ephy_embed_shell_get_frecent_store (ephy_embed_shell_get_default ()));
+  data_str = g_string_new (NULL);
 
-  data_str = ephy_about_handler_generate_overview_html(store);
+  if (success) {
+    EphySnapshotService *snapshot_service = ephy_snapshot_service_get_default ();
+
+    for (l = urls; l; l = g_list_next (l)) {
+      EphyHistoryURL *url = (EphyHistoryURL *)l->data;
+      const char *snapshot;
+      char *thumbnail_style = NULL;
+
+      snapshot = ephy_snapshot_service_lookup_snapshot_path (snapshot_service, url->url);
+      if (!snapshot) {
+        GetSnapshotPathAsyncData *data = g_new (GetSnapshotPathAsyncData, 1);
+
+        data->url = g_strdup (url->url);
+        data->mtime = url->thumbnail_time;
+        ephy_snapshot_service_get_snapshot_path_for_url_async (ephy_snapshot_service_get_default (),
+                                                               url->url, url->thumbnail_time, NULL,
+                                                               (GAsyncReadyCallback)got_snapshot_path_for_url_cb,
+                                                               data);
+      } else {
+        thumbnail_style = g_strdup_printf (" style=\"background: url(file://%s) no-repeat;\"", snapshot);
+      }
+
+      g_string_append_printf (data_str,
+                              "<li>"
+                              "  <a class=\"overview-item\" title=\"%s\" href=\"%s\">"
+                              "    <div class=\"close-button\" onclick=\"removeFromOverview(this.parentNode,event)\" title=\"%s\">&#10006;</div>"
+                              "    <span class=\"thumbnail\"%s></span>"
+                              "    <span class=\"title\">%s</span>"
+                              "  </a>"
+                              "</li>",
+                              g_markup_escape_text (url->title, -1), url->url, _("Remove from overview"),
+                              thumbnail_style ? thumbnail_style : "", url->title);
+      g_free (thumbnail_style);
+    }
+  }
+
   html_file = g_resources_lookup_data (EPHY_PAGE_TEMPLATE_OVERVIEW, 0, NULL);
 
   lang = g_strdup (pango_language_to_string (gtk_get_default_language ()));
@@ -469,10 +485,29 @@ ephy_about_handler_handle_html_overview (EphyAboutHandler *handler,
 
   data_length = html->len;
   ephy_about_handler_finish_request (request, g_string_free (html, FALSE), data_length);
+  g_object_unref (request);
 
   g_string_free (data_str,TRUE);
   g_bytes_unref (html_file);
   g_free (lang);
+}
+
+static gboolean
+ephy_about_handler_handle_html_overview (EphyAboutHandler *handler,
+                                         WebKitURISchemeRequest *request)
+{
+  EphyHistoryService *history;
+  EphyHistoryQuery *query;
+
+  history = EPHY_HISTORY_SERVICE (ephy_embed_shell_get_global_history_service (ephy_embed_shell_get_default ()));
+  query = ephy_history_query_new ();
+  query->sort_type = EPHY_HISTORY_SORT_MOST_VISITED;
+  query->limit = EPHY_ABOUT_OVERVIEW_MAX_ITEMS;
+  query->ignore_hidden = TRUE;
+  ephy_history_service_query_urls (history, query, NULL,
+                                   (EphyHistoryJobCallback)history_service_query_urls_cb,
+                                   g_object_ref (request));
+  ephy_history_query_free (query);
 
   return TRUE;
 }

embed/ephy-about-handler.h
@@ -34,6 +34,7 @@ G_BEGIN_DECLS
 
 #define EPHY_ABOUT_SCHEME "ephy-about"
 #define EPHY_ABOUT_SCHEME_LEN 10
+#define EPHY_ABOUT_OVERVIEW_MAX_ITEMS 10
 
 typedef struct _EphyAboutHandlerClass   EphyAboutHandlerClass;
 typedef struct _EphyAboutHandler        EphyAboutHandler;

embed/ephy-embed-private.h
@@ -23,7 +23,6 @@
 #endif
 
 #include "ephy-embed-shell.h"
-#include "ephy-frecent-store.h"
 #include "ephy-history-types.h"
 #include "ephy-web-view.h"
 
@@ -61,8 +60,6 @@ void                       ephy_web_view_load_homepage            (EphyWebView  
 char *                     ephy_web_view_create_web_application   (EphyWebView               *view,
                                                                    const char                *title,
                                                                    GdkPixbuf                 *icon);
-EphyFrecentStore          *ephy_embed_shell_get_frecent_store      (EphyEmbedShell *shell);
-
 G_END_DECLS
 
 #endif

embed/ephy-embed-shell.h
@@ -88,7 +88,11 @@ gboolean           ephy_embed_shell_launch_handler             (EphyEmbedShell  
                                                                 GFile            *file,
                                                                 const char       *mime_type,
                                                                 guint32           user_time);
-void               ephy_embed_shell_clear_cache                (EphyEmbedShell *shell);
+void               ephy_embed_shell_clear_cache                (EphyEmbedShell   *shell);
+void               ephy_embed_shell_set_thumbanil_path         (EphyEmbedShell   *shell,
+                                                                const char       *url,
+                                                                time_t            mtime,
+                                                                const char       *path);
 
 G_END_DECLS
 

commit 4c4c719dfe363f5a11d28af0e5a0f073127fb7b4

Save the correct thumbnail mtime in the history service
Save the thumbnail mtime when saving thumbnails so it actually reflects
the mtime embedded in the thumbnail, not the time the page was saved in
the history service. This regressed in 0433ac9. It's only noticeable now
due to 9754735, which has resulted in thumbnails regularly disappearing
from the overview.

https://bugzilla.gnome.org/show_bug.cgi?id=763184

embed/ephy-embed-shell.c
@@ -376,10 +376,6 @@ ephy_embed_shell_set_thumbnail_path (EphyEmbedShell *shell,
   EphyEmbedShellPrivate *priv = ephy_embed_shell_get_instance_private (shell);
   GList *l;
 
-  ephy_history_service_set_url_thumbnail_time (priv->global_history_service,
-                                               url, mtime,
-                                               NULL, NULL, NULL);
-
   for (l = priv->web_extensions; l; l = g_list_next (l)) {
     EphyWebExtensionProxy *web_extension = (EphyWebExtensionProxy *)l->data;
     if (GPOINTER_TO_INT (g_object_get_data (G_OBJECT (web_extension), "initialized"))) {
@@ -430,6 +426,17 @@ ephy_embed_shell_get_global_history_service (EphyEmbedShell *shell)
   }
 
   return G_OBJECT (priv->global_history_service);
+}
+
+static void
+snapshot_saved_cb (EphySnapshotService *service,
+                   const char          *url,
+                   gint64               mtime,
+                   EphyEmbedShell      *shell)
+{
+  ephy_history_service_set_url_thumbnail_time (EPHY_HISTORY_SERVICE (ephy_embed_shell_get_global_history_service (shell)),
+                                               url, mtime,
+                                               NULL, NULL, NULL);
 }
 
 /**
@@ -831,6 +838,10 @@ ephy_embed_shell_constructed (GObject *object)
     ephy_embed_shell_create_web_context (shell);
     priv->user_content = webkit_user_content_manager_new ();
   }
+
+  g_signal_connect_object (ephy_snapshot_service_get_default (),
+                           "snapshot-saved", G_CALLBACK (snapshot_saved_cb),
+                           shell, 0);
 }
 
 static void

lib/ephy-snapshot-service.c
@@ -39,6 +39,13 @@ struct _EphySnapshotService {
 
 G_DEFINE_TYPE (EphySnapshotService, ephy_snapshot_service, G_TYPE_OBJECT)
 
+enum {
+  SNAPSHOT_SAVED,
+  LAST_SIGNAL
+};
+
+static guint signals[LAST_SIGNAL];
+
 typedef enum {
   SNAPSHOT_STALE,
   SNAPSHOT_FRESH
@@ -59,6 +66,24 @@ snapshot_path_cached_data_free (SnapshotPathCachedData *data)
 static void
 ephy_snapshot_service_class_init (EphySnapshotServiceClass *klass)
 {
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  /**
+   * EphySnapshotService::snapshot-saved:
+   * @url: the URL the snapshot was saved for
+   * @mtime: the mtime embedded in the snapshot, needed to retrieve it
+   *
+   * The ::snapshot-saved signal is emitted when a new snapshot is saved.
+   **/
+  signals[SNAPSHOT_SAVED] = g_signal_new ("snapshot-saved",
+                                          G_OBJECT_CLASS_TYPE (object_class),
+                                          G_SIGNAL_RUN_LAST,
+                                          0,
+                                          NULL, NULL, NULL,
+                                          G_TYPE_NONE,
+                                          2,
+                                          G_TYPE_STRING | G_SIGNAL_TYPE_STATIC_SCOPE,
+                                          G_TYPE_INT64);
 }
 
 static void
@@ -656,33 +681,58 @@ ephy_snapshot_service_get_snapshot_finish (EphySnapshotService *service,
 }
 
 typedef struct {
+  EphySnapshotService *service;
   GdkPixbuf *snapshot;
   char *url;
   time_t mtime;
+  gint refcount;
 } SaveSnapshotAsyncData;
 
 static SaveSnapshotAsyncData *
-save_snapshot_async_data_new (GdkPixbuf  *snapshot,
-                              const char *url,
-                              time_t      mtime)
+save_snapshot_async_data_new (EphySnapshotService *service,
+                              GdkPixbuf           *snapshot,
+                              const char          *url,
+                              time_t               mtime)
 {
   SaveSnapshotAsyncData *data;
 
   data = g_slice_new0 (SaveSnapshotAsyncData);
+  data->service = g_object_ref (service);
   data->snapshot = g_object_ref (snapshot);
   data->url = g_strdup (url);
   data->mtime = mtime;
+  data->refcount = 1;
 
   return data;
 }
 
-static void
-save_snapshot_async_data_free (SaveSnapshotAsyncData *data)
+static SaveSnapshotAsyncData *
+save_snapshot_async_data_ref (SaveSnapshotAsyncData *data)
 {
-  g_object_unref (data->snapshot);
-  g_free (data->url);
+  g_atomic_int_add (&data->refcount, 1);
+  return data;
+}
 
-  g_slice_free (SaveSnapshotAsyncData, data);
+static void
+save_snapshot_async_data_unref (SaveSnapshotAsyncData *data)
+{
+  if (g_atomic_int_dec_and_test (&data->refcount)) {
+    g_object_unref (data->service);
+    g_object_unref (data->snapshot);
+    g_free (data->url);
+    g_slice_free (SaveSnapshotAsyncData, data);
+  }
+}
+
+static gboolean
+idle_emit_snapshot_saved (gpointer user_data)
+{
+  SaveSnapshotAsyncData *data = (SaveSnapshotAsyncData *)user_data;
+
+  g_signal_emit (data->service, signals[SNAPSHOT_SAVED], 0, data->url, data->mtime);
+
+  save_snapshot_async_data_unref (data);
+  return G_SOURCE_REMOVE;
 }
 
 static void
@@ -697,9 +747,9 @@ save_snapshot_thread (GTask                 *task,
                                                   data->snapshot,
                                                   data->url,
                                                   data->mtime);
+  g_idle_add (idle_emit_snapshot_saved, save_snapshot_async_data_ref (data));
 
   path = gnome_desktop_thumbnail_path_for_uri (data->url, GNOME_DESKTOP_THUMBNAIL_SIZE_LARGE);
-
   cache_snapshot_data_in_idle (service, data->url, path, SNAPSHOT_FRESH);
 
   g_task_return_pointer (task, path, g_free);
@@ -723,8 +773,8 @@ ephy_snapshot_service_save_snapshot_async (EphySnapshotService *service,
   task = g_task_new (service, cancellable, callback, user_data);
   g_task_set_priority (task, G_PRIORITY_LOW);
   g_task_set_task_data (task,
-                        save_snapshot_async_data_new (snapshot, url, mtime),
-                        (GDestroyNotify)save_snapshot_async_data_free);
+                        save_snapshot_async_data_new (service, snapshot, url, mtime),
+                        (GDestroyNotify)save_snapshot_async_data_unref);
   g_task_run_in_thread (task, (GTaskThreadFunc)save_snapshot_thread);
   g_object_unref (task);
 }