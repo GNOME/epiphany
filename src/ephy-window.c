dd337ce0cd44418e5a9c3a2c80679526a1e6dc51
Iulian Radu <iulian.radu67@gmail.com>
2016-07-22 15:29:15 +0300

    Add myself to copyright header

--- src/ephy-window.c
+++ src/ephy-window.c
@@ -3,6 +3,7 @@
  *  Copyright © 2000, 2001, 2002, 2003, 2004 Marco Pesenti Gritti
  *  Copyright © 2003, 2004 Christian Persch
  *  Copyright © 2011 Igalia S.L.
+ *  Copyright © 2016 Iulian-Gabriel Radu
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
