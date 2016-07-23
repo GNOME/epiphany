219ce7a294ee952b549ff563f7f847d929c88d70
Iulian Radu <iulian.radu67@gmail.com>
2016-07-23 23:20:23 +0300

    bookmarks: Add basic bookmark model

---
+++ src/ephy-bookmark.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2016 Iulian-Gabriel Radu <iulian.radu67@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _EPHY_BOOKMARK_H
+#define _EPHY_BOOKMARK_H
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define EPHY_TYPE_BOOKMARK (ephy_bookmark_get_type ())
+
+G_DECLARE_FINAL_TYPE (EphyBookmark, ephy_bookmark, EPHY, BOOKMARK, GObject)
+
+EphyBookmark        *ephy_bookmark_new          (const char *url,
+                                                 const char *title);
+
+G_END_DECLS
+
+#endif /* _EPHY_BOOKMARK_H */
