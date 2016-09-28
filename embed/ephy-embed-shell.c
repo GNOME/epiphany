0bda122235eb4bc86c35817ed5cbf4e7110e98f1
Gustavo Noronha Silva <gns@gnome.org>
2016-09-28 12:25:41 +0200

    Save decision to allow or deny notifications
    The decision is stored using gsettings database. No UI exists currently to
    forget or manage the decisions.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=748339

--- embed/ephy-embed-shell.c
+++ embed/ephy-embed-shell.c
@@ -54,6 +54,7 @@ typedef struct {
   EphyEmbedShellMode mode;
   WebKitUserContentManager *user_content;
   EphyDownloadsManager *downloads_manager;
+  EphyHostsManager *hosts_manager;
   EphyAboutHandler *about_handler;
   guint update_overview_timeout_id;
   guint hiding_overview_item;
@@ -102,6 +103,7 @@ ephy_embed_shell_dispose (GObject *object)
   g_clear_object (&priv->about_handler);
   g_clear_object (&priv->user_content);
   g_clear_object (&priv->downloads_manager);
+  g_clear_object (&priv->hosts_manager);
   g_clear_object (&priv->web_context);
   g_clear_object (&priv->dbus_server);
 
@@ -1233,4 +1235,14 @@ ephy_embed_shell_get_downloads_manager (EphyEmbedShell *shell)
   if (!priv->downloads_manager)
     priv->downloads_manager = EPHY_DOWNLOADS_MANAGER (g_object_new (EPHY_TYPE_DOWNLOADS_MANAGER, NULL));
   return priv->downloads_manager;
+}
+
+EphyHostsManager *
+ephy_embed_shell_get_hosts_manager (EphyEmbedShell *shell)
+{
+  EphyEmbedShellPrivate *priv = ephy_embed_shell_get_instance_private (shell);
+
+  if (!priv->hosts_manager)
+    priv->hosts_manager = ephy_hosts_manager_new ();
+  return priv->hosts_manager;
 }
---
+++ embed/ephy-hosts-manager.h
@@ -0,0 +1,45 @@
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ *  Copyright © 2015 Gustavo Noronha Silva <gns@gnome.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef EPHY_HOSTS_MANAGER_H
+#define EPHY_HOSTS_MANAGER_H
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define EPHY_TYPE_HOSTS_MANAGER (ephy_hosts_manager_get_type ())
+
+G_DECLARE_FINAL_TYPE (EphyHostsManager, ephy_hosts_manager, EPHY, HOSTS_MANAGER, GObject)
+
+typedef enum {
+  EPHY_HOST_PERMISSION_UNDECIDED = -1,
+  EPHY_HOST_PERMISSION_DENY = 0,
+  EPHY_HOST_PERMISSION_ALLOW = 1,
+} EphyHostPermission;
+
+EphyHostsManager*       ephy_hosts_manager_new                                      (void);
+EphyHostPermission      ephy_hosts_manager_get_notifications_permission_for_address (EphyHostsManager    *manager,
+                                                                                     const char          *address);
+void                    ephy_hosts_manager_set_notifications_permission_for_address (EphyHostsManager    *manager,
+                                                                                     const char          *address,
+                                                                                     EphyHostPermission   permission);
+
+G_END_DECLS
+
+#endif /* EPHY_HOSTS_MANAGER_H */
