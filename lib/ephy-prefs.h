b5914109106d3fdc760a690195651957a38d04d7
Michael Catanzaro <mcatanzaro@gnome.org>
2016-10-27 11:06:34 -0500

    Restore enable-plugins setting, hidden and off by default
    For people who really need deprecated NPAPI plugins still.
    
    We have several months to argue if this should be exposed to users.

--- lib/ephy-prefs.h
+++ lib/ephy-prefs.h
@@ -76,6 +76,7 @@ typedef enum
 #define EPHY_PREFS_WEB_MONOSPACE_FONT        "monospace-font"
 #define EPHY_PREFS_WEB_ENABLE_USER_CSS       "enable-user-css"
 #define EPHY_PREFS_WEB_ENABLE_POPUPS         "enable-popups"
+#define EPHY_PREFS_WEB_ENABLE_PLUGINS        "enable-plugins"
 #define EPHY_PREFS_WEB_ENABLE_SPELL_CHECKING "enable-spell-checking"
 #define EPHY_PREFS_WEB_ENABLE_WEBGL          "enable-webgl"
 #define EPHY_PREFS_WEB_ENABLE_WEBAUDIO       "enable-webaudio"
