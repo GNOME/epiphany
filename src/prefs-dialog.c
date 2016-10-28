51930150253889e18f667730137242aa73d45a37
Michael Catanzaro <mcatanzaro@gnome.org>
2016-10-27 20:05:30 -0500

    Revert "Remove support for browser plugins"
    This reverts commit 9e1457f64b44d6847ea6b75a096aec105691c996.
    
    You win again, gravity....

--- src/prefs-dialog.c
+++ src/prefs-dialog.c
@@ -69,6 +69,7 @@ struct _PrefsDialog {
   GtkWidget *restore_session_checkbutton;
   GtkWidget *popups_allow_checkbutton;
   GtkWidget *adblock_allow_checkbutton;
+  GtkWidget *enable_plugins_checkbutton;
 
   /* fonts */
   GtkWidget *use_gnome_fonts_checkbutton;
@@ -521,6 +522,7 @@ prefs_dialog_class_init (PrefsDialogClass *klass)
   gtk_widget_class_bind_template_child (widget_class, PrefsDialog, restore_session_checkbutton);
   gtk_widget_class_bind_template_child (widget_class, PrefsDialog, popups_allow_checkbutton);
   gtk_widget_class_bind_template_child (widget_class, PrefsDialog, adblock_allow_checkbutton);
+  gtk_widget_class_bind_template_child (widget_class, PrefsDialog, enable_plugins_checkbutton);
   gtk_widget_class_bind_template_child (widget_class, PrefsDialog, download_button_hbox);
   gtk_widget_class_bind_template_child (widget_class, PrefsDialog, download_button_label);
 
@@ -1421,6 +1423,11 @@ setup_general_page (PrefsDialog *dialog)
                    dialog->adblock_allow_checkbutton,
                    "active",
                    G_SETTINGS_BIND_INVERT_BOOLEAN);
+  g_settings_bind (web_settings,
+                   EPHY_PREFS_WEB_ENABLE_PLUGINS,
+                   dialog->enable_plugins_checkbutton,
+                   "active",
+                   G_SETTINGS_BIND_DEFAULT);
 
   create_download_path_button (dialog);
   create_search_engine_combo (GTK_COMBO_BOX (dialog->search_engine_combo));
