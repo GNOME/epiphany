GNOME/epiphany

commit 54006df8dba31a5a58686e7d2473d64f3e454683

profile-migrator: Don't migrate bookmarks if gvdb file exists
This makes the profile migrator robust to being run several times, e.g.
when I switch back and forth between system and JHBuild copies of
Epiphany.

I wound up with two copies of all of my bookmarks!

src/profile-migrator/ephy-profile-migrator.c
@@ -570,7 +570,15 @@ migrate_bookmarks (void)
   xmlNodePtr root;
 
   filename = g_build_filename (ephy_dot_dir (),
-                               EPHY_BOOKMARKS_FILE_RDF,
+                               "bookmarks.gvdb",
+                               NULL);
+
+  if (g_file_test (filename, G_FILE_TEST_EXISTS) == TRUE)
+    goto out;
+
+  g_free (filename);
+  filename = g_build_filename (ephy_dot_dir (),
+                               "bookmarks.rdf",
                                NULL);
 
   if (g_file_test (filename, G_FILE_TEST_EXISTS) == FALSE)

lib/ephy-profile-utils.h
@@ -27,8 +27,6 @@ G_BEGIN_DECLS
 #define EPHY_PROFILE_MIGRATION_VERSION 11
 
 #define EPHY_HISTORY_FILE       "ephy-history.db"
-#define EPHY_BOOKMARKS_FILE     "ephy-bookmarks.xml"
-#define EPHY_BOOKMARKS_FILE_RDF "bookmarks.rdf"
 
 int ephy_profile_utils_get_migration_version (void);
 

commit 86fd75de53ba32726e46a3409e0ec2465e6e0181

Fix bookmarks migration
EphyBookmarksManager always creates an empty bookmarks.gvdb on its init
method, and migrate_bookmarks() checks whether bookmarks.gvdb already
exists to avoid migrating the bookmarks twice. Since migrate_bookmarks()
creates the EphyBookmarksManager before checking for bookmarks.gvdb, it
always exists and bookmarks are never migrated. This is a regression
introduced in commit 54006df8dba31a5a58686e7d2473d64f3e454683.
This patch creates the manager after the initial checks, so that we also
avoid creating the manager and all the sync IO involved if we are not
going to use it.

https://bugzilla.gnome.org/show_bug.cgi?id=774785

src/profile-migrator/ephy-profile-migrator.c
@@ -548,7 +548,7 @@ parse_rdf_item (EphyBookmarksManager *manager,
 static void
 migrate_bookmarks (void)
 {
-  EphyBookmarksManager *manager = ephy_bookmarks_manager_new ();
+  EphyBookmarksManager *manager;
   char *filename;
   xmlDocPtr doc;
   xmlNodePtr child;
@@ -558,23 +558,29 @@ migrate_bookmarks (void)
                                EPHY_BOOKMARKS_FILE,
                                NULL);
 
-  if (g_file_test (filename, G_FILE_TEST_EXISTS) == TRUE)
-    goto out;
+  if (g_file_test (filename, G_FILE_TEST_EXISTS)) {
+    g_free (filename);
+    return;
+  }
 
   g_free (filename);
   filename = g_build_filename (ephy_dot_dir (),
                                "bookmarks.rdf",
                                NULL);
 
-  if (g_file_test (filename, G_FILE_TEST_EXISTS) == FALSE)
-    goto out;
-
-  doc = xmlParseFile (filename);
-  if (doc == NULL) {
-    g_warning ("Failed to re-import the bookmarks. All bookmarks lost!\n");
-    goto out;
+  if (!g_file_test (filename, G_FILE_TEST_EXISTS)) {
+    g_free (filename);
+    return;
   }
 
+  doc = xmlParseFile (filename);
+  g_free (filename);
+  if (doc == NULL) {
+    g_warning ("Failed to re-import the bookmarks. All bookmarks lost!\n");
+    return;
+  }
+
+  manager = ephy_bookmarks_manager_new ();
   root = xmlDocGetRootElement (doc);
 
   child = root->children;
@@ -593,8 +599,6 @@ migrate_bookmarks (void)
                                              NULL);
 
   xmlFreeDoc (doc);
-out:
-  g_free (filename);
   g_object_unref (manager);
 }
 