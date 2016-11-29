fe2cc4b7995bba1c6c3cbba5752c48a8660acee3
Michael Catanzaro <mcatanzaro@gnome.org>
2016-11-28 19:43:00 -0600

    location-entry: Remove stale comment

--- lib/widgets/ephy-location-entry.c
+++ lib/widgets/ephy-location-entry.c
@@ -656,7 +656,6 @@ entry_populate_popup_cb (GtkEntry          *entry,
   /* Search for the Paste menu item and insert right after it. */
   children = gtk_container_get_children (GTK_CONTAINER (menu));
   for (item = children, pos = 0; item != NULL; item = item->next, pos++) {
-    /* Translators: Location entry context menu item, must EXACTLY match GtkEntry's translation. */
     if (g_strcmp0 (gtk_menu_item_get_label (item->data), g_dgettext (GTK_GETTEXT_DOMAIN, "_Paste")) == 0) {
       paste_menuitem = item->data;
       break;
