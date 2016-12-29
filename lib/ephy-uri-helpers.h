54470fddc29c5fee60f6264fee52df743a55c15e
Michael Catanzaro <mcatanzaro@gnome.org>
2016-12-29 12:54:43 -0600

    uri-helpers: Add function to convert URI to security origin

--- lib/ephy-uri-helpers.h
+++ lib/ephy-uri-helpers.h
@@ -1,6 +1,7 @@
 /* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /*
  *  Copyright © 2013 Bastien Nocera <hadess@hadess.net>
+ *  Copyright © 2016 Igalia S.L.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -26,6 +27,7 @@ G_BEGIN_DECLS
 char *ephy_remove_tracking_from_uri (const char *uri);
 char *ephy_uri_decode (const char *uri);
 char *ephy_uri_normalize (const char *uri);
+char *ephy_uri_to_security_origin (const char *uri);
 
 G_END_DECLS
 
