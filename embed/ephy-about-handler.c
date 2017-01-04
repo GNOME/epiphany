GNOME/epiphany

commit 39a1c7fb4e866241855b7cbfdfd68a654ef72b94

Move some duplicate code into ephy_history_query_new_for_overview
https://bugzilla.gnome.org/show_bug.cgi?id=775612

embed/ephy-about-handler.c
@@ -40,9 +40,12 @@ struct _EphyAboutHandler {
   EphySMaps *smaps;
 };
 
-#define EPHY_PAGE_TEMPLATE_ABOUT_CSS        "ephy-resource:///org/gnome/epiphany/page-templates/about.css"
-
 G_DEFINE_TYPE (EphyAboutHandler, ephy_about_handler, G_TYPE_OBJECT)
+
+
+#define EPHY_ABOUT_OVERVIEW_MAX_ITEMS 9
+
+#define EPHY_PAGE_TEMPLATE_ABOUT_CSS        "ephy-resource:///org/gnome/epiphany/page-templates/about.css"
 
 static void
 ephy_about_handler_finalize (GObject *object)
@@ -541,6 +544,20 @@ history_service_query_urls_cb (EphyHistoryService     *history,
   g_object_unref (request);
 }
 
+EphyHistoryQuery *
+ephy_history_query_new_for_overview (void)
+{
+  EphyHistoryQuery *query;
+
+  query = ephy_history_query_new ();
+  query->sort_type = EPHY_HISTORY_SORT_MOST_VISITED;
+  query->limit = EPHY_ABOUT_OVERVIEW_MAX_ITEMS;
+  query->ignore_hidden = TRUE;
+  query->ignore_local = TRUE;
+
+  return query;
+}
+
 static gboolean
 ephy_about_handler_handle_html_overview (EphyAboutHandler       *handler,
                                          WebKitURISchemeRequest *request)
@@ -549,11 +566,7 @@ ephy_about_handler_handle_html_overview (EphyAboutHandler       *handler,
   EphyHistoryQuery *query;
 
   history = EPHY_HISTORY_SERVICE (ephy_embed_shell_get_global_history_service (ephy_embed_shell_get_default ()));
-  query = ephy_history_query_new ();
-  query->sort_type = EPHY_HISTORY_SORT_MOST_VISITED;
-  query->limit = EPHY_ABOUT_OVERVIEW_MAX_ITEMS;
-  query->ignore_hidden = TRUE;
-  query->ignore_local = TRUE;
+  query = ephy_history_query_new_for_overview ();
   ephy_history_service_query_urls (history, query, NULL,
                                    (EphyHistoryJobCallback)history_service_query_urls_cb,
                                    g_object_ref (request));

embed/ephy-about-handler.h
@@ -19,6 +19,8 @@
 #ifndef EPHY_ABOUT_HANDLER_H
 #define EPHY_ABOUT_HANDLER_H
 
+#include "ephy-history-service.h"
+
 #include <webkit2/webkit2.h>
 
 G_BEGIN_DECLS
@@ -29,11 +31,13 @@ G_DECLARE_FINAL_TYPE (EphyAboutHandler, ephy_about_handler, EPHY, ABOUT_HANDLER,
 
 #define EPHY_ABOUT_SCHEME "ephy-about"
 #define EPHY_ABOUT_SCHEME_LEN 10
-#define EPHY_ABOUT_OVERVIEW_MAX_ITEMS 10
 
 EphyAboutHandler *ephy_about_handler_new            (void);
 void              ephy_about_handler_handle_request (EphyAboutHandler       *handler,
                                                      WebKitURISchemeRequest *request);
+
+EphyHistoryQuery *ephy_history_query_new_for_overview (void);
+
 G_END_DECLS
 
 #endif /* EPHY_ABOUT_HANDLER_H */

embed/ephy-embed-shell.c
@@ -157,12 +157,7 @@ ephy_embed_shell_update_overview_urls (EphyEmbedShell *shell)
   EphyEmbedShellPrivate *priv = ephy_embed_shell_get_instance_private (shell);
   EphyHistoryQuery *query;
 
-  query = ephy_history_query_new ();
-  query->sort_type = EPHY_HISTORY_SORT_MOST_VISITED;
-  query->limit = EPHY_ABOUT_OVERVIEW_MAX_ITEMS;
-  query->ignore_hidden = TRUE;
-  query->ignore_local = TRUE;
-
+  query = ephy_history_query_new_for_overview ();
   ephy_history_service_query_urls (priv->global_history_service, query, NULL,
                                    (EphyHistoryJobCallback)history_service_query_urls_cb,
                                    shell);

commit b9286461368ccc76cbde8d5510a9c1ac0cd3feac

about-handler: Fix max overview items
This is 9 in master, but should be 10 in gnome-3-22. I broke it by
mistake in 39a1c7fb4e866241855b7cbfdfd68a654ef72b94 which was a
backport.

embed/ephy-about-handler.c
@@ -43,7 +43,7 @@ struct _EphyAboutHandler {
 G_DEFINE_TYPE (EphyAboutHandler, ephy_about_handler, G_TYPE_OBJECT)
 
 
-#define EPHY_ABOUT_OVERVIEW_MAX_ITEMS 9
+#define EPHY_ABOUT_OVERVIEW_MAX_ITEMS 10
 
 #define EPHY_PAGE_TEMPLATE_ABOUT_CSS        "ephy-resource:///org/gnome/epiphany/page-templates/about.css"
 