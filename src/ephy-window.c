0f67243b5bba89ee5fa6f762fcc267354e61a615
Michael Catanzaro <mcatanzaro@gnome.org>
2017-02-03 13:45:01 -0600

    window: Fix missing return value

--- src/ephy-window.c
+++ src/ephy-window.c
@@ -880,6 +880,8 @@ ephy_window_should_view_receive_key_press_event (EphyWindow  *window,
 	if ((event->state & state_mask) == GDK_MOD1_MASK)
 		return keyval != GDK_KEY_Left &&      /* Back */
 		       keyval != GDK_KEY_Right;       /* Forward */
+
+	return TRUE;
 }
 
 static gboolean
