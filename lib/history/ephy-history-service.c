492b0fce565b3517ae65a4484e2fb9413b953426
Michael Catanzaro <mcatanzaro@gnome.org>
2017-02-19 08:57:04 -0600

    history-service: Don't schedule commit after clearing history
    Now that clear all is implemented by deleting the database file, there's
    no longer any need to schedule a commit here.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=778649

--- lib/history/ephy-history-service.c
+++ lib/history/ephy-history-service.c
@@ -1169,7 +1169,6 @@ ephy_history_service_execute_clear (EphyHistoryService *self,
     return FALSE;
 
   ephy_history_service_clear_all (self);
-  ephy_history_service_schedule_commit (self);
 
   return TRUE;
 }
