0e478618fa89fc5e13f781f49c5f74620e51ddcd
Michael Catanzaro <mcatanzaro@gnome.org>
2017-04-06 11:27:09 -0500

    Attempt to fix -Werror build with libxml 2.9.4
    The prototype of this printf function changed.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=780398

--- lib/ephy-langs.c
+++ lib/ephy-langs.c
@@ -225,8 +225,8 @@ load_iso_entries (int      iso,
   if (reader == NULL)
     goto out;
 
-  xmlStrPrintf (iso_entries, sizeof (iso_entries), (const xmlChar *)"iso_%d_entries", iso);
-  xmlStrPrintf (iso_entry, sizeof (iso_entry), (const xmlChar *)"iso_%d_entry", iso);
+  xmlStrPrintf (iso_entries, sizeof (iso_entries), "iso_%d_entries", iso);
+  xmlStrPrintf (iso_entry, sizeof (iso_entry), "iso_%d_entry", iso);
 
   ret = xmlTextReaderRead (reader);
 
