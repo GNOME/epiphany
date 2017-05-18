c1c0d5935a82c3678655004b20f252904a33e435
Michael Catanzaro <mcatanzaro@igalia.com>
2017-05-18 10:16:26 -0500

    web-view: Reword insecure password form warning
    I've received several complaints that this warning is overblown and
    overly-specific, focusing on only one particular threat. Improve it.

--- embed/ephy-web-view.c
+++ embed/ephy-web-view.c
@@ -783,7 +783,7 @@ sensitive_form_focused_cb (EphyEmbedShell *shell,
     return;
 
   /* Translators: Message appears when insecure password form is focused. */
-  label = gtk_label_new (_("Heads-up: this form is not secure. If you type your password, it will be visible to cybercriminals!"));
+  label = gtk_label_new (_("Heads-up: this form is not secure. If you type your password, it will not be kept private."));
   gtk_label_set_line_wrap (GTK_LABEL (label), TRUE);
   gtk_widget_show (label);
 
