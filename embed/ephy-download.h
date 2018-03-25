2cb622453eeb4acccad6bb25038827b7a386fa42
Michael Catanzaro <mcatanzaro@igalia.com>
2018-03-25 16:26:06 -0500

    Do not auto-open adblock filters after download
    Oooops!
    
    https://bugzilla.gnome.org/show_bug.cgi?id=794646

--- embed/ephy-download.h
+++ embed/ephy-download.h
@@ -33,7 +33,8 @@ typedef enum
 {
   EPHY_DOWNLOAD_ACTION_NONE,
   EPHY_DOWNLOAD_ACTION_BROWSE_TO,
-  EPHY_DOWNLOAD_ACTION_OPEN
+  EPHY_DOWNLOAD_ACTION_OPEN,
+  EPHY_DOWNLOAD_ACTION_DO_NOT_AUTO_OPEN
 } EphyDownloadActionType;
 
 EphyDownload *ephy_download_new                   (WebKitDownload *download);
