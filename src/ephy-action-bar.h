b3fdf128f08643aab37448ac46a1f26ccd81d367
Adrien Plazas <kekun.plazas@laposte.net>
2018-04-26 13:34:42 +0200

    Add EphyActionBar
    This will be used to display actions at the bottom of the window when it
    is narrow.

---
+++ src/ephy-action-bar.h
@@ -0,0 +1,42 @@
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ *  Copyright © 2018 Purism SPC
+ *  Copyright © 2018 Adrien Plazas <kekun.plazas@laposte.net>
+ *
+ *  This file is part of Epiphany.
+ *
+ *  Epiphany is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Epiphany is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Epiphany.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+
+#include "ephy-action-bar-end.h"
+#include "ephy-action-bar-start.h"
+#include "ephy-adaptive-mode.h"
+
+G_BEGIN_DECLS
+
+#define EPHY_TYPE_ACTION_BAR (ephy_action_bar_get_type ())
+
+G_DECLARE_FINAL_TYPE (EphyActionBar, ephy_action_bar, EPHY, ACTION_BAR, GtkRevealer);
+
+EphyActionBar      *ephy_action_bar_new                  (void);
+EphyActionBarStart *ephy_action_bar_get_action_bar_start (EphyActionBar *action_bar);
+EphyActionBarEnd   *ephy_action_bar_get_action_bar_end   (EphyActionBar *action_bar);
+void                ephy_action_bar_set_adaptive_mode    (EphyActionBar    *action_bar,
+                                                          EphyAdaptiveMode  adaptive_mode);
+
+G_END_DECLS
