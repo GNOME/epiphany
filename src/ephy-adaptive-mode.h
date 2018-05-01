9058f2d5f1896c537bfe4b75112080afcf46d3f7
Adrien Plazas <kekun.plazas@laposte.net>
2018-05-01 10:31:50 +0200

    Add EphyAdaptiveMode
    This will be used to implement two different adaptive UI modes.

---
+++ src/ephy-adaptive-mode.h
@@ -0,0 +1,34 @@
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ *  Copyright © 2018 Purism SPC
+ *  Copyright © 2018 Adrien Plazas <kekun.plazas@laposte.net>
+ *
+ *  This file is part of Epiphany.
+ *
+ *  Epiphany is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Epiphany is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Epiphany.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+typedef enum
+{
+  EPHY_ADAPTIVE_MODE_NARROW,
+  EPHY_ADAPTIVE_MODE_NORMAL,
+} EphyAdaptiveMode;
+
+G_END_DECLS
