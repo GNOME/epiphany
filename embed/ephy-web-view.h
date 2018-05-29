8976ca034341186a313c371955c6dad40738aec2
Michael Catanzaro <mcatanzaro@igalia.com>
2018-05-28 21:38:57 -0500

    Revert "Track the currently committed address"
    This reverts commit 34216ae096a296e29c5176a704f076434214d1ed.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=796204

--- embed/ephy-web-view.h
+++ embed/ephy-web-view.h
@@ -109,7 +109,6 @@ void                       ephy_web_view_get_security_level       (EphyWebView  
                                                                    GTlsCertificateFlags      *errors);
 void                       ephy_web_view_print                    (EphyWebView               *view);
 const char *               ephy_web_view_get_address              (EphyWebView               *view);
-const char *             ephy_web_view_get_last_committed_address (EphyWebView               *view);
 const char *               ephy_web_view_get_display_address      (EphyWebView               *view);
 void                       ephy_web_view_set_placeholder          (EphyWebView               *view,
                                                                    const char                *uri,
