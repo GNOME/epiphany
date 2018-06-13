GNOME/epiphany

commit e17dc3627218aed60e2fa61486757b55dc804b6e

EphyUriTester: Avoid g_strdup() when a boolean is enough
https://bugzilla.gnome.org/show_bug.cgi?id=754954

embed/web-extension/ephy-uri-tester.c
@@ -372,16 +372,16 @@ ephy_uri_tester_is_matched (EphyUriTester  *tester,
 
   /* Check cached URLs first. */
   if ((value = g_hash_table_lookup (urlcache, req_uri)))
-    return (value[0] != '0') ? TRUE : FALSE;
+    return GPOINTER_TO_INT (value);
 
   /* Matching by pattern is pretty expensive, so do it if needed only. */
   if (ephy_uri_tester_is_matched_by_pattern (tester, req_uri, page_uri, whitelist))
     {
-      g_hash_table_insert (urlcache, g_strdup (req_uri), g_strdup ("1"));
+      g_hash_table_insert (urlcache, g_strdup (req_uri), GINT_TO_POINTER (TRUE));
       return TRUE;
     }
 
-  g_hash_table_insert (urlcache, g_strdup (req_uri), g_strdup ("0"));
+  g_hash_table_insert (urlcache, g_strdup (req_uri), GINT_TO_POINTER (FALSE));
   return FALSE;
 }
 
@@ -746,7 +746,7 @@ ephy_uri_tester_init (EphyUriTester *tester)
                                             (GDestroyNotify)g_free);
   tester->urlcache = g_hash_table_new_full (g_str_hash, g_str_equal,
                                             (GDestroyNotify)g_free,
-                                            (GDestroyNotify)g_free);
+                                            NULL);
 
   tester->whitelisted_pattern = g_hash_table_new_full (g_str_hash, g_str_equal,
                                                        (GDestroyNotify)g_free,
@@ -756,7 +756,7 @@ ephy_uri_tester_init (EphyUriTester *tester)
                                                         (GDestroyNotify)g_free);
   tester->whitelisted_urlcache = g_hash_table_new_full (g_str_hash, g_str_equal,
                                                         (GDestroyNotify)g_free,
-                                                        (GDestroyNotify)g_free);
+                                                        NULL);
 
   tester->blockcss = g_string_new ("z-non-exist");
   tester->blockcssprivate = g_string_new ("");

commit d7fefdec64750f954c4f8d2ebc6eb5a847689f3e

uri-tester: Fix cache lookups when URI is not matched
This regressed in e17dc3627218aed60e2fa61486757b55dc804b6e.
g_hash_table_lookup() cannot distinguish between a missing value and a
NULL value. We are storing a NULL pointer (GINT_TO_POINTER (FALSE)) to
indicate that the URL is not a match, so the end result is that instead
of a cache hit indicating we should return FALSE, we instead get a cache
miss and then have to manually determine that we need to return FALSE.

This should be a performance fix only, it should not affect correctness.

Fixes #37

embed/web-extension/ephy-uri-tester.c
@@ -178,14 +178,14 @@ ephy_uri_tester_is_matched (EphyUriTester *tester,
                             const char    *page_uri,
                             gboolean       whitelist)
 {
-  char *value;
+  gpointer is_matched;
   GHashTable *urlcache = tester->urlcache;
   if (whitelist)
     urlcache = tester->whitelisted_urlcache;
 
   /* Check cached URLs first. */
-  if ((value = g_hash_table_lookup (urlcache, req_uri)))
-    return GPOINTER_TO_INT (value);
+  if (g_hash_table_lookup_extended (urlcache, req_uri, NULL, &is_matched))
+    return GPOINTER_TO_INT (is_matched);
 
   /* Look for a match either by key or by pattern. */
   if (ephy_uri_tester_is_matched_by_key (tester, opts, req_uri, page_uri, whitelist)) {
@@ -199,6 +199,7 @@ ephy_uri_tester_is_matched (EphyUriTester *tester,
     return TRUE;
   }
 
+  /* No match. */
   g_hash_table_insert (urlcache, g_strdup (req_uri), GINT_TO_POINTER (FALSE));
   return FALSE;
 }