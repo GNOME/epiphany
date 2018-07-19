66fb5efa25680f1d24fead2d1cb2f6487102ad7f
Adrian Perez de Castro <aperez@igalia.com>
2018-07-20 00:16:57 +0200

    First pass at improving reader mode styling
    The following fonts are used for reader mode:
    
    - Merriweather (both the serif and sans variants) for text and titles.
    - Adobe Source Pro for monospaced/preformatted text content.
    
    A copy of the fonts is included and used as Web fonts. The CSS rules
    are designed to match locally installed copies first, thus avoiding
    the need for WebKit to fetch them from the bundled resources when
    available in the system. The fonts are distributed under the terms of
    the SIL OFL license.
    
    On addition to the existing styling, a dark variant is provided as
    well. Chosing the colorscheme is achieved by setting the "light" or
    "dark" class in the <body> element. Whether to use the serif or
    sans-serif font variant is chosen similarly using the "sans" and
    "serif" CSS classes.
    
    This adds two settings as well, which for now can be set using external
    tools like dconf Editor or the gsettings command line tools:
    
    - reader-font-style: Chooses between sans-serif and serif font.
    - reader-color-scheme: Chooses between light or dark background.
    
    For the moment the settings are read only when reader mode is activated.

--- embed/ephy-web-view.c
+++ embed/ephy-web-view.c
@@ -3654,6 +3654,23 @@ ephy_web_view_set_visit_type (EphyWebView *view, EphyHistoryPageVisitType visit_
 }
 
 
+static inline const char*
+enum_nick (GType enum_type, int value)
+{
+    GEnumClass *enum_class;
+    const GEnumValue *enum_value;
+    const char *nick = NULL;
+
+    enum_class = g_type_class_ref (enum_type);
+    enum_value = g_enum_get_value (enum_class, value);
+    if (enum_value)
+        nick = enum_value->value_nick;
+
+    g_type_class_unref (enum_class);
+    return nick;
+}
+
+
 /**
  * ephy_web_view_toggle_reader_mode:
  * @view: an #EphyWebView
@@ -3669,6 +3686,8 @@ ephy_web_view_toggle_reader_mode (EphyWebView *view,
   GString *html;
   GBytes *style_css;
   const gchar *title;
+  const gchar *font_style;
+  const gchar *color_scheme;
 
   if (view->reader_active == active)
     return;
@@ -3689,9 +3708,15 @@ ephy_web_view_toggle_reader_mode (EphyWebView *view,
   html = g_string_new ("");
   style_css = g_resources_lookup_data ("/org/gnome/epiphany/reader.css", G_RESOURCE_LOOKUP_FLAGS_NONE, NULL);
   title = webkit_web_view_get_title (web_view);
+  font_style = enum_nick (EPHY_TYPE_PREFS_UI_READER_FONT_STYLE,
+                          g_settings_get_enum (EPHY_SETTINGS_UI,
+                                               EPHY_PREFS_UI_READER_FONT_STYLE));
+  color_scheme = enum_nick (EPHY_TYPE_PREFS_UI_READER_COLOR_SCHEME,
+                            g_settings_get_enum (EPHY_SETTINGS_UI,
+                                                 EPHY_PREFS_UI_READER_COLOR_SCHEME));
   g_string_append_printf (html, "<style>%s</style>"
                                 "<title>%s</title>"
-                                "<body>"
+                                "<body class='%s %s'>"
                                 "<article>"
                                 "<h2>"
                                 "%s"
@@ -3702,6 +3727,8 @@ ephy_web_view_toggle_reader_mode (EphyWebView *view,
                                 "<hr>",
                                 (gchar *)g_bytes_get_data (style_css, NULL),
                                 title,
+                                font_style,
+                                color_scheme,
                                 title,
                                 view->reader_byline != NULL ? view->reader_byline : "");
   g_string_append (html, view->reader_content);
