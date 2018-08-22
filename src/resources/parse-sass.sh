1b199314e87dd917fddf39d7c0435dcd0258d2e0
Alexander Mikhaylenko <exalm7659@gmail.com>
2018-08-22 20:09:27 +0000

    theme: Port parse-sass.sh to sassc
    Let's do the same thing as GTK and gnome-shell.

--- src/resources/parse-sass.sh
+++ src/resources/parse-sass.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+if [ ! "$(which sassc 2> /dev/null)" ]; then
+   echo sassc needs to be installed to generate the css.
+   exit 1
+fi
+
+SASSC_OPT="-M -t compact"
+
 : ${GTK_SOURCE_PATH:="../../../gtk+-3"}
 
-sass --sourcemap=none --update -I ${GTK_SOURCE_PATH}/gtk/theme/Adwaita \
-	"$(dirname "$0")/themes"
+sassc $SASSC_OPT -I${GTK_SOURCE_PATH}/gtk/theme/Adwaita \
+	themes/Adwaita.scss themes/Adwaita.css
+sassc $SASSC_OPT -I${GTK_SOURCE_PATH}/gtk/theme/Adwaita \
+	themes/shared.scss themes/shared.css
