0b8294d313d101a67a25c6316fa2678f4e2cc6b0
Jan-Michael Brummer <jan.brummer@tabos.org>
2018-12-19 11:28:16 +0100

    Drop "Help" entry in Pantheon
    Add check for pantheon desktop function and remove help entry if needed
    
    Fixes: https://gitlab.gnome.org/GNOME/epiphany/issues/71

--- src/ephy-header-bar.c
+++ src/ephy-header-bar.c
@@ -138,6 +138,14 @@ update_revealer_visibility (GtkRevealer *revealer)
                           gtk_revealer_get_child_revealed (revealer));
 }
 
+static gboolean
+is_desktop_pantheon (void)
+{
+  const gchar *xdg_current_desktop = g_environ_getenv (g_get_environ (), "XDG_CURRENT_DESKTOP");
+
+  return strstr (xdg_current_desktop, "Pantheon") != NULL;
+}
+
 static void
 ephy_header_bar_constructed (GObject *object)
 {
@@ -219,10 +227,17 @@ ephy_header_bar_constructed (GObject *object)
     gtk_widget_destroy (GTK_WIDGET (gtk_builder_get_object (builder, "override-text-encoding-button")));
     gtk_widget_destroy (GTK_WIDGET (gtk_builder_get_object (builder, "keyboard-shortcuts-button")));
     gtk_widget_destroy (GTK_WIDGET (gtk_builder_get_object (builder, "help-button")));
-  } else if (ephy_is_running_inside_flatpak ()) {
-    gtk_widget_destroy (GTK_WIDGET (gtk_builder_get_object (builder, "save-as-application-separator")));
-    gtk_widget_destroy (GTK_WIDGET (gtk_builder_get_object (builder, "save-as-application-button")));
+  } else {
+    if (ephy_is_running_inside_flatpak ()) {
+      gtk_widget_destroy (GTK_WIDGET (gtk_builder_get_object (builder, "save-as-application-separator")));
+      gtk_widget_destroy (GTK_WIDGET (gtk_builder_get_object (builder, "save-as-application-button")));
+    }
+
+    if (is_desktop_pantheon ()) {
+      gtk_widget_destroy (GTK_WIDGET (gtk_builder_get_object (builder, "help-button")));
+    }
   }
+
   gtk_menu_button_set_popover (GTK_MENU_BUTTON (button), page_menu_popover);
   g_object_unref (builder);
 
