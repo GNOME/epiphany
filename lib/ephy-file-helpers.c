f56ac57bee15e649a00490323b457799511961da
Jan-Michael Brummer <jan.brummer@tabos.org>
2018-12-21 11:47:24 +0100

    Reenable test for ephy-file-helpers
    Fix ephy_file_get_downloads_dir () returning the wrong value for Desktop keys and reenable test for ephy-file-helpers.
    
    Fixes: https://gitlab.gnome.org/GNOME/epiphany/issues/419

--- lib/ephy-file-helpers.c
+++ lib/ephy-file-helpers.c
@@ -143,16 +143,16 @@ ephy_file_get_downloads_dir (void)
   download_dir = g_settings_get_string (EPHY_SETTINGS_STATE,
                                         EPHY_PREFS_STATE_DOWNLOAD_DIR);
 
+  if (g_strcmp0 (download_dir, "Desktop") == 0) {
+    g_free (download_dir);
+    return ephy_file_desktop_dir ();
+  }
+
   if (g_strcmp0 (download_dir, "Downloads") == 0 ||
       !g_path_is_absolute (download_dir) ||
       ephy_is_running_inside_flatpak ()) {
     g_free (download_dir);
     return ephy_file_download_dir ();
-  }
-
-  if (g_strcmp0 (download_dir, "Desktop") == 0) {
-    g_free (download_dir);
-    return ephy_file_desktop_dir ();
   }
 
   return download_dir;
