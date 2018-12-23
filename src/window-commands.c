4910109c4bb34556e2e01b6f2d98a05da350bffb
Jan-Michael Brummer <jan.brummer@tabos.org>
2018-12-23 21:53:14 +0100

    Fix tab reordering and add ability to loop around
    Fixes: https://gitlab.gnome.org/GNOME/epiphany/issues/399

--- src/window-commands.c
+++ src/window-commands.c
@@ -2248,15 +2248,22 @@ window_cmd_tabs_move_left (GSimpleAction *action,
 {
   GtkWidget *child;
   GtkNotebook *notebook;
-  int page;
+  int page, n_pages, to;
 
   notebook = GTK_NOTEBOOK (ephy_window_get_notebook (EPHY_WINDOW (user_data)));
   page = gtk_notebook_get_current_page (notebook);
-  if (page < 1)
+  n_pages = gtk_notebook_get_n_pages (notebook) - 1;
+
+  if (!n_pages)
     return;
 
+  if (page < 1)
+    to = n_pages;
+  else
+    to = page - 1;
+
   child = gtk_notebook_get_nth_page (notebook, page);
-  gtk_notebook_reorder_child (notebook, child, page - 1);
+  gtk_notebook_reorder_child (notebook, child, to);
 }
 
 void window_cmd_tabs_move_right (GSimpleAction *action,
@@ -2265,16 +2272,22 @@ void window_cmd_tabs_move_right (GSimpleAction *action,
 {
   GtkWidget *child;
   GtkNotebook *notebook;
-  int page, n_pages;
+  int page, n_pages, to;
 
   notebook = GTK_NOTEBOOK (ephy_window_get_notebook (EPHY_WINDOW (user_data)));
   page = gtk_notebook_get_current_page (notebook);
   n_pages = gtk_notebook_get_n_pages (notebook) - 1;
-  if (page > n_pages - 1)
+
+  if (!n_pages)
     return;
 
+  if (page > n_pages - 1)
+    to = 0;
+  else
+    to = page + 1;
+
   child = gtk_notebook_get_nth_page (notebook, page);
-  gtk_notebook_reorder_child (notebook, child, page + 1);
+  gtk_notebook_reorder_child (notebook, child, to);
 }
 
 void
