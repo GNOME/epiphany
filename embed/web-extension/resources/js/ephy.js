c8f2d88378ac256c2f125f5014a73b6df45f5d36
Jan-Michael Brummer <jan.brummer@tabos.org>
2018-12-25 13:35:51 +0100

    Trust localhost ip address as EPHY_SECURITY_LEVEL_LOCAL_PAGE
    Fixes: https://gitlab.gnome.org/GNOME/epiphany/issues/410

--- embed/web-extension/resources/js/ephy.js
+++ embed/web-extension/resources/js/ephy.js
@@ -583,7 +583,9 @@ Ephy.FormManager = class FormManager
 
     _sensitiveElementFocused(event)
     {
-        let isInsecureAction = this._form.action.startsWith('http://');
+        let url = new URL(this._form.action);
+        // Warning: we do not whitelist localhost because it could be redirected by DNS.
+        let isInsecureAction = url.protocol == 'http:' && url.hostname != "127.0.0.1" && url.hostname != "::1";
         window.webkit.messageHandlers.sensitiveFormFocused.postMessage(this._sensitiveElementMessageSerializer(this._pageID, isInsecureAction));
     }
 
