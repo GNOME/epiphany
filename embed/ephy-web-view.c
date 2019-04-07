3bf0d4201cd1b1b5365df850b427244c07cb9ddb
Jan-Michael Brummer <jan.brummer@tabos.org>
2019-04-07 10:16:36 +0200

    Add option to use known credentials on first authentication try
    If enabled support the user to login within known pages if the credentials are known and
    it is the first login try. Otherwise the credentials could be wrong.

--- embed/ephy-web-view.c
+++ embed/ephy-web-view.c
@@ -2806,6 +2806,31 @@ reader_setting_changed_cb (GSettings   *settings,
   g_free (js_snippet);
 }
 
+static gboolean
+authenticate_cb (WebKitWebView               *web_view,
+                 WebKitAuthenticationRequest *request,
+                 gpointer                     user_data)
+{
+  gboolean res = FALSE;
+
+  if (g_settings_get_boolean (EPHY_SETTINGS_WEB_EXTENSION_WEB, EPHY_PREFS_WEB_NO_AUTH_DIALOG_FOR_SAVED_PASSWORDS)) {
+    WebKitCredential *credential;
+
+    credential = webkit_authentication_request_get_proposed_credential (request);
+
+    if (credential) {
+      if (!webkit_authentication_request_is_retry (request)) {
+        webkit_authentication_request_authenticate (request, credential);
+        res = TRUE;
+      }
+
+      webkit_credential_free (credential);
+    }
+  }
+
+  return res;
+}
+
 static void
 ephy_web_view_init (EphyWebView *web_view)
 {
@@ -2880,6 +2905,10 @@ ephy_web_view_init (EphyWebView *web_view)
 
   g_signal_connect (web_view, "new-window",
                     G_CALLBACK (new_window_cb),
+                    NULL);
+
+  g_signal_connect (web_view, "authenticate",
+                    G_CALLBACK (authenticate_cb),
                     NULL);
 
   g_signal_connect_object (ephy_embed_shell_get_default (), "page-created",
