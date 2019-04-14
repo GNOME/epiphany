5dcb0531017404bd711de58e0ea60a7c2e2fe1f3
Jan-Michael Brummer <jan.brummer@tabos.org>
2019-04-14 18:18:21 +0200

    Add option to start always in incognito mode
    Fixes: https://gitlab.gnome.org/GNOME/epiphany/issues/300

--- lib/ephy-prefs.h
+++ lib/ephy-prefs.h
@@ -171,6 +171,7 @@ static const char * const ephy_prefs_web_schema[] = {
 #define EPHY_PREFS_SEARCH_ENGINES                     "search-engines"
 #define EPHY_PREFS_DEFAULT_SEARCH_ENGINE              "default-search-engine"
 #define EPHY_PREFS_ASK_FOR_DEFAULT                    "ask-for-default"
+#define EPHY_PREFS_START_IN_INCOGNITO_MODE            "start-in-incognito-mode"
 
 #define EPHY_PREFS_LOCKDOWN_SCHEMA            "org.gnome.Epiphany.lockdown"
 #define EPHY_PREFS_LOCKDOWN_FULLSCREEN        "disable-fullscreen"
