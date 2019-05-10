433ce47815c9cb4d7ee09d828456d27bb6f7663f
Jan-Michael Brummer <jan.brummer@tabos.org>
2019-05-10 19:58:08 +0200

    Fix Crash closing last tab with Ctrl+W
    Fixes: https://gitlab.gnome.org/GNOME/epiphany/issues/758

--- src/ephy-window.c
+++ src/ephy-window.c
@@ -2736,8 +2736,10 @@ notebook_page_removed_cb (EphyNotebook *notebook,
 
   tab_accels_update (window);
 
-  if (gtk_notebook_get_n_pages (window->notebook) == 0)
-    gtk_window_close (GTK_WINDOW (window));
+  if (gtk_notebook_get_n_pages (window->notebook) == 0) {
+    ephy_window_close (window);
+    gtk_widget_destroy (GTK_WIDGET (window));
+  }
 }
 
 static void
