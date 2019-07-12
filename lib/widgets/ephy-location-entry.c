9bbfc5201751beae4b0e983fb37e186690061f57
Sevagen <sevagenv@gmail.com>
2019-07-12 21:27:57 +0530

    ephy-location-entry.c: Set placeholder text in empty address bar
    Fixes#852

--- lib/widgets/ephy-location-entry.c
+++ lib/widgets/ephy-location-entry.c
@@ -852,6 +852,7 @@ ephy_location_entry_construct_contents (EphyLocationEntry *entry)
   /* URL entry */
   entry->url_entry = dzl_suggestion_entry_new ();
   gtk_entry_set_icon_tooltip_text (GTK_ENTRY (entry->url_entry), GTK_ENTRY_ICON_PRIMARY, _("Show website security status and permissions"));
+  gtk_entry_set_placeholder_text (GTK_ENTRY (entry->url_entry), _("Search for websites, bookmarks, and open tabs"));
 
   /* Add special widget css provider */
   context = gtk_widget_get_style_context (GTK_WIDGET (entry->url_entry));
