d749f1b166cad923fb431f0ff9a50b0da0df2aca
Jan-Michael Brummer <jan.brummer@tabos.org>
2019-07-28 17:04:41 +0200

    Add ctrl+g shortcut to search entry
    Fixes: https://gitlab.gnome.org/GNOME/epiphany/issues/863

--- embed/ephy-find-toolbar.c
+++ embed/ephy-find-toolbar.c
@@ -262,6 +262,10 @@ entry_key_press_event_cb (GtkEntry        *entry,
               event->keyval == GDK_KEY_KP_Enter ||
               event->keyval == GDK_KEY_ISO_Enter)) {
     handled = ephy_find_toolbar_activate_link (toolbar, event->state);
+  } else if ((event->state & mask) == GDK_CONTROL_MASK &&
+             (event->keyval == GDK_KEY_g)) {
+    handled = TRUE;
+    ephy_find_toolbar_find_next (toolbar);
   } else if ((event->state & mask) == GDK_SHIFT_MASK &&
              (event->keyval == GDK_KEY_Return ||
               event->keyval == GDK_KEY_KP_Enter ||
