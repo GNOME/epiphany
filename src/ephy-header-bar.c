13529ac01a3408ad503a48dd35f92accf85cf75f
Alexander Mikhaylenko <alexm@gnome.org>
2019-09-27 19:20:05 +0500

    action-bar-start: Hide new tab button in fullscreen
    Ideally, there is a tab bar in fullscreen. However, since we don't have
    that, hide the button instead. This still leaves shortcuts though.

--- src/ephy-header-bar.c
+++ src/ephy-header-bar.c
@@ -124,6 +124,7 @@ fullscreen_changed_cb (EphyHeaderBar *header_bar)
 
   gtk_header_bar_set_show_close_button (GTK_HEADER_BAR (header_bar), !fullscreen);
   gtk_widget_set_visible (header_bar->restore_button, fullscreen);
+  ephy_action_bar_set_is_fullscreen (header_bar->action_bar_start, fullscreen);
 }
 
 static void
