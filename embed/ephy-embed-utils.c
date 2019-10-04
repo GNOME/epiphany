b2569974563f969fcbef692386b0e1c0d69163e6
Michael Catanzaro <mcatanzaro@gnome.org>
2019-10-04 10:48:20 -0500

    Expose about:gpu
    This doesn't work properly yet:
    
     * Entering webkit://gpu into the address bar performs a search, even
    though I have added it to the non-search regex
     * It displays an insecure lock icon, which I believe is a WebKit bug.
    WebKitSecurityManager does not know that the webkit:// protocol is
    local.

--- embed/ephy-embed-utils.c
+++ embed/ephy-embed-utils.c
@@ -133,7 +133,8 @@ ephy_embed_utils_address_has_web_scheme (const char *address)
                      g_ascii_strncasecmp (address, "ephy-about", colonpos) &&
                      g_ascii_strncasecmp (address, "ephy-source", colonpos) &&
                      g_ascii_strncasecmp (address, "gopher", colonpos) &&
-                     g_ascii_strncasecmp (address, "inspector", colonpos));
+                     g_ascii_strncasecmp (address, "inspector", colonpos) &&
+                     g_ascii_strncasecmp (address, "webkit", colonpos));
 
   return has_web_scheme;
 }
@@ -248,8 +249,13 @@ ephy_embed_utils_normalize_address (const char *address)
   if (ephy_embed_utils_address_is_existing_absolute_filename (address))
     return g_strconcat ("file://", address, NULL);
 
-  if (g_str_has_prefix (address, "about:") && strcmp (address, "about:blank"))
-    return g_strconcat (EPHY_ABOUT_SCHEME, address + strlen ("about"), NULL);
+  if (g_str_has_prefix (address, "about:")) {
+    if (strcmp (address, "about:gpu") == 0)
+      return g_strdup ("webkit://gpu");
+
+    if (strcmp (address, "about:blank") != 0)
+      return g_strconcat (EPHY_ABOUT_SCHEME, address + strlen ("about"), NULL);
+  }
 
   if (!ephy_embed_utils_address_has_web_scheme (address)) {
     SoupURI *uri;
--- embed/ephy-web-view.h
+++ embed/ephy-web-view.h
@@ -43,6 +43,7 @@ G_DECLARE_FINAL_TYPE (EphyWebView, ephy_web_view, EPHY, WEB_VIEW, WebKitWebView)
                                         "^data:.*$|" \
                                         "^file:.*$|" \
                                         "^inspector://.*$" \
+                                        "^webkit://.*$" \
                                         ")"
 
 #define EPHY_WEB_VIEW_DOMAIN_REGEX "^localhost(\\.[^[:space:]]+)?(:\\d+)?(:[0-9]+)?(/.*)?$|" \
