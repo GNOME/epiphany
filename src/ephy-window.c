b51f6501876127bc6bbaff03acf8b77a1c528850
Jan-Michael Brummer <jan.brummer@tabos.org>
2020-02-28 23:33:03 +0100

    Prevent recursive Epiphany calls for unsupported schemes
    Fixes: https://gitlab.gnome.org/GNOME/epiphany/issues/1053

--- src/ephy-window.c
+++ src/ephy-window.c
@@ -2040,15 +2040,23 @@ decide_navigation_policy (WebKitWebView            *web_view,
   uri = webkit_uri_request_get_uri (request);
 
   if (!ephy_embed_utils_address_has_web_scheme (uri)) {
-    GError *error = NULL;
+    g_autoptr (SoupURI) soup_uri = soup_uri_new (uri);
 
-    gtk_show_uri_on_window (GTK_WINDOW (window), uri, GDK_CURRENT_TIME, &error);
+    if (soup_uri) {
+      g_autoptr (GAppInfo) app_info = g_app_info_get_default_for_uri_scheme (soup_uri->scheme);
 
-    if (error) {
-      LOG ("failed to handle non web scheme: %s", error->message);
-      g_error_free (error);
+      if (app_info && strncmp (g_app_info_get_id (app_info), "org.gnome.Epiphany", 18) != 0) {
+        g_autoptr (GError) error = NULL;
 
-      return FALSE;
+        gtk_show_uri_on_window (GTK_WINDOW (window), uri, GDK_CURRENT_TIME, &error);
+
+        if (error) {
+          LOG ("failed to handle non web scheme: %s", error->message);
+          g_error_free (error);
+
+          return FALSE;
+        }
+      }
     }
 
     webkit_policy_decision_ignore (decision);
