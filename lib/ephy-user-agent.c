bb37c773ee29cc67efcd2bd8292b86802762e820
Jan-Michael Brummer <jan.brummer@tabos.org>
2020-02-29 19:52:40 +0100

    Set mobile user agent depending on chassis type

--- lib/ephy-user-agent.c
+++ lib/ephy-user-agent.c
@@ -26,10 +26,15 @@
 
 #include <webkit2/webkit2.h>
 
+#define CHASSIS_TYPE_PORTABLE   8
+#define CHASSIS_TYPE_DETACHABLE 28
+#define CHASSIS_TYPE_TABLET     32
+
 const char *
 ephy_user_agent_get (void)
 {
   static char *user_agent = NULL;
+  g_autoptr (GFile) sysfs_file = NULL;
   WebKitSettings *settings;
   gboolean mobile;
   gboolean web_app;
@@ -44,7 +49,33 @@ ephy_user_agent_get (void)
     g_free (user_agent);
   }
 
-  mobile = g_settings_get_boolean (EPHY_SETTINGS_WEB, EPHY_PREFS_WEB_MOBILE_USER_AGENT);
+  sysfs_file = g_file_new_for_path ("/sys/devices/virtual/dmi/id/chassis_type");
+  if (g_file_query_exists (sysfs_file, NULL)) {
+    g_autoptr (GFileInfo) file_info = NULL;
+    g_autofree char *data = NULL;
+    g_autoptr (GFileInputStream) input_stream = NULL;
+    goffset file_size;
+
+    file_info = g_file_query_info (sysfs_file, G_FILE_ATTRIBUTE_STANDARD_SIZE, G_FILE_QUERY_INFO_NONE, NULL, NULL);
+    file_size = g_file_info_get_size (file_info);
+
+    if (file_size != 0) {
+      int type;
+
+      data = g_malloc0 (file_size + 1);
+      input_stream = g_file_read (sysfs_file, NULL, NULL);
+
+      g_input_stream_read_all (G_INPUT_STREAM (input_stream), data, file_size, NULL, NULL, NULL);
+
+      type = atoi (data);
+
+      if (type == CHASSIS_TYPE_TABLET || type == CHASSIS_TYPE_DETACHABLE || type == CHASSIS_TYPE_PORTABLE)
+        mobile = TRUE;
+    }
+  } else {
+    mobile = g_settings_get_boolean (EPHY_SETTINGS_WEB, EPHY_PREFS_WEB_MOBILE_USER_AGENT);
+  }
+
   web_app = ephy_profile_dir_is_web_application ();
 
   settings = webkit_settings_new ();
