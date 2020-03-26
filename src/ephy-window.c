GNOME/epiphany

commit 7a4e510e5b4318522daaa07680bdb71976e5ee1a

Set focus to entry for no show addresses

src/ephy-window.c
@@ -3016,6 +3016,26 @@ real_get_active_tab (EphyWindow *window,
   return EPHY_EMBED (embed);
 }
 
+void
+ephy_window_update_entry_focus (EphyWindow  *window,
+                                EphyWebView *view)
+{
+  GtkWidget *title_widget;
+  EphyLocationEntry *lentry;
+  GtkWidget *entry;
+  const char *address = NULL;
+
+  address = ephy_web_view_get_address (view);
+  if (!ephy_embed_utils_is_no_show_address (address) && !ephy_web_view_is_overview (view))
+    return;
+
+  title_widget = GTK_WIDGET (ephy_header_bar_get_title_widget (EPHY_HEADER_BAR (window->header_bar)));
+  lentry = EPHY_LOCATION_ENTRY (title_widget);
+  entry = ephy_location_entry_get_entry (lentry);
+
+  gtk_entry_grab_focus_without_selecting (GTK_ENTRY (entry));
+}
+
 static void
 notebook_switch_page_cb (GtkNotebook *notebook,
                          GtkWidget   *page,
@@ -3045,6 +3065,8 @@ notebook_switch_page_cb (GtkNotebook *notebook,
   g_simple_action_set_state (G_SIMPLE_ACTION (action), g_variant_new_uint32 (page_num));
 
   update_reader_mode (window, view);
+
+  ephy_window_update_entry_focus (window, view);
 }
 
 static void

src/ephy-session.c
@@ -1295,6 +1295,7 @@ session_end_element (GMarkupParseContext  *ctx,
 
       active_child = ephy_embed_container_get_active_child (EPHY_EMBED_CONTAINER (context->window));
       gtk_widget_grab_focus (GTK_WIDGET (active_child));
+      ephy_window_update_entry_focus (context->window, ephy_embed_get_web_view (active_child));
       gtk_widget_show (GTK_WIDGET (context->window));
     }
 

src/ephy-window.h
@@ -84,4 +84,7 @@ void              ephy_window_show_fullscreen_header_bar (EphyWindow *window);
 int               ephy_window_get_position_for_new_embed (EphyWindow *window,
                                                           EphyEmbed  *embed);
 
+void              ephy_window_update_entry_focus         (EphyWindow  *window,
+                                                          EphyWebView *view);
+
 G_END_DECLS

commit 9bcda80d33126a559960436183c43d4cb6f01012

window: fix critical on startup in app mode
This regressed in 7a4e510e. We have to make sure the title widget is
actually a location entry before using it as such. (In app mode, there
is no location entry.)

src/ephy-window.c
@@ -3040,7 +3040,6 @@ ephy_window_update_entry_focus (EphyWindow  *window,
                                 EphyWebView *view)
 {
   GtkWidget *title_widget;
-  EphyLocationEntry *lentry;
   GtkWidget *entry;
   const char *address = NULL;
 
@@ -3049,10 +3048,10 @@ ephy_window_update_entry_focus (EphyWindow  *window,
     return;
 
   title_widget = GTK_WIDGET (ephy_header_bar_get_title_widget (EPHY_HEADER_BAR (window->header_bar)));
-  lentry = EPHY_LOCATION_ENTRY (title_widget);
-  entry = ephy_location_entry_get_entry (lentry);
-
-  gtk_entry_grab_focus_without_selecting (GTK_ENTRY (entry));
+  if (EPHY_IS_LOCATION_ENTRY (title_widget)) {
+    entry = ephy_location_entry_get_entry (EPHY_LOCATION_ENTRY (title_widget));
+    gtk_entry_grab_focus_without_selecting (GTK_ENTRY (entry));
+  }
 }
 
 static void