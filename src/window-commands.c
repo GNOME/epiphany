f64243eb8620482367dbf716bb5bab17ef0d7616
Michael Catanzaro <mcatanzaro@gnome.org>
2020-04-02 20:53:02 -0500

    Fix back/forward shortcuts and gestures in shortcuts dialogs
    These shortcuts are inverted in RTL. The shortcuts are working properly;
    we just need to fix the shortcuts dialog. Ditto for the gestures.
    
    Fixes #1145

--- src/window-commands.c
+++ src/window-commands.c
@@ -550,6 +550,22 @@ window_cmd_show_shortcuts (GSimpleAction *action,
     builder = gtk_builder_new_from_resource ("/org/gnome/epiphany/gtk/shortcuts-dialog.ui");
     shortcuts_window = GTK_WIDGET (gtk_builder_get_object (builder, "shortcuts-dialog"));
 
+    if (gtk_widget_get_default_direction () == GTK_TEXT_DIR_RTL) {
+      GtkShortcutsShortcut *shortcut;
+
+      shortcut = GTK_SHORTCUTS_SHORTCUT (gtk_builder_get_object (builder, "go-back-shortcut"));
+      g_object_set (shortcut, "accelerator", "<Alt>Right", NULL);
+
+      shortcut = GTK_SHORTCUTS_SHORTCUT (gtk_builder_get_object (builder, "go-forward-shortcut"));
+      g_object_set (shortcut, "accelerator", "<Alt>Left", NULL);
+
+      shortcut = GTK_SHORTCUTS_SHORTCUT (gtk_builder_get_object (builder, "go-back-gesture"));
+      g_object_set (shortcut, "shortcut-type", GTK_SHORTCUT_GESTURE_TWO_FINGER_SWIPE_LEFT, NULL);
+
+      shortcut = GTK_SHORTCUTS_SHORTCUT (gtk_builder_get_object (builder, "go-forward-gesture"));
+      g_object_set (shortcut, "shortcut-type", GTK_SHORTCUT_GESTURE_TWO_FINGER_SWIPE_RIGHT, NULL);
+    }
+
     g_signal_connect (shortcuts_window,
                       "destroy",
                       G_CALLBACK (gtk_widget_destroyed),
