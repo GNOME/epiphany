28a902168bdfd6398212fd46e71d6e1545f60582
Jan-Michael Brummer <jan.brummer@tabos.org>
2020-04-20 11:39:27 +0000

    Only create a new-window if the session is not empty
    Fixes: https://gitlab.gnome.org/GNOME/epiphany/-/issues/1152
    
    
    (cherry picked from commit bd3316e97db493fd0f6de0ec27432c375e6b3dec)

--- src/ephy-shell.c
+++ src/ephy-shell.c
@@ -124,7 +124,7 @@ ephy_shell_startup_continue (EphyShell               *shell,
     g_assert (session != NULL);
     ephy_session_load (session, (const char *)ctx->session_filename,
                        ctx->user_time, NULL, NULL, NULL);
-  } else if (new_window_option) {
+  } else if (new_window_option && shell->remote_startup_context) {
     char *homepage_url = g_settings_get_string (EPHY_SETTINGS_MAIN, EPHY_PREFS_HOMEPAGE_URL);
     const char *default_uris[] = { homepage_url, NULL };
     const char **uris = NULL;
