b8e2dff86d84dfff6d97fea3975c3b26ca1b05f9
Jan-Michael Brummer <jan.brummer@tabos.org>
2020-04-30 11:17:33 +0200

    Add Chrome/Chromium password import
    Fixes: https://gitlab.gnome.org/GNOME/epiphany/-/issues/424

---
+++ lib/sync/ephy-password-import.h
@@ -0,0 +1,33 @@
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ *  Copyright © 20120 Jan-Michael Brummer <jan.brummer@tabos.org>
+ *
+ *  This file is part of Epiphany.
+ *
+ *  Epiphany is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Epiphany is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Epiphany.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <glib.h>
+
+#include "ephy-password-manager.h"
+
+typedef enum {
+  CHROME,
+  CHROMIUM
+} ChromeType;
+
+void ephy_password_import_from_chrome_async (EphyPasswordManager *manager, ChromeType type, GAsyncReadyCallback callback, gpointer user_data);
+gboolean ephy_password_import_from_chrome_finish (GObject *source_object, GAsyncResult *result, GError **error);
