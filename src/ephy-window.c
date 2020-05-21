1b32ccb95a1766990d563ceec2604491561b41a5
vanadiae <vanadiae35@gmail.com>
2020-05-21 15:31:53 +0200

    image-as-wallpaper: disable in flatpak until full support
    Currently, setting an image as wallpaper in a flatpak is totally
    broken. So disable for 3.36 and a fix will be pushed for 3.38.

--- src/ephy-window.c
+++ src/ephy-window.c
@@ -1717,8 +1717,8 @@ populate_context_menu (WebKitWebView       *web_view,
       add_action_to_context_menu (context_menu, popup_action_group,
                                   "view-image", window);
 
-    add_action_to_context_menu (context_menu, popup_action_group,
-                                "set-image-as-background", window);
+    /* add_action_to_context_menu (context_menu, popup_action_group, */
+    /*                             "set-image-as-background", window); */
   }
 
   if (is_media) {
