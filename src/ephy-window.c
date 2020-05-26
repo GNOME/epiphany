478d9a9c22935f7f924977255c149e225a91204e
Alexander Mikhaylenko <alexm@gnome.org>
2020-05-26 21:19:37 +0500

    window: Stop exposing GtkStack
    Remove ephy_window_get_stack() and replace it with more generic
    ephy_window_open_pages_view() and ephy_window_close_pages_view().
    
    This will allow to use a different widget in the next commits.
    
    This reverts commit 1603943c626854600a25535b4d64a379a05927f7.

--- src/ephy-window.c
+++ src/ephy-window.c
@@ -547,7 +547,7 @@ update_adaptive_mode (EphyWindow *window)
    * to the main view.
    */
   if (adaptive_mode == EPHY_ADAPTIVE_MODE_NORMAL)
-    gtk_stack_set_visible_child_name (GTK_STACK (window->main_stack), "content");
+    ephy_window_close_pages_view (window);
 }
 
 static void
@@ -3953,20 +3953,33 @@ ephy_window_get_notebook (EphyWindow *window)
 }
 
 /**
- * ephy_window_get_stack:
+ * ephy_window_open_pages_view
  * @window: an #EphyWindow
  *
- * Returns the #GtkStack housing the content and tab views
- *
- * Return value: (transfer none): the @window's #GtkStack
+ * Opens the mobile pages view
  **/
-GtkWidget *
-ephy_window_get_stack (EphyWindow *window)
+void
+ephy_window_open_pages_view (EphyWindow *window)
 {
   g_assert (EPHY_IS_WINDOW (window));
 
-  return GTK_WIDGET (window->main_stack);
+  gtk_stack_set_visible_child_name (GTK_STACK (window->main_stack), "tabs");
 }
+
+/**
+ * ephy_window_close_pages_view
+ * @window: an #EphyWindow
+ *
+ * Closes the mobile pages view
+ **/
+void
+ephy_window_close_pages_view (EphyWindow *window)
+{
+  g_assert (EPHY_IS_WINDOW (window));
+
+  gtk_stack_set_visible_child_name (GTK_STACK (window->main_stack), "content");
+}
+
 /**
  * ephy_window_get_find_toolbar:
  * @window: an #EphyWindow
