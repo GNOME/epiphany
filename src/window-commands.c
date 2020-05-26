86b6d87083dafb2c80dc3e2b10ac337805cd227a
Alexander Mikhaylenko <alexm@gnome.org>
2020-05-26 21:19:37 +0500

    window: Stop exposing GtkStack
    Remove ephy_window_get_stack() and replace it with more generic
    ephy_window_open_pages_view() and ephy_window_close_pages_view().
    
    This will allow to use a different widget in the next commits.
    
    This reverts commit 1603943c626854600a25535b4d64a379a05927f7.

--- src/window-commands.c
+++ src/window-commands.c
@@ -1246,11 +1246,9 @@ window_cmd_new_tab (GSimpleAction *action,
                     GVariant      *parameter,
                     gpointer       user_data)
 {
-  GtkWidget *stack;
   EphyWindow *window = user_data;
   char *url;
 
-  stack = ephy_window_get_stack (window);
   url = g_settings_get_string (EPHY_SETTINGS_MAIN, EPHY_PREFS_HOMEPAGE_URL);
   if (g_strcmp0 (url, "about:blank") != 0) {
     g_free (url);
@@ -1260,7 +1258,7 @@ window_cmd_new_tab (GSimpleAction *action,
   ephy_link_open (EPHY_LINK (window),
                   url, NULL,
                   EPHY_LINK_NEW_TAB | EPHY_LINK_JUMP_TO);
-  gtk_stack_set_visible_child_name (GTK_STACK (stack), "content");
+  ephy_window_close_pages_view (window);
   g_free (url);
 }
 
@@ -2507,11 +2505,7 @@ window_cmd_go_content (GSimpleAction *action,
                        GVariant      *parameter,
                        gpointer       user_data)
 {
-  GtkWidget *stack;
-  EphyWindow *window = EPHY_WINDOW (user_data);
-
-  stack = ephy_window_get_stack (window);
-  gtk_stack_set_visible_child_name (GTK_STACK (stack), "content");
+  ephy_window_close_pages_view (EPHY_WINDOW (user_data));
 }
 
 void
@@ -2519,11 +2513,7 @@ window_cmd_go_tabs_view (GSimpleAction *action,
                          GVariant      *parameter,
                          gpointer       user_data)
 {
-  GtkWidget *stack;
-  EphyWindow *window = EPHY_WINDOW (user_data);
-
-  stack = ephy_window_get_stack (window);
-  gtk_stack_set_visible_child_name (GTK_STACK (stack), "tabs");
+  ephy_window_open_pages_view (EPHY_WINDOW (user_data));
 }
 
 void
