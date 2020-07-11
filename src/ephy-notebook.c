6af9dd06e746d433132542863b3a3a156d40b3e0
Jan-Michael Brummer <jan.brummer@tabos.org>
2020-07-11 15:22:18 +0200

    Flash notebook arrow in case new tab is not visible
    Fixes: https://gitlab.gnome.org/GNOME/epiphany/-/issues/48

--- src/ephy-notebook.c
+++ src/ephy-notebook.c
@@ -854,6 +854,65 @@ ephy_notebook_insert_page (GtkNotebook *gnotebook,
   return position;
 }
 
+typedef struct {
+  EphyNotebook *notebook;
+  int old_pos;
+  int new_pos;
+  guint arrow_flash_source;
+} PresentTabData;
+
+static PresentTabData *
+present_tab_data_new (EphyNotebook *notebook,
+                      gint          old_pos,
+                      gint          new_pos)
+{
+  PresentTabData *pt_data = g_new0 (PresentTabData, 1);
+  pt_data->notebook = g_object_ref (notebook);
+  pt_data->old_pos = old_pos;
+  pt_data->new_pos = new_pos;
+
+  return pt_data;
+}
+
+static void
+present_tab_data_free (PresentTabData *pt_data)
+{
+  g_clear_object (&pt_data->notebook);
+  g_free (pt_data);
+}
+
+static gboolean
+remove_arrow_flash (gpointer user_data)
+{
+  PresentTabData *pt_data = user_data;
+
+  gtk_style_context_remove_class (gtk_widget_get_style_context (GTK_WIDGET (pt_data->notebook)), "tab-arrow-attention");
+  pt_data->arrow_flash_source = 0;
+  present_tab_data_free (pt_data);
+
+  return G_SOURCE_REMOVE;
+}
+
+static gboolean
+present_new_tab (gpointer user_data)
+{
+  PresentTabData *pt_data = user_data;
+  GtkWidget *page;
+  GtkWidget *label;
+
+  page = gtk_notebook_get_nth_page (GTK_NOTEBOOK (pt_data->notebook), pt_data->new_pos);
+  label = gtk_notebook_get_tab_label (GTK_NOTEBOOK (pt_data->notebook), page);
+
+  if (!gtk_widget_get_mapped (GTK_WIDGET (label))) {
+    gtk_style_context_add_class (gtk_widget_get_style_context (GTK_WIDGET (pt_data->notebook)), "tab-arrow-attention");
+    pt_data->arrow_flash_source = g_timeout_add (500, remove_arrow_flash, pt_data);
+  } else {
+    present_tab_data_free (pt_data);
+  }
+
+  return G_SOURCE_REMOVE;
+}
+
 int
 ephy_notebook_add_tab (EphyNotebook *notebook,
                        EphyEmbed    *embed,
@@ -878,6 +937,9 @@ ephy_notebook_add_tab (EphyNotebook *notebook,
     gtk_notebook_set_current_page (gnotebook, position);
     g_object_set_data (G_OBJECT (embed), "jump_to",
                        GINT_TO_POINTER (jump_to));
+  } else {
+    PresentTabData *pt_data = present_tab_data_new (notebook, gtk_notebook_get_current_page (gnotebook), position);
+    g_idle_add (present_new_tab, pt_data);
   }
 
   return position;
