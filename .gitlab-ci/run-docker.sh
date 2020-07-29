0622fb282c0b008980ab72172dbdfe38bde3431e
Jan-Michael Brummer <jan.brummer@tabos.org>
2020-07-29 18:48:33 +0200

    Use own docker image for check-code-style
    Instead of installing necessary tools during pipeline execution, make
    use of a well prepared docker image.

---
+++ .gitlab-ci/run-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+TAG="registry.gitlab.gnome.org/gnome/epiphany/master:v1"
+
+cd "$(dirname "$0")"
+
+podman build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" --file "Dockerfile" --format=docker .
+
+if [ "$1" = "--push" ]; then
+  podman login registry.gitlab.gnome.org
+  podman push $TAG
+else
+  podman run --rm \
+      --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
+      --tty --interactive "${TAG}" bash
+fi
