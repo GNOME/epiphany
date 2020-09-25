4d85e4fbd540fb36c55b3c299a72a871c8aabd0d
Jan-Michael Brummer <jan.brummer@tabos.org>
2020-09-25 17:50:57 +0200

    ephy-reader-handler: Only fetch existing source data for user triggered actions
    Store user reader mode toggle request as EphyWebView property. Based on this information
    load the existing source data or trigger a new load.
    
    Fixes: https://gitlab.gnome.org/GNOME/epiphany/-/issues/1339

--- embed/ephy-reader-handler.c
+++ embed/ephy-reader-handler.c
@@ -45,7 +45,6 @@ typedef struct {
   WebKitWebView *web_view;
   GCancellable *cancellable;
   guint load_changed_id;
-  char *uri;
 } EphyReaderRequest;
 
 static EphyReaderRequest *
@@ -70,7 +69,6 @@ ephy_reader_request_free (EphyReaderRequest *request)
   if (request->load_changed_id > 0)
     g_signal_handler_disconnect (request->web_view, request->load_changed_id);
 
-  g_clear_pointer (&request->uri, g_free);
   g_object_unref (request->source_handler);
   g_object_unref (request->scheme_request);
   g_clear_object (&request->web_view);
@@ -253,42 +251,11 @@ ephy_reader_request_begin_get_source_from_uri (EphyReaderRequest *request,
 }
 
 static void
-web_resource_data_cb (WebKitWebResource *resource,
-                      GAsyncResult      *result,
-                      EphyReaderRequest *request)
-{
-  g_autofree guchar *data = NULL;
-  g_autoptr (GError) error = NULL;
-  gsize length;
-
-  data = webkit_web_resource_get_data_finish (resource, result, &length, &error);
-  if (!data || !*data || length == 0) {
-    g_clear_object (&request->web_view);
-    ephy_reader_request_begin_get_source_from_uri (request, request->uri);
-  } else {
-    ephy_reader_request_begin_get_source_from_web_view (request, request->web_view);
-  }
-}
-
-static void
-ephy_reader_request_begin_get_source_from_initiator (EphyReaderRequest *request,
-                                                     WebKitWebView     *web_view)
-{
-  WebKitWebResource *resource = webkit_web_view_get_main_resource (web_view);
-
-  g_assert (resource);
-  webkit_web_resource_get_data (resource,
-                                request->cancellable,
-                                (GAsyncReadyCallback)web_resource_data_cb,
-                                request);
-}
-
-static void
 ephy_reader_request_start (EphyReaderRequest *request)
 {
   g_autoptr (SoupURI) soup_uri = NULL;
-  const char *modified_uri;
   const char *original_uri;
+  WebKitWebView *web_view;
 
   original_uri = webkit_uri_scheme_request_get_uri (request->scheme_request);
   soup_uri = soup_uri_new (original_uri);
@@ -303,18 +270,28 @@ ephy_reader_request_start (EphyReaderRequest *request)
     return;
   }
 
-  /* Extract URI:
-   * ephy-reader:https://example.com/whatever?xyz into https://example.com/whatever?xyz
-   */
-  modified_uri = soup_uri_to_string (soup_uri, TRUE);
-  g_assert (modified_uri);
+  web_view = webkit_uri_scheme_request_get_web_view (request->scheme_request);
+  if (web_view) {
+    gboolean entering_reader_mode;
 
-  request->uri = g_strdup (modified_uri);
-  request->web_view = webkit_uri_scheme_request_get_web_view (request->scheme_request);
-  if (request->web_view && webkit_web_view_get_main_resource (request->web_view))
-    ephy_reader_request_begin_get_source_from_initiator (request, g_object_ref (request->web_view));
-  else
-    ephy_reader_request_begin_get_source_from_uri (request, request->uri);
+    g_object_get (G_OBJECT (web_view), "entering-reader-mode", &entering_reader_mode, NULL);
+    if (!entering_reader_mode)
+      web_view = NULL;
+  }
+
+  if (web_view) {
+    ephy_reader_request_begin_get_source_from_web_view (request, web_view);
+  } else {
+    const char *source_uri;
+
+    /* Extract URI:
+     * ephy-reader:https://example.com/whatever?xyz into https://example.com/whatever?xyz
+     */
+    source_uri = soup_uri_to_string (soup_uri, TRUE);
+    g_assert (source_uri);
+
+    ephy_reader_request_begin_get_source_from_uri (request, source_uri);
+  }
 
   request->source_handler->outstanding_requests =
     g_list_prepend (request->source_handler->outstanding_requests, request);
