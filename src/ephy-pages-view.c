8d505327d55edeec25f9d82cf65e8d00b72ba817
Alexander Mikhaylenko <alexm@gnome.org>
2021-01-28 03:01:15 +0500

    Tweak page list styles
    Match GTK4 and the upcoming new tabs more closely.
    
    Since the close button style is shared with the bookmark tags, match that
    as well.

--- src/ephy-pages-view.c
+++ src/ephy-pages-view.c
@@ -181,32 +181,9 @@ ephy_pages_view_class_init (EphyPagesViewClass *klass)
 }
 
 static void
-separator_header (GtkListBoxRow *row,
-                  GtkListBoxRow *before,
-                  gpointer       user_data)
-{
-  GtkWidget *header;
-
-  if (before == NULL) {
-    gtk_list_box_row_set_header (row, NULL);
-
-    return;
-  }
-
-  if (gtk_list_box_row_get_header (row) != NULL)
-    return;
-
-  header = gtk_separator_new (GTK_ORIENTATION_HORIZONTAL);
-  gtk_widget_show (header);
-  gtk_list_box_row_set_header (row, header);
-}
-
-static void
 ephy_pages_view_init (EphyPagesView *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
-
-  gtk_list_box_set_header_func (self->list_box, separator_header, NULL, NULL);
 
   self->list_store = g_list_store_new (EPHY_TYPE_PAGE_ROW);
 
