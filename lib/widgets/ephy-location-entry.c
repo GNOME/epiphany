GNOME/epiphany

commit 5410fec78bc699b593d327e6a98b105f6cef7cd5

Add initial WebExtension support

lib/widgets/ephy-location-entry.c
@@ -57,6 +57,8 @@ struct _EphyLocationEntry {
   GtkOverlay parent_instance;
 
   GtkWidget *url_entry;
+  GtkWidget *button_box;
+  GtkWidget *page_action_box;
   GtkWidget *bookmark;
   GtkWidget *bookmark_event_box;
   GtkWidget *reader_mode;
@@ -990,7 +992,6 @@ static void
 ephy_location_entry_construct_contents (EphyLocationEntry *entry)
 {
   GtkWidget *event;
-  GtkWidget *box;
   GtkStyleContext *context;
   DzlShortcutController *controller;
 
@@ -1030,14 +1031,26 @@ ephy_location_entry_construct_contents (EphyLocationEntry *entry)
   gtk_overlay_add_overlay (GTK_OVERLAY (entry), event);
 
   /* Button Box */
-  box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
-  gtk_container_add (GTK_CONTAINER (event), box);
-  g_signal_connect (G_OBJECT (box), "size-allocate", G_CALLBACK (button_box_size_allocated_cb), entry);
-  gtk_widget_set_halign (box, GTK_ALIGN_END);
-  gtk_widget_set_valign (box, GTK_ALIGN_CENTER);
-  gtk_widget_show (box);
+  entry->button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+  gtk_container_add (GTK_CONTAINER (event), entry->button_box);
+  gtk_box_set_homogeneous (GTK_BOX (entry->button_box), FALSE);
+  g_signal_connect (G_OBJECT (entry->button_box), "size-allocate", G_CALLBACK (button_box_size_allocated_cb), entry);
+  gtk_button_box_set_layout (GTK_BUTTON_BOX (entry->button_box), GTK_BUTTONBOX_EXPAND);
+  gtk_widget_set_valign (entry->button_box, GTK_ALIGN_CENTER);
+  gtk_widget_set_halign (entry->button_box, GTK_ALIGN_END);
+  gtk_widget_set_margin_end (entry->button_box, 5);
+  gtk_widget_show (entry->button_box);
 
-  context = gtk_widget_get_style_context (box);
+  /* Page action box */
+  entry->page_action_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+  gtk_box_set_homogeneous (GTK_BOX (entry->page_action_box), FALSE);
+  gtk_widget_show (entry->page_action_box);
+  gtk_button_box_set_layout (GTK_BUTTON_BOX (entry->page_action_box), GTK_BUTTONBOX_EXPAND);
+  gtk_widget_set_valign (entry->page_action_box, GTK_ALIGN_CENTER);
+  gtk_widget_set_halign (entry->page_action_box, GTK_ALIGN_END);
+  gtk_box_pack_start (GTK_BOX (entry->button_box), entry->page_action_box, FALSE, FALSE, 0);
+
+  context = gtk_widget_get_style_context (entry->button_box);
   gtk_style_context_add_class (context, "entry_icon_box");
 
   /* Bookmark */
@@ -1048,7 +1061,7 @@ ephy_location_entry_construct_contents (EphyLocationEntry *entry)
   gtk_widget_show (entry->bookmark);
   g_signal_connect (G_OBJECT (entry->bookmark_event_box), "button_press_event", G_CALLBACK (bookmark_icon_button_press_event_cb), entry);
   gtk_container_add (GTK_CONTAINER (entry->bookmark_event_box), entry->bookmark);
-  gtk_box_pack_end (GTK_BOX (box), entry->bookmark_event_box, FALSE, FALSE, 0);
+  gtk_box_pack_end (GTK_BOX (entry->button_box), entry->bookmark_event_box, FALSE, FALSE, 6);
 
   context = gtk_widget_get_style_context (entry->bookmark);
   gtk_style_context_add_class (context, "entry_icon");
@@ -1066,7 +1079,7 @@ ephy_location_entry_construct_contents (EphyLocationEntry *entry)
   gtk_widget_set_valign (entry->reader_mode, GTK_ALIGN_CENTER);
   gtk_widget_show (entry->reader_mode);
   gtk_container_add (GTK_CONTAINER (entry->reader_mode_event_box), entry->reader_mode);
-  gtk_box_pack_end (GTK_BOX (box), entry->reader_mode_event_box, FALSE, FALSE, 0);
+  gtk_box_pack_end (GTK_BOX (entry->button_box), entry->reader_mode_event_box, FALSE, FALSE, 6);
 
   context = gtk_widget_get_style_context (entry->reader_mode);
   gtk_style_context_add_class (context, "entry_icon");
@@ -1504,4 +1517,36 @@ ephy_location_entry_set_mobile_popdown (EphyLocationEntry *entry,
     dzl_suggestion_entry_set_position_func (DZL_SUGGESTION_ENTRY (entry->url_entry), dzl_suggestion_entry_window_position_func, NULL, NULL);
   else
     dzl_suggestion_entry_set_position_func (DZL_SUGGESTION_ENTRY (entry->url_entry), position_func, NULL, NULL);
+}
+
+void
+ephy_location_entry_page_action_add (EphyLocationEntry *entry,
+                                     GtkWidget         *action)
+{
+  GtkStyleContext *context;
+
+  context = gtk_widget_get_style_context (action);
+  gtk_style_context_add_class (context, "entry_icon");
+
+  gtk_box_pack_end (GTK_BOX (entry->page_action_box), action, FALSE, FALSE, 6);
+}
+
+static
+void clear_page_actions (GtkWidget *child,
+                         gpointer   user_data)
+{
+  EphyLocationEntry *entry = EPHY_LOCATION_ENTRY (user_data);
+  GtkStyleContext *context;
+
+  context = gtk_widget_get_style_context (child);
+
+  gtk_style_context_remove_class (context, "entry_icon");
+
+  gtk_container_remove (GTK_CONTAINER (entry->page_action_box), child);
+}
+
+void
+ephy_location_entry_page_action_clear (EphyLocationEntry *entry)
+{
+  gtk_container_foreach (GTK_CONTAINER (entry->page_action_box), clear_page_actions, entry);
 }

data/org.gnome.Epiphany.desktop.in.in
@@ -11,7 +11,7 @@ Type=Application
 # Translators: Do NOT translate or transliterate this text (this is an icon file name)!
 Icon=@icon@
 Categories=Network;GNOME;GTK;WebBrowser;
-MimeType=text/html;text/xml;application/xhtml+xml;x-scheme-handler/http;x-scheme-handler/https;multipart/related;application/x-mimearchive;message/rfc822;
+MimeType=text/html;text/xml;application/xhtml+xml;x-scheme-handler/http;x-scheme-handler/https;multipart/related;application/x-mimearchive;message/rfc822;application/x-xpinstall;
 Actions=new-window;Incognito;
 # Translators: Do NOT translate or transliterate this text (these are enum types)!
 X-Purism-FormFactor=Workstation;Mobile;

data/org.gnome.epiphany.gschema.xml
@@ -245,6 +245,16 @@
 			<summary>Enable immediately switch to new open tab</summary>
 			<description>Whether to automatically switch to a new open tab.</description>
 		</key>
+		<key type="b" name="enable-webextensions">
+			<default>false</default>
+			<summary>Enable WebExtensions</summary>
+			<description>Whether to enable WebExtensions. WebExtensions is a cross-browser system for extensions.</description>
+		</key>
+		<key type="as" name="webextensions-active">
+			<default>[]</default>
+			<summary>Active WebExtensions</summary>
+			<description>Indicates which WebExtensions are set to active.</description>
+		</key>
 	</schema>
 	<schema id="org.gnome.Epiphany.webapp">
 		<key type="as" name="additional-urls">

embed/ephy-web-view.c
@@ -67,6 +67,8 @@
 #define EPHY_PAGE_TEMPLATE_ERROR         "/org/gnome/epiphany/page-templates/error.html"
 #define EPHY_PAGE_TEMPLATE_ERROR_CSS     "/org/gnome/epiphany/page-templates/error.css"
 
+static guint64 web_view_uid = 1;
+
 struct _EphyWebView {
   WebKitWebView parent_instance;
 
@@ -125,6 +127,8 @@ struct _EphyWebView {
   char *tls_error_failing_uri;
 
   EphyWebViewErrorPage error_page;
+
+  guint64 uid;
 };
 
 enum {
@@ -1427,6 +1431,11 @@ update_security_status_for_committed_load (EphyWebView *view,
   if (view->loading_error_page)
     return;
 
+  if (g_str_has_prefix (uri, "webextension://")) {
+    /* Hidden WebExtension webview, ignoring */
+    return;
+  }
+
   toplevel = gtk_widget_get_toplevel (GTK_WIDGET (view));
   if (EPHY_IS_EMBED_CONTAINER (toplevel))
     embed = EPHY_GET_EMBED_FROM_EPHY_WEB_VIEW (view);
@@ -2725,6 +2734,19 @@ scale_factor_changed_cb (EphyWebView *web_view,
   _ephy_web_view_update_icon (web_view);
 }
 
+GtkWidget *
+ephy_web_view_new_with_user_content_manager (WebKitUserContentManager *ucm)
+{
+  EphyEmbedShell *shell = ephy_embed_shell_get_default ();
+
+  return g_object_new (EPHY_TYPE_WEB_VIEW,
+                       "web-context", ephy_embed_shell_get_web_context (shell),
+                       "user-content-manager", ucm,
+                       "settings", ephy_embed_prefs_get_settings (),
+                       "is-controlled-by-automation", ephy_embed_shell_get_mode (shell) == EPHY_EMBED_SHELL_MODE_AUTOMATION,
+                       NULL);
+}
+
 /**
  * ephy_web_view_load_request:
  * @view: the #EphyWebView in which to load the request
@@ -3771,6 +3793,8 @@ ephy_web_view_init (EphyWebView *web_view)
 
   shell = ephy_embed_shell_get_default ();
 
+  web_view->uid = web_view_uid++;
+
   web_view->is_blank = TRUE;
   web_view->ever_committed = FALSE;
   web_view->document_type = EPHY_WEB_VIEW_DOCUMENT_HTML;
@@ -4119,4 +4143,10 @@ ephy_web_view_new_with_related_view (WebKitWebView *related_view)
                        "user-content-manager", ucm,
                        "settings", ephy_embed_prefs_get_settings (),
                        NULL);
+}
+
+guint64
+ephy_web_view_get_uid (EphyWebView *web_view)
+{
+  return web_view->uid;
 }

embed/ephy-web-view.h
@@ -182,4 +182,8 @@ void                       ephy_web_view_show_auth_form_save_request (EphyWebVie
                                                                       gpointer                        response_data,
                                                                       GDestroyNotify                  response_destroy);
 
+GtkWidget                 *ephy_web_view_new_with_user_content_manager (WebKitUserContentManager *ucm);
+
+guint64                    ephy_web_view_get_uid                       (EphyWebView *web_view);
+
 G_END_DECLS

embed/web-process-extension/ephy-web-process-extension-main.c
@@ -63,8 +63,10 @@ webkit_web_extension_initialize_with_user_data (WebKitWebExtension *webkit_exten
 static void __attribute__((destructor))
 ephy_web_process_extension_shutdown (void)
 {
-  if (extension)
+  if (extension) {
+    ephy_web_process_extension_deinitialize (extension);
     g_object_unref (extension);
+  }
 
   ephy_settings_shutdown ();
   ephy_file_helpers_shutdown ();

embed/web-process-extension/ephy-web-process-extension.c
@@ -20,6 +20,7 @@
 
 #include "config.h"
 #include "ephy-web-process-extension.h"
+#include "ephy-webextension-api.h"
 
 #include "ephy-debug.h"
 #include "ephy-file-helpers.h"
@@ -56,9 +57,16 @@ struct _EphyWebProcessExtension {
   gboolean is_private_profile;
 
   GHashTable *frames_map;
+  GHashTable *translation_table;
 };
 
 G_DEFINE_TYPE (EphyWebProcessExtension, ephy_web_process_extension, G_TYPE_OBJECT)
+
+GHashTable *
+ephy_web_process_extension_get_translations (EphyWebProcessExtension *extension)
+{
+  return extension->translation_table;
+}
 
 static void
 web_page_will_submit_form (WebKitWebPage            *web_page,
@@ -326,6 +334,18 @@ ephy_web_process_extension_user_message_received_cb (EphyWebProcessExtension *ex
       return;
 
     g_variant_get (parameters, "b", &extension->should_remember_passwords);
+  } else if (g_strcmp0 (name, "WebExtension.Add") == 0) {
+    GVariant *parameters;
+    const char *name;
+    const char *data;
+    guint64 length;
+
+    parameters = webkit_user_message_get_parameters (message);
+    if (!parameters)
+      return;
+
+    g_variant_get (parameters, "(&s&st)", &name, &data, &length);
+    webextensions_add_translation (extension, name, data, length);
   }
 }
 
@@ -645,6 +665,8 @@ window_object_cleared_cb (WebKitScriptWorld       *world,
   js_context = webkit_frame_get_js_context_for_script_world (frame, world);
   jsc_context_push_exception_handler (js_context, (JSCExceptionHandler)js_exception_handler, NULL, NULL);
 
+  set_up_webextensions (extension, page, js_context);
+
   bytes = g_resources_lookup_data ("/org/gnome/epiphany-web-process-extension/js/ephy.js", G_RESOURCE_LOOKUP_FLAGS_NONE, NULL);
   data = g_bytes_get_data (bytes, &data_size);
   result = jsc_context_evaluate_with_source_uri (js_context, data, data_size, "resource:///org/gnome/epiphany-web-process-extension/js/ephy.js", 1);
@@ -771,7 +793,12 @@ ephy_web_process_extension_initialize (EphyWebProcessExtension *extension,
 
   extension->initialized = TRUE;
 
-  extension->script_world = webkit_script_world_new_with_name (guid);
+  /* Note: An empty guid is used ONLY for WebExtensions which do have an own initialization function */
+  if (strlen (guid) > 0)
+    extension->script_world = webkit_script_world_new_with_name (guid);
+  else
+    extension->script_world = webkit_script_world_get_default ();
+
   g_signal_connect (extension->script_world,
                     "window-object-cleared",
                     G_CALLBACK (window_object_cleared_cb),
@@ -793,4 +820,12 @@ ephy_web_process_extension_initialize (EphyWebProcessExtension *extension,
 
   extension->frames_map = g_hash_table_new_full (g_int64_hash, g_int64_equal,
                                                  g_free, NULL);
+
+  extension->translation_table = g_hash_table_new (g_str_hash, NULL);
+}
+
+void
+ephy_web_process_extension_deinitialize (EphyWebProcessExtension *extension)
+{
+  g_clear_pointer (&extension->translation_table, g_hash_table_destroy);
 }

commit d811ecf9ea197460cfd01df9f3ad0da25ce64d94

Fix entry icon spacing and margins
This is a fallout from 5410fec78bc699b593d327e6a98b105f6cef7cd5

Move from GtkButtonBox back to GtkBox, hide the extension page actions box
when empty. Add entry_icon style class to icons rather than their event
boxes.

lib/widgets/ephy-location-entry.c
@@ -57,7 +57,6 @@ struct _EphyLocationEntry {
   GtkOverlay parent_instance;
 
   GtkWidget *url_entry;
-  GtkWidget *button_box;
   GtkWidget *page_action_box;
   GtkWidget *bookmark;
   GtkWidget *bookmark_event_box;
@@ -992,6 +991,7 @@ static void
 ephy_location_entry_construct_contents (EphyLocationEntry *entry)
 {
   GtkWidget *event;
+  GtkWidget *box;
   GtkStyleContext *context;
   DzlShortcutController *controller;
 
@@ -1031,26 +1031,18 @@ ephy_location_entry_construct_contents (EphyLocationEntry *entry)
   gtk_overlay_add_overlay (GTK_OVERLAY (entry), event);
 
   /* Button Box */
-  entry->button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add (GTK_CONTAINER (event), entry->button_box);
-  gtk_box_set_homogeneous (GTK_BOX (entry->button_box), FALSE);
-  g_signal_connect (G_OBJECT (entry->button_box), "size-allocate", G_CALLBACK (button_box_size_allocated_cb), entry);
-  gtk_button_box_set_layout (GTK_BUTTON_BOX (entry->button_box), GTK_BUTTONBOX_EXPAND);
-  gtk_widget_set_valign (entry->button_box, GTK_ALIGN_CENTER);
-  gtk_widget_set_halign (entry->button_box, GTK_ALIGN_END);
-  gtk_widget_set_margin_end (entry->button_box, 5);
-  gtk_widget_show (entry->button_box);
+  box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+  gtk_container_add (GTK_CONTAINER (event), box);
+  g_signal_connect (G_OBJECT (box), "size-allocate", G_CALLBACK (button_box_size_allocated_cb), entry);
+  gtk_widget_set_valign (box, GTK_ALIGN_CENTER);
+  gtk_widget_set_halign (box, GTK_ALIGN_END);
+  gtk_widget_show (box);
 
   /* Page action box */
-  entry->page_action_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-  gtk_box_set_homogeneous (GTK_BOX (entry->page_action_box), FALSE);
-  gtk_widget_show (entry->page_action_box);
-  gtk_button_box_set_layout (GTK_BUTTON_BOX (entry->page_action_box), GTK_BUTTONBOX_EXPAND);
-  gtk_widget_set_valign (entry->page_action_box, GTK_ALIGN_CENTER);
-  gtk_widget_set_halign (entry->page_action_box, GTK_ALIGN_END);
-  gtk_box_pack_start (GTK_BOX (entry->button_box), entry->page_action_box, FALSE, FALSE, 0);
+  entry->page_action_box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+  gtk_box_pack_start (GTK_BOX (box), entry->page_action_box, FALSE, FALSE, 0);
 
-  context = gtk_widget_get_style_context (entry->button_box);
+  context = gtk_widget_get_style_context (box);
   gtk_style_context_add_class (context, "entry_icon_box");
 
   /* Bookmark */
@@ -1061,7 +1053,7 @@ ephy_location_entry_construct_contents (EphyLocationEntry *entry)
   gtk_widget_show (entry->bookmark);
   g_signal_connect (G_OBJECT (entry->bookmark_event_box), "button_press_event", G_CALLBACK (bookmark_icon_button_press_event_cb), entry);
   gtk_container_add (GTK_CONTAINER (entry->bookmark_event_box), entry->bookmark);
-  gtk_box_pack_end (GTK_BOX (entry->button_box), entry->bookmark_event_box, FALSE, FALSE, 6);
+  gtk_box_pack_end (GTK_BOX (box), entry->bookmark_event_box, FALSE, FALSE, 0);
 
   context = gtk_widget_get_style_context (entry->bookmark);
   gtk_style_context_add_class (context, "entry_icon");
@@ -1079,7 +1071,7 @@ ephy_location_entry_construct_contents (EphyLocationEntry *entry)
   gtk_widget_set_valign (entry->reader_mode, GTK_ALIGN_CENTER);
   gtk_widget_show (entry->reader_mode);
   gtk_container_add (GTK_CONTAINER (entry->reader_mode_event_box), entry->reader_mode);
-  gtk_box_pack_end (GTK_BOX (entry->button_box), entry->reader_mode_event_box, FALSE, FALSE, 6);
+  gtk_box_pack_end (GTK_BOX (box), entry->reader_mode_event_box, FALSE, FALSE, 0);
 
   context = gtk_widget_get_style_context (entry->reader_mode);
   gtk_style_context_add_class (context, "entry_icon");
@@ -1523,24 +1515,16 @@ void
 ephy_location_entry_page_action_add (EphyLocationEntry *entry,
                                      GtkWidget         *action)
 {
-  GtkStyleContext *context;
+  gtk_box_pack_end (GTK_BOX (entry->page_action_box), action, FALSE, FALSE, 0);
 
-  context = gtk_widget_get_style_context (action);
-  gtk_style_context_add_class (context, "entry_icon");
-
-  gtk_box_pack_end (GTK_BOX (entry->page_action_box), action, FALSE, FALSE, 6);
+  gtk_widget_show (entry->page_action_box);
 }
 
-static
-void clear_page_actions (GtkWidget *child,
-                         gpointer   user_data)
+static void
+clear_page_actions (GtkWidget *child,
+                    gpointer   user_data)
 {
   EphyLocationEntry *entry = EPHY_LOCATION_ENTRY (user_data);
-  GtkStyleContext *context;
-
-  context = gtk_widget_get_style_context (child);
-
-  gtk_style_context_remove_class (context, "entry_icon");
 
   gtk_container_remove (GTK_CONTAINER (entry->page_action_box), child);
 }
@@ -1549,4 +1533,6 @@ void
 ephy_location_entry_page_action_clear (EphyLocationEntry *entry)
 {
   gtk_container_foreach (GTK_CONTAINER (entry->page_action_box), clear_page_actions, entry);
+
+  gtk_widget_hide (entry->page_action_box);
 }

src/webextension/ephy-web-extension-manager.c
@@ -372,6 +372,7 @@ create_page_action_widget (EphyWebExtensionManager *self,
 {
   GtkWidget *image;
   GtkWidget *event_box;
+  GtkStyleContext *context;
 
   /* Create new event box with page action */
   event_box = gtk_event_box_new ();
@@ -379,6 +380,9 @@ create_page_action_widget (EphyWebExtensionManager *self,
   gtk_container_add (GTK_CONTAINER (event_box), image);
   g_signal_connect_object (event_box, "button_press_event", G_CALLBACK (page_action_clicked), web_extension, 0);
   gtk_widget_show_all (event_box);
+
+  context = gtk_widget_get_style_context (image);
+  gtk_style_context_add_class (context, "entry_icon");
 
   return g_object_ref (event_box);
 }