16aa45ebec0e53d556cb06b8edbd1467580d8a8b
Alexander Mikhaylenko <alexm@gnome.org>
2021-02-18 13:53:49 +0500

    window-commands: Hide webapp shortcuts instead of destroying
    Avoid a use-after-free.
    
    Fixes https://gitlab.gnome.org/GNOME/epiphany/-/issues/1452

--- src/window-commands.c
+++ src/window-commands.c
@@ -917,7 +917,7 @@ window_cmd_show_shortcuts (GSimpleAction *action,
     shortcuts_window = GTK_WIDGET (gtk_builder_get_object (builder, "shortcuts-dialog"));
 
     if (ephy_is_running_inside_flatpak ())
-      gtk_widget_destroy (GTK_WIDGET (gtk_builder_get_object (builder, "shortcuts-web-apps-group")));
+      gtk_widget_hide (GTK_WIDGET (gtk_builder_get_object (builder, "shortcuts-web-apps-group")));
 
     if (gtk_widget_get_default_direction () == GTK_TEXT_DIR_RTL) {
       GtkShortcutsShortcut *shortcut;
