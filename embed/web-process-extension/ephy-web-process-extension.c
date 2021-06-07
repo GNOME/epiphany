a0bf5da72e8c52ad8631ab6e4db397d810e4e8a5
Michael Catanzaro <mcatanzaro@redhat.com>
2021-06-07 06:17:51 -0500

    Remove workaround for window-object-cleared bug
    Apparently this code is a workaround for
    https://bugs.webkit.org/show_bug.cgi?id=221771, which has been fixed, so
    we should no longer need it anymore.
    
    Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/973>

--- embed/web-process-extension/ephy-web-process-extension.c
+++ embed/web-process-extension/ephy-web-process-extension.c
@@ -231,10 +231,6 @@ ephy_web_process_extension_page_created_cb (EphyWebProcessExtension *extension,
 {
   g_autoptr (JSCContext) js_context = NULL;
 
-  /* Enforce the creation of the script world global context in the main frame */
-  js_context = webkit_frame_get_js_context_for_script_world (webkit_web_page_get_main_frame (web_page), extension->script_world);
-  (void)js_context;
-
   g_signal_connect (web_page, "context-menu",
                     G_CALLBACK (web_page_context_menu),
                     extension);
