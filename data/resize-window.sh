6cff8a6940d768f9bf1413ec15c0d26a6300f27f
Michael Catanzaro <mcatanzaro@redhat.com>
2022-03-09 15:20:46 -0600

    Remove resize-window script
    This script for taking screenshots is X11-specific and doesn't belong in
    2022.
    
    Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1087>

--- data/resize-window.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Aim for a size of 1280x720, but wmctrl doesn't seem to take into account
-# the size of the window borders, 52x52. Note: must be run under X11.
-wmctrl -x -r epiphany.Epiphany -e 0,-1,-1,1332,772
-wmctrl -x -a epiphany.Epiphany
