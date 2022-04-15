GNOME/epiphany

commit 232c613472b38ff0d0d97338f366024ddb9cd228

Simplify ephy_string_shorten()
It's too much effort to add an ellipsis character.

lib/ephy-string.c
@@ -29,8 +29,6 @@
 #include <string.h>
 #include <sys/types.h>
 
-#define ELLIPSIS "\xe2\x80\xa6"
-
 gboolean
 ephy_string_to_int (const char *string, gulong *integer)
 {
@@ -116,10 +114,11 @@ ephy_string_shorten (char *str,
   /* create string */
   bytes = GPOINTER_TO_UINT (g_utf8_offset_to_pointer (str, target_length - 1) - str);
 
-  new_str = g_new (gchar, bytes + strlen (ELLIPSIS) + 1);
+  /* +1 for ellipsis, +1 for trailing NUL */
+  new_str = g_new (gchar, bytes + 1 + 1);
 
   strncpy (new_str, str, bytes);
-  strncpy (new_str + bytes, ELLIPSIS, strlen (ELLIPSIS) + 1);
+  strcat (new_str, "…");
 
   g_free (str);
 

commit 486da133569ebfc436c959a7419565ab102e8525

Fix memory corruption in ephy_string_shorten()
This fixes a regression that I introduced in 232c613472b38ff0d0d97338f366024ddb9cd228.

I got my browser stuck in a crash loop today while visiting a website
with a page title greater than ephy-embed.c's MAX_TITLE_LENGTH, the only
condition in which ephy_string_shorten() is ever used. Turns out this
commit is wrong: an ellipses is a multibyte character (three bytes in
UTF-8) and so we're writing past the end of the buffer when calling
strcat() here. Ooops.

Shame it took nearly four years to notice and correct this.

Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1106>

lib/ephy-string.c
@@ -114,11 +114,10 @@ ephy_string_shorten (char  *str,
   /* create string */
   bytes = GPOINTER_TO_UINT (g_utf8_offset_to_pointer (str, target_length - 1) - str);
 
-  /* +1 for ellipsis, +1 for trailing NUL */
-  new_str = g_new (gchar, bytes + 1 + 1);
+  new_str = g_new (gchar, bytes + strlen ("…") + 1);
 
   strncpy (new_str, str, bytes);
-  strcat (new_str, "…");
+  strncpy (new_str + bytes, "…", strlen ("…") + 1);
 
   g_free (str);
 