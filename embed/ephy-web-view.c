6a3699b463e2d98afd0cc061bc5eeb181367c8da
Michael Catanzaro <mcatanzaro@redhat.com>
2023-05-01 16:19:33 -0500

    Fix handling of website data access permission request
    Oops, the storage access permission requests were always a bit special
    because we don't look up permission from the permission store.
    Previously we always opened an info bar here, but that got removed and
    replaced with nothing by mistake.
    
    Fixes #2065
    
    Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1327>

--- embed/ephy-web-view.c
+++ embed/ephy-web-view.c
@@ -946,6 +946,8 @@ permission_request_cb (WebKitWebView           *web_view,
     permission_type = EPHY_PERMISSION_TYPE_SHOW_NOTIFICATIONS;
   } else if (WEBKIT_IS_CLIPBOARD_PERMISSION_REQUEST (decision)) {
     permission_type = EPHY_PERMISSION_TYPE_CLIPBOARD;
+  } else if (WEBKIT_IS_WEBSITE_DATA_ACCESS_PERMISSION_REQUEST (decision)) {
+    permission_type = EPHY_PERMISSION_TYPE_COOKIES;
   } else if (WEBKIT_IS_USER_MEDIA_PERMISSION_REQUEST (decision)) {
     gboolean is_for_audio_device = webkit_user_media_permission_is_for_audio_device (WEBKIT_USER_MEDIA_PERMISSION_REQUEST (decision));
     gboolean is_for_video_device = webkit_user_media_permission_is_for_video_device (WEBKIT_USER_MEDIA_PERMISSION_REQUEST (decision));
@@ -960,8 +962,6 @@ permission_request_cb (WebKitWebView           *web_view,
     } else {
       return FALSE;
     }
-  } else if (WEBKIT_IS_WEBSITE_DATA_ACCESS_PERMISSION_REQUEST (decision)) {
-    return TRUE;
   } else {
     return FALSE;
   }
