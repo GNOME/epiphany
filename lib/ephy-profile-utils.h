GNOME/epiphany

commit 4cfb37b4a6690503cf4f2264ed602a582b73668a

Remove Safe Browsing support
This is unfortunate, but this feature has been disabled everywhere
except Epiphany Tech Preview due to changes in Google's terms of
service. We have to keep Google API keys secret now, and it's just not
possible unless build secrets are allowed. We can do that in Tech
Preview, but Linux distros cannot do this. Keeping the feature around
only for ourselves and not for normal users is just not very useful.

Closes #1393
Closes #1334
Closes #1699
Closes #1721

Related: #682

lib/ephy-profile-utils.h
@@ -33,8 +33,6 @@ G_BEGIN_DECLS
 
 #define EPHY_BOOKMARKS_FILE     "bookmarks.gvdb"
 #define EPHY_HISTORY_FILE       "ephy-history.db"
-/* Threat list database for Google Safe Browsing. */
-#define EPHY_GSB_FILE           "gsb-threats.db"
 
 int ephy_profile_utils_get_migration_version (void);
 int ephy_profile_utils_get_migration_version_for_profile_dir (const char *profile_directory);

.gitlab-ci.yml
@@ -89,13 +89,13 @@ eslint:
   rules:
     - if: $CI_COMMIT_BRANCH =~ /gnome-\d\d+/
       variables:
-        CONFIG_OPTS: '-Dprofile=Devel -Dunit_tests=enabled -Dgsb_api_key=${GSB_API_KEY} -Dgranite=enabled'
+        CONFIG_OPTS: '-Dprofile=Devel -Dunit_tests=enabled -Dgranite=enabled'
     - if: $CI_COMMIT_BRANCH == "master"
       variables:
-        CONFIG_OPTS: '-Dtech_preview=true -Dprofile=Devel -Dunit_tests=enabled -Dgsb_api_key=${GSB_API_KEY} -Dgranite=enabled'
+        CONFIG_OPTS: '-Dtech_preview=true -Dprofile=Devel -Dunit_tests=enabled -Dgranite=enabled'
     - if: $CI_COMMIT_BRANCH !~ /gnome-\d\d+/ && $CI_COMMIT_BRANCH != "master"
       variables:
-        CONFIG_OPTS: '-Dprofile=Devel -Dunit_tests=enabled -Dgsb_api_key=${GSB_API_KEY} -Dgranite=enabled --werror'
+        CONFIG_OPTS: '-Dprofile=Devel -Dunit_tests=enabled -Dgranite=enabled --werror'
 
 flatpak:
   extends:

data/org.gnome.epiphany.gschema.xml
@@ -215,11 +215,6 @@
 			<summary>Enable site-specific quirks</summary>
 			<description>Enable quirks to make specific websites work better. You might want to disable this setting if debugging a specific issue.</description>
 		</key>
-		<key type="b" name="enable-safe-browsing">
-			<default>true</default>
-			<summary>Enable safe browsing</summary>
-			<description>Whether to enable safe browsing. Safe browsing operates via Google Safe Browsing API v4.</description>
-		</key>
 		<key type="b" name="enable-itp">
 			<default>true</default>
 			<summary>Enable Intelligent Tracking Prevention (ITP)</summary>

embed/ephy-embed-shell.c
@@ -55,7 +55,6 @@ typedef struct {
   WebKitWebContext *web_context;
   WebKitNetworkSession *network_session;
   EphyHistoryService *global_history_service;
-  EphyGSBService *global_gsb_service;
   EphyEncodings *encodings;
   GtkPageSetup *page_setup;
   GtkPrintSettings *print_settings;
@@ -76,7 +75,6 @@ enum {
   RESTORED_WINDOW,
   WEB_VIEW_CREATED,
   ALLOW_TLS_CERTIFICATE,
-  ALLOW_UNSAFE_BROWSING,
   RELOAD_PAGE,
   PASSWORD_FORM_FOCUSED,
   PASSWORD_FORM_SUBMITTED,
@@ -192,7 +190,6 @@ ephy_embed_shell_dispose (GObject *object)
   g_clear_object (&priv->page_setup);
   g_clear_object (&priv->print_settings);
   g_clear_object (&priv->global_history_service);
-  g_clear_object (&priv->global_gsb_service);
   g_clear_object (&priv->about_handler);
   g_clear_object (&priv->reader_handler);
   g_clear_object (&priv->source_handler);
@@ -319,17 +316,6 @@ web_process_extension_reload_page_message_received_cb (WebKitUserContentManager 
 
   page_id = jsc_value_to_double (message);
   g_signal_emit (shell, signals[RELOAD_PAGE], 0, page_id);
-}
-
-static void
-web_process_extension_unsafe_browsing_error_page_message_received_cb (WebKitUserContentManager *manager,
-                                                                      JSCValue                 *message,
-                                                                      EphyEmbedShell           *shell)
-{
-  guint64 page_id;
-
-  page_id = jsc_value_to_double (message);
-  g_signal_emit (shell, signals[ALLOW_UNSAFE_BROWSING], 0, page_id);
 }
 
 static void
@@ -592,30 +578,6 @@ ephy_embed_shell_get_global_history_service (EphyEmbedShell *shell)
   }
 
   return priv->global_history_service;
-}
-
-/**
- * ephy_embed_shell_get_global_gsb_service:
- * @shell: the #EphyEmbedShell
- *
- * Return value: (transfer none): the global #EphyGSBService
- **/
-EphyGSBService *
-ephy_embed_shell_get_global_gsb_service (EphyEmbedShell *shell)
-{
-  EphyEmbedShellPrivate *priv = ephy_embed_shell_get_instance_private (shell);
-
-  g_assert (EPHY_IS_EMBED_SHELL (shell));
-
-  if (!priv->global_gsb_service) {
-    g_autofree char *db_path = NULL;
-    g_autofree char *default_cache_dir = ephy_default_cache_dir ();
-
-    db_path = g_build_filename (default_cache_dir, EPHY_GSB_FILE, NULL);
-    priv->global_gsb_service = ephy_gsb_service_new (GSB_API_KEY, db_path);
-  }
-
-  return priv->global_gsb_service;
 }
 
 /**
@@ -1065,22 +1027,6 @@ ephy_embed_shell_class_init (EphyEmbedShellClass *klass)
                   G_TYPE_UINT64);
 
   /**
-   * EphyEmbedShell::allow-unsafe-browsing:
-   * @shell: the #EphyEmbedShell
-   * @page_id: the identifier of the web page
-   *
-   * Emitted when the web process extension requests an exception be
-   * permitted for the unsafe browsing warning on the given page
-   */
-  signals[ALLOW_UNSAFE_BROWSING] =
-    g_signal_new ("allow-unsafe-browsing",
-                  EPHY_TYPE_EMBED_SHELL,
-                  G_SIGNAL_RUN_FIRST,
-                  0, NULL, NULL, NULL,
-                  G_TYPE_NONE, 1,
-                  G_TYPE_UINT64);
-
-  /**
    * EphyEmbedShell::reload-page:
    * @shell: the #EphyEmbedShell
    * @page_id: the identifier of the web page
@@ -1378,13 +1324,6 @@ ephy_embed_shell_register_ucm_handler (EphyEmbedShell           *shell,
                            shell, 0);
 
   webkit_user_content_manager_register_script_message_handler (ucm,
-                                                               "unsafeBrowsingErrorPage",
-                                                               NULL);
-  g_signal_connect_object (ucm, "script-message-received::unsafeBrowsingErrorPage",
-                           G_CALLBACK (web_process_extension_unsafe_browsing_error_page_message_received_cb),
-                           shell, 0);
-
-  webkit_user_content_manager_register_script_message_handler (ucm,
                                                                "passwordFormFocused",
                                                                priv->guid);
   g_signal_connect_object (ucm, "script-message-received::passwordFormFocused",
@@ -1442,9 +1381,6 @@ ephy_embed_shell_unregister_ucm_handler (EphyEmbedShell           *shell,
                                                                  priv->guid);
   webkit_user_content_manager_unregister_script_message_handler (ucm,
                                                                  "tlsErrorPage",
-                                                                 NULL);
-  webkit_user_content_manager_unregister_script_message_handler (ucm,
-                                                                 "unsafeBrowsingErrorPage",
                                                                  NULL);
   webkit_user_content_manager_unregister_script_message_handler (ucm,
                                                                  "passwordManagerRequestSave",

embed/ephy-embed-shell.h
@@ -26,7 +26,6 @@
 
 #include "ephy-downloads-manager.h"
 #include "ephy-encodings.h"
-#include "ephy-gsb-service.h"
 #include "ephy-history-service.h"
 #include "ephy-password-manager.h"
 #include "ephy-permissions-manager.h"
@@ -65,7 +64,6 @@ WebKitWebContext  *ephy_embed_shell_get_web_context            (EphyEmbedShell  
 WebKitNetworkSession *ephy_embed_shell_get_network_session     (EphyEmbedShell   *shell);
 EphyHistoryService
                   *ephy_embed_shell_get_global_history_service (EphyEmbedShell   *shell);
-EphyGSBService    *ephy_embed_shell_get_global_gsb_service     (EphyEmbedShell   *shell);
 EphyEncodings     *ephy_embed_shell_get_encodings              (EphyEmbedShell   *shell);
 void               ephy_embed_shell_restored_window            (EphyEmbedShell   *shell);
 void               ephy_embed_shell_set_page_setup             (EphyEmbedShell   *shell,

embed/ephy-web-view.c
@@ -34,7 +34,6 @@
 #include "ephy-file-helpers.h"
 #include "ephy-file-monitor.h"
 #include "ephy-filters-manager.h"
-#include "ephy-gsb-utils.h"
 #include "ephy-history-service.h"
 #include "ephy-lib-type-builtins.h"
 #include "ephy-output-encoding.h"
@@ -116,7 +115,6 @@ struct _EphyWebView {
   GTlsCertificate *certificate;
   GTlsCertificateFlags tls_errors;
 
-  gboolean bypass_safe_browsing;
   gboolean loading_error_page;
   char *tls_error_failing_uri;
 
@@ -589,18 +587,6 @@ allow_tls_certificate_cb (EphyEmbedShell *shell,
   webkit_network_session_allow_tls_certificate_for_host (ephy_embed_shell_get_network_session (shell),
                                                          view->certificate,
                                                          g_uri_get_host (uri));
-  ephy_web_view_load_url (view, ephy_web_view_get_address (view));
-}
-
-static void
-allow_unsafe_browsing_cb (EphyEmbedShell *shell,
-                          guint64         page_id,
-                          EphyWebView    *view)
-{
-  if (webkit_web_view_get_page_id (WEBKIT_WEB_VIEW (view)) != page_id)
-    return;
-
-  ephy_web_view_set_should_bypass_safe_browsing (view, TRUE);
   ephy_web_view_load_url (view, ephy_web_view_get_address (view));
 }
 
@@ -1737,94 +1723,6 @@ format_tls_error_page (EphyWebView  *view,
 }
 
 static void
-format_unsafe_browsing_error_page (EphyWebView  *view,
-                                   const char   *origin,
-                                   const char   *threat_type,
-                                   char        **page_title,
-                                   char        **message_title,
-                                   char        **message_body,
-                                   char        **message_details,
-                                   char        **button_label,
-                                   char        **button_action,
-                                   const char  **button_accesskey,
-                                   char        **hidden_button_label,
-                                   char        **hidden_button_action,
-                                   const char  **hidden_button_accesskey,
-                                   const char  **icon_name,
-                                   const char  **style)
-{
-  g_autofree char *encoded_origin = NULL;
-  g_autofree char *formatted_origin = NULL;
-  g_autofree char *first_paragraph = NULL;
-
-  /* Page title when a site is flagged by Google Safe Browsing verification. */
-  *page_title = g_strdup_printf (_("Security Warning"));
-
-  /* Message title on the unsafe browsing error page. */
-  *message_title = g_strdup (_("Unsafe website detected!"));
-
-  encoded_origin = ephy_encode_for_html_entity (origin);
-  formatted_origin = g_strdup_printf ("<strong>%s</strong>", encoded_origin);
-  /* Error details on the unsafe browsing error page.
-   * https://developers.google.com/safe-browsing/v4/usage-limits#UserWarnings
-   */
-  if (!g_strcmp0 (threat_type, GSB_THREAT_TYPE_MALWARE)) {
-    first_paragraph = g_strdup_printf (_("Visiting %s may harm your computer. This "
-                                         "page appears to contain malicious code that could "
-                                         "be downloaded to your computer without your consent."),
-                                       formatted_origin);
-    *message_details = g_strdup_printf (_("You can learn more about harmful web content "
-                                          "including viruses and other malicious code "
-                                          "and how to protect your computer at %s."),
-                                        "<a href=\"https://www.stopbadware.org/\">"
-                                        "www.stopbadware.org"
-                                        "</a>");
-  } else if (!g_strcmp0 (threat_type, GSB_THREAT_TYPE_SOCIAL_ENGINEERING)) {
-    first_paragraph = g_strdup_printf (_("Attackers on %s may trick you into doing "
-                                         "something dangerous like installing software or "
-                                         "revealing your personal information (for example, "
-                                         "passwords, phone numbers, or credit cards)."),
-                                       formatted_origin);
-    *message_details = g_strdup_printf (_("You can find out more about social engineering "
-                                          "(phishing) at %s or from %s."),
-                                        "<a href=\"https://support.google.com/webmasters/answer/6350487\">"
-                                        "Social Engineering (Phishing and Deceptive Sites)"
-                                        "</a>",
-                                        "<a href=\"https://www.antiphishing.org/\">"
-                                        "www.antiphishing.org"
-                                        "</a>");
-  } else {
-    first_paragraph = g_strdup_printf (_("%s may contain harmful programs. Attackers might "
-                                         "attempt to trick you into installing programs that "
-                                         "harm your browsing experience (for example, by changing "
-                                         "your homepage or showing extra ads on sites you visit)."),
-                                       formatted_origin);
-    *message_details = g_strdup_printf (_("You can learn more about unwanted software at %s."),
-                                        "<a href=\"https://www.google.com/about/unwanted-software-policy.html\">"
-                                        "Unwanted Software Policy"
-                                        "</a>");
-  }
-
-  *message_body = g_strdup_printf ("<p>%s</p>", first_paragraph);
-
-  /* The button on unsafe browsing error page. DO NOT ADD MNEMONICS HERE. */
-  *button_label = g_strdup (_("Go Back"));
-  *button_action = g_strdup ("window.history.back();");
-  /* Mnemonic for the Go Back button on the unsafe browsing error page. */
-  *button_accesskey = C_("back-access-key", "B");
-
-  /* The hidden button on the unsafe browsing error page. Do not add mnemonics here. */
-  *hidden_button_label = g_strdup (_("Accept Risk and Proceed"));
-  *hidden_button_action = g_strdup_printf ("window.webkit.messageHandlers.unsafeBrowsingErrorPage.postMessage(%"G_GUINT64_FORMAT ");",
-                                           webkit_web_view_get_page_id (WEBKIT_WEB_VIEW (view)));
-  /* Mnemonic for the Accept Risk and Proceed button on the unsafe browsing error page. */
-  *hidden_button_accesskey = C_("proceed-anyway-access-key", "P");
-
-  *icon_name = "security-high-symbolic.svg";
-  *style = "danger";
-}
-
-static void
 format_no_such_file_error_page (EphyWebView  *view,
                                 char        **page_title,
                                 char        **message_title,
@@ -1992,23 +1890,6 @@ ephy_web_view_load_error_page (EphyWebView          *view,
                              &hidden_button_accesskey,
                              &icon_name,
                              &style);
-      break;
-    case EPHY_WEB_VIEW_ERROR_UNSAFE_BROWSING:
-      format_unsafe_browsing_error_page (view,
-                                         origin,
-                                         user_data,
-                                         &page_title,
-                                         &msg_title,
-                                         &msg_body,
-                                         &msg_details,
-                                         &button_label,
-                                         &button_action,
-                                         &button_accesskey,
-                                         &hidden_button_label,
-                                         &hidden_button_action,
-                                         &hidden_button_accesskey,
-                                         &icon_name,
-                                         &style);
       break;
     case EPHY_WEB_VIEW_ERROR_NO_SUCH_FILE:
       format_no_such_file_error_page (view,
@@ -2899,23 +2780,6 @@ ephy_web_view_set_typed_address (EphyWebView *view,
   g_object_notify_by_pspec (G_OBJECT (view), obj_properties[PROP_TYPED_ADDRESS]);
 }
 
-gboolean
-ephy_web_view_get_should_bypass_safe_browsing (EphyWebView *view)
-{
-  g_assert (EPHY_IS_WEB_VIEW (view));
-
-  return view->bypass_safe_browsing;
-}
-
-void
-ephy_web_view_set_should_bypass_safe_browsing (EphyWebView *view,
-                                               gboolean     bypass_safe_browsing)
-{
-  g_assert (EPHY_IS_WEB_VIEW (view));
-
-  view->bypass_safe_browsing = bypass_safe_browsing;
-}
-
 static gboolean
 has_modified_forms_timeout_cb (gpointer user_data)
 {
@@ -3745,10 +3609,6 @@ ephy_web_view_init (EphyWebView *web_view)
 
   g_signal_connect_object (shell, "allow-tls-certificate",
                            G_CALLBACK (allow_tls_certificate_cb),
-                           web_view, 0);
-
-  g_signal_connect_object (shell, "allow-unsafe-browsing",
-                           G_CALLBACK (allow_unsafe_browsing_cb),
                            web_view, 0);
 
   g_signal_connect_object (shell, "reload-page",

embed/ephy-web-view.h
@@ -72,7 +72,6 @@ typedef enum {
   EPHY_WEB_VIEW_ERROR_PROCESS_CRASH,
   EPHY_WEB_VIEW_ERROR_UNRESPONSIVE_PROCESS,
   EPHY_WEB_VIEW_ERROR_INVALID_TLS_CERTIFICATE,
-  EPHY_WEB_VIEW_ERROR_UNSAFE_BROWSING,
   EPHY_WEB_VIEW_ERROR_NO_SUCH_FILE,
 } EphyWebViewErrorPage;
 
@@ -99,9 +98,6 @@ void                       ephy_web_view_set_security_level       (EphyWebView  
 const char *               ephy_web_view_get_typed_address        (EphyWebView               *view);
 void                       ephy_web_view_set_typed_address        (EphyWebView               *view,
                                                                    const char                *address);
-gboolean            ephy_web_view_get_should_bypass_safe_browsing (EphyWebView               *view);
-void                ephy_web_view_set_should_bypass_safe_browsing (EphyWebView               *view,
-                                                                   gboolean                   bypass_safe_browsing);
 gboolean                   ephy_web_view_get_is_blank             (EphyWebView               *view);
 gboolean                   ephy_web_view_is_newtab                (EphyWebView               *view);
 gboolean                   ephy_web_view_is_overview              (EphyWebView               *view);

lib/ephy-prefs.h
@@ -94,7 +94,6 @@ static const char * const ephy_prefs_state_schema[] = {
 #define EPHY_PREFS_WEB_ENABLE_ADBLOCK               "enable-adblock"
 #define EPHY_PREFS_WEB_REMEMBER_PASSWORDS           "remember-passwords"
 #define EPHY_PREFS_WEB_ENABLE_SITE_SPECIFIC_QUIRKS  "enable-site-specific-quirks"
-#define EPHY_PREFS_WEB_ENABLE_SAFE_BROWSING         "enable-safe-browsing"
 #define EPHY_PREFS_WEB_ENABLE_ITP                   "enable-itp"
 #define EPHY_PREFS_WEB_ENABLE_WEBSITE_DATA_STORAGE  "enable-website-data-storage"
 #define EPHY_PREFS_WEB_DEFAULT_ZOOM_LEVEL           "default-zoom-level"
@@ -125,7 +124,6 @@ static const char * const ephy_prefs_web_schema[] = {
   EPHY_PREFS_WEB_ENABLE_ADBLOCK,
   EPHY_PREFS_WEB_REMEMBER_PASSWORDS,
   EPHY_PREFS_WEB_ENABLE_SITE_SPECIFIC_QUIRKS,
-  EPHY_PREFS_WEB_ENABLE_SAFE_BROWSING,
   EPHY_PREFS_WEB_ENABLE_ITP,
   EPHY_PREFS_WEB_ENABLE_WEBSITE_DATA_STORAGE,
   EPHY_PREFS_WEB_DEFAULT_ZOOM_LEVEL,

commit 646b8f74673b01f8a8e293a8531aea48d9eb1a06

Fix profile migrator crashing with clean profile
I broke the profile migrator in 4cfb37b4a6690503cf4f2264ed602a582b73668a
by failing to update the migration version as required when adding new
migrators. Oops. This causes Epiphany to crash on startup for anybody
who hasn't started it before.

Fixes #2245

Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1419>

lib/ephy-profile-utils.h
@@ -24,7 +24,7 @@
 
 G_BEGIN_DECLS
 
-#define EPHY_PROFILE_MIGRATION_VERSION 37
+#define EPHY_PROFILE_MIGRATION_VERSION 38
 #define EPHY_INSECURE_PASSWORDS_MIGRATION_VERSION 11
 #define EPHY_FIREFOX_SYNC_PASSWORDS_MIGRATION_VERSION 19
 #define EPHY_TARGET_ORIGIN_MIGRATION_VERSION 21