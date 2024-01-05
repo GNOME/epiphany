07572e3952cba02c38b5398a9563383df568738d
Michael Catanzaro <mcatanzaro@redhat.com>
2024-01-05 09:12:02 -0600

    Revert "Fix history pattern match"
    This reverts commit cfa0e1c761be99af2d4def6846f719d8818b289f.

--- lib/ephy-sqlite-statement.c
+++ lib/ephy-sqlite-statement.c
@@ -304,8 +304,7 @@ ephy_sqlite_create_match_pattern (const char *match_string)
   char *string, *pattern;
 
   string = g_strndup (match_string, EPHY_SQLITE_LIMIT_LIKE_PATTERN_LENGTH - 2);
-  /* Create a search pattern: '%string%' which will be used in sqlite search statement */
-  pattern = g_strdup_printf ("%%%s%%", string);
+  pattern = g_strdup_printf ("%%:%%%s%%", string);
   g_free (string);
 
   return pattern;
