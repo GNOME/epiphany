GNOME/epiphany

commit 78a30bac7c52a6cbf0cc28454d3ef0ad852f5e39

Don't delay internal Epiphany pages on adblocker initialization
When launching Epiphany for the first time, with no adblock filters
previously downloaded, it takes several seconds to display the overview
page. There's no good reason for this delay. We should only wait for the
adblocker on normal web pages, not internal pages.

Fixes #2267

Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1460>

src/ephy-window.c
@@ -2261,30 +2261,33 @@ decide_policy_cb (WebKitWebView            *web_view,
                   WebKitPolicyDecisionType  decision_type,
                   EphyWindow               *window)
 {
-  EphyFiltersManager *filters_manager;
-
   /* Response policy decisions are handled in EphyWebView instead. */
   if (decision_type != WEBKIT_POLICY_DECISION_TYPE_NAVIGATION_ACTION &&
       decision_type != WEBKIT_POLICY_DECISION_TYPE_NEW_WINDOW_ACTION)
     return FALSE;
 
-  filters_manager = ephy_embed_shell_get_filters_manager (ephy_embed_shell_get_default ());
-  if (!ephy_filters_manager_get_is_initialized (filters_manager)) {
-    /* Queue request while filters initialization is in progress. */
-    VerifyUrlAsyncData *async_data = verify_url_async_data_new (window,
-                                                                web_view,
-                                                                decision,
-                                                                decision_type);
-    window->pending_decisions = g_list_append (window->pending_decisions,
-                                               async_data);
-    if (!window->filters_initialized_id) {
-      window->filters_initialized_id =
-        g_signal_connect_object (filters_manager,
-                                 "notify::is-initialized",
-                                 G_CALLBACK (filters_initialized_cb),
-                                 window, 0);
+  /* We don't want to allow HTTP requests before the adblocker is ready, except
+   * for internal Epiphany pages, which should never be delayed.
+   */
+  if (!g_str_has_prefix (webkit_web_view_get_uri (web_view), "ephy-about:")) {
+    EphyFiltersManager *filters_manager = ephy_embed_shell_get_filters_manager (ephy_embed_shell_get_default ());
+    if (!ephy_filters_manager_get_is_initialized (filters_manager)) {
+      /* Queue request while filters initialization is in progress. */
+      VerifyUrlAsyncData *async_data = verify_url_async_data_new (window,
+                                                                  web_view,
+                                                                  decision,
+                                                                  decision_type);
+      window->pending_decisions = g_list_append (window->pending_decisions,
+                                                 async_data);
+      if (!window->filters_initialized_id) {
+        window->filters_initialized_id =
+          g_signal_connect_object (filters_manager,
+                                   "notify::is-initialized",
+                                   G_CALLBACK (filters_initialized_cb),
+                                   window, 0);
+      }
+      return TRUE;
     }
-    return TRUE;
   }
 
   return decide_navigation_policy (web_view, decision, decision_type, window);

commit ac40e0de9e79b49d82089fea1d89c531df3c2a8e

window: fix crash when deciding policy for view without URL
This is a regression from 78a30bac7c52a6cbf0cc28454d3ef0ad852f5e39. It's
actually expected that a web view may not have a URL yet when it's being
opened from JavaScript. We have to guard against this condition.

Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1469>

src/ephy-window.c
@@ -2261,6 +2261,8 @@ decide_policy_cb (WebKitWebView            *web_view,
                   WebKitPolicyDecisionType  decision_type,
                   EphyWindow               *window)
 {
+  const char *uri;
+
   /* Response policy decisions are handled in EphyWebView instead. */
   if (decision_type != WEBKIT_POLICY_DECISION_TYPE_NAVIGATION_ACTION &&
       decision_type != WEBKIT_POLICY_DECISION_TYPE_NEW_WINDOW_ACTION)
@@ -2269,7 +2271,8 @@ decide_policy_cb (WebKitWebView            *web_view,
   /* We don't want to allow HTTP requests before the adblocker is ready, except
    * for internal Epiphany pages, which should never be delayed.
    */
-  if (!g_str_has_prefix (webkit_web_view_get_uri (web_view), "ephy-about:")) {
+  uri = webkit_web_view_get_uri (web_view);
+  if (uri && !g_str_has_prefix (uri, "ephy-about:")) {
     EphyFiltersManager *filters_manager = ephy_embed_shell_get_filters_manager (ephy_embed_shell_get_default ());
     if (!ephy_filters_manager_get_is_initialized (filters_manager)) {
       /* Queue request while filters initialization is in progress. */