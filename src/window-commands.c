GNOME/epiphany

commit 24a80bb55e42f34db1afd24c1bb1cd745e4a16b7

Fix use of web app icon on app.standardnotes.com
If the current page's URL ends with a / then we create an icon name with
two slashes, example.com//icon.png. It seems some servers don't like
this, so let's avoid doing that.

Sort of fixes #2118, although that issue will be closed by !1454
instead.

Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1455>

src/window-commands.c
@@ -2007,11 +2007,12 @@ download_manifest_finished_cb (WebKitDownload            *download,
   icon = ephy_json_array_get_object (icons, pos);
   str = ephy_json_object_get_string (icon, "src");
 
-  if (ephy_embed_utils_address_has_web_scheme (str)) {
+  if (ephy_embed_utils_address_has_web_scheme (str))
     uri = g_strdup (str);
-  } else {
+  else if (g_str_has_suffix (uri, "/"))
+    uri = g_strdup_printf ("%s%s", data->url, str);
+  else
     uri = g_strdup_printf ("%s/%s", data->url, str);
-  }
 
   display = ephy_json_object_get_string (manifest_object, "display");
   if (g_strcmp0 (display, "standalone") == 0 || g_strcmp0 (display, "fullscreen") == 0)

commit f89dc0f0cc8c7fc7d52c259760a81a44661bd653

window-commands: fix logic error when computing icon URL
This is a regression from 24a80bb55e42f34db1afd24c1bb1cd745e4a16b7.
Oops!

Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1473>

src/window-commands.c
@@ -2027,7 +2027,7 @@ download_manifest_finished_cb (WebKitDownload            *download,
 
   if (ephy_embed_utils_address_has_web_scheme (str))
     uri = g_strdup (str);
-  else if (g_str_has_suffix (uri, "/"))
+  else if (g_str_has_suffix (data->url, "/"))
     uri = g_strdup_printf ("%s%s", data->url, str);
   else
     uri = g_strdup_printf ("%s/%s", data->url, str);