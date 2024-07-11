GNOME/epiphany

commit 2ae1feb13ba494d050e8182a48732740ebd97220

Replace AdwMessageDialog with AdwAlertDialog

embed/ephy-web-view.c
@@ -759,9 +759,9 @@ static gboolean
 unresponsive_process_timeout_cb (gpointer user_data);
 
 static void
-on_unresponsive_dialog_response (AdwMessageDialog *dialog,
-                                 const char       *response,
-                                 EphyWebView      *web_view)
+on_unresponsive_dialog_response (AdwAlertDialog *dialog,
+                                 const char     *response,
+                                 EphyWebView    *web_view)
 {
   if (!strcmp (response, "stop"))
     webkit_web_view_terminate_web_process (WEBKIT_WEB_VIEW (web_view));
@@ -784,21 +784,19 @@ unresponsive_process_timeout_cb (gpointer user_data)
     return G_SOURCE_CONTINUE;
 
   web_view->unresponsive_process_dialog =
-    GTK_WINDOW (adw_message_dialog_new (GTK_WINDOW (gtk_widget_get_root (GTK_WIDGET (web_view))),
-                                        _("Page Unresponsive"),
-                                        NULL));
+    GTK_WINDOW (adw_alert_dialog_new (_("Page Unresponsive"), NULL));
 
-  adw_message_dialog_format_body (ADW_MESSAGE_DIALOG (web_view->unresponsive_process_dialog),
-                                  _("The current page “%s” is not responding"),
-                                  ephy_web_view_get_address (web_view));
+  adw_alert_dialog_format_body (ADW_ALERT_DIALOG (web_view->unresponsive_process_dialog),
+                                _("The current page “%s” is not responding"),
+                                ephy_web_view_get_address (web_view));
 
-  adw_message_dialog_add_responses (ADW_MESSAGE_DIALOG (web_view->unresponsive_process_dialog),
-                                    "wait", _("_Wait"),
-                                    "stop", _("Force _Stop"),
-                                    NULL);
+  adw_alert_dialog_add_responses (ADW_ALERT_DIALOG (web_view->unresponsive_process_dialog),
+                                  "wait", _("_Wait"),
+                                  "stop", _("Force _Stop"),
+                                  NULL);
 
   g_signal_connect (web_view->unresponsive_process_dialog, "response", G_CALLBACK (on_unresponsive_dialog_response), web_view);
-  gtk_window_present (web_view->unresponsive_process_dialog);
+  adw_dialog_present (ADW_DIALOG (web_view->unresponsive_process_dialog), GTK_WIDGET (gtk_widget_get_root (GTK_WIDGET (web_view))));
 
   web_view->unresponsive_process_timeout_id = 0;
 

embed/ephy-client-certificate-manager.c
@@ -248,12 +248,12 @@ logged_in_cb (GObject      *obj,
 }
 
 static void
-certificate_pin_response (AdwMessageDialog *dialog,
-                          char             *response,
-                          gpointer          user_data)
+certificate_pin_response (AdwAlertDialog *dialog,
+                          char           *response,
+                          gpointer        user_data)
 {
   EphyClientCertificateManager *self = user_data;
-  GtkWidget *entry = adw_message_dialog_get_extra_child (dialog);
+  GtkWidget *entry = adw_alert_dialog_get_extra_child (dialog);
   const char *password = gtk_editable_get_text (GTK_EDITABLE (entry));
 
   if (strcmp (response, "cancel") == 0) {
@@ -276,7 +276,7 @@ session_opened_cb (GObject      *obj,
   g_autoptr (GError) error = NULL;
   g_autoptr (GList) modules = NULL;
   g_autoptr (GList) slots = NULL;
-  GtkWidget *dialog;
+  AdwDialog *dialog;
   GtkWidget *entry;
   g_autofree char *body = NULL;
   GckTokenInfo *info;
@@ -289,7 +289,7 @@ session_opened_cb (GObject      *obj,
     return;
   }
 
-  dialog = adw_message_dialog_new (GTK_WINDOW (gtk_widget_get_root (GTK_WIDGET (self->web_view))), _("PIN required"), NULL);
+  dialog = adw_alert_dialog_new (_("PIN required"), NULL);
 
   info = gck_slot_get_token_info (slot);
   body = g_strdup_printf (_("Please enter PIN for %s, to authenticate at %s:%d."),
@@ -297,38 +297,38 @@ session_opened_cb (GObject      *obj,
                           webkit_authentication_request_get_host (self->request),
                           webkit_authentication_request_get_port (self->request));
 
-  adw_message_dialog_format_body (ADW_MESSAGE_DIALOG (dialog), "%s", body);
+  adw_alert_dialog_format_body (ADW_ALERT_DIALOG (dialog), "%s", body);
 
-  adw_message_dialog_add_responses (ADW_MESSAGE_DIALOG (dialog),
-                                    "cancel", _("_Cancel"),
-                                    "login", _("_Login"),
-                                    NULL);
+  adw_alert_dialog_add_responses (ADW_ALERT_DIALOG (dialog),
+                                  "cancel", _("_Cancel"),
+                                  "login", _("_Login"),
+                                  NULL);
 
-  adw_message_dialog_set_response_appearance (ADW_MESSAGE_DIALOG (dialog), "login", ADW_RESPONSE_SUGGESTED);
+  adw_alert_dialog_set_response_appearance (ADW_ALERT_DIALOG (dialog), "login", ADW_RESPONSE_SUGGESTED);
 
-  adw_message_dialog_set_default_response (ADW_MESSAGE_DIALOG (dialog), "login");
-  adw_message_dialog_set_close_response (ADW_MESSAGE_DIALOG (dialog), "cancel");
+  adw_alert_dialog_set_default_response (ADW_ALERT_DIALOG (dialog), "login");
+  adw_alert_dialog_set_close_response (ADW_ALERT_DIALOG (dialog), "cancel");
 
   entry = adw_password_entry_row_new ();
   gtk_text_set_activates_default (GTK_TEXT (gtk_editable_get_delegate (GTK_EDITABLE (entry))), TRUE);
 
   adw_preferences_row_set_title (ADW_PREFERENCES_ROW (entry), "PIN");
-  adw_message_dialog_set_extra_child (ADW_MESSAGE_DIALOG (dialog), entry);
+  adw_alert_dialog_set_extra_child (ADW_ALERT_DIALOG (dialog), entry);
 
   g_signal_connect (dialog, "response", G_CALLBACK (certificate_pin_response), self);
 
-  gtk_window_present (GTK_WINDOW (dialog));
+  adw_dialog_present (dialog, GTK_WIDGET (gtk_widget_get_root (GTK_WIDGET (self->web_view))));
 }
 
 static void
-certificate_selection_dialog_response_cb (AdwMessageDialog *dialog,
-                                          char             *response,
-                                          gpointer          user_data)
+certificate_selection_dialog_response_cb (AdwAlertDialog *dialog,
+                                          char           *response,
+                                          gpointer        user_data)
 {
   EphyClientCertificateManager *self = user_data;
   g_autoptr (GError) error = NULL;
   GckSlot *slot = NULL;
-  GtkWidget *listbox = adw_message_dialog_get_extra_child (dialog);
+  GtkWidget *listbox = adw_alert_dialog_get_extra_child (dialog);
   GtkListBoxRow *row;
   const char *label;
 
@@ -360,7 +360,7 @@ certificate_selection_dialog_response_cb (AdwMessageDialog *dialog,
 static void
 certificate_selection_dialog (EphyClientCertificateManager *self)
 {
-  GtkWidget *dialog;
+  AdwDialog *dialog;
   GtkWidget *listbox;
   g_autofree char *body = NULL;
   const char *realm = webkit_authentication_request_get_realm (self->request);
@@ -371,7 +371,7 @@ certificate_selection_dialog (EphyClientCertificateManager *self)
     return;
   }
 
-  dialog = adw_message_dialog_new (GTK_WINDOW (gtk_widget_get_root (GTK_WIDGET (self->web_view))), _("Select certificate"), NULL);
+  dialog = adw_alert_dialog_new (_("Select certificate"), NULL);
 
   if (strlen (realm) > 0)
     body = g_strdup_printf (_("The website %s:%d requests that you provide a certificate for authentication for %s."),
@@ -383,16 +383,16 @@ certificate_selection_dialog (EphyClientCertificateManager *self)
                             webkit_authentication_request_get_host (self->request),
                             webkit_authentication_request_get_port (self->request));
 
-  adw_message_dialog_format_body (ADW_MESSAGE_DIALOG (dialog), "%s", body);
-  adw_message_dialog_add_responses (ADW_MESSAGE_DIALOG (dialog),
-                                    "cancel", _("_Cancel"),
-                                    "select", _("_Select"),
-                                    NULL);
+  adw_alert_dialog_format_body (ADW_ALERT_DIALOG (dialog), "%s", body);
+  adw_alert_dialog_add_responses (ADW_ALERT_DIALOG (dialog),
+                                  "cancel", _("_Cancel"),
+                                  "select", _("_Select"),
+                                  NULL);
 
-  adw_message_dialog_set_response_appearance (ADW_MESSAGE_DIALOG (dialog), "select", ADW_RESPONSE_SUGGESTED);
+  adw_alert_dialog_set_response_appearance (ADW_ALERT_DIALOG (dialog), "select", ADW_RESPONSE_SUGGESTED);
 
-  adw_message_dialog_set_default_response (ADW_MESSAGE_DIALOG (dialog), "select");
-  adw_message_dialog_set_close_response (ADW_MESSAGE_DIALOG (dialog), "cancel");
+  adw_alert_dialog_set_default_response (ADW_ALERT_DIALOG (dialog), "select");
+  adw_alert_dialog_set_close_response (ADW_ALERT_DIALOG (dialog), "cancel");
 
   listbox = gtk_list_box_new ();
   gtk_widget_add_css_class (listbox, "content");
@@ -407,11 +407,11 @@ certificate_selection_dialog (EphyClientCertificateManager *self)
     gtk_list_box_append (GTK_LIST_BOX (listbox), row);
   }
 
-  adw_message_dialog_set_extra_child (ADW_MESSAGE_DIALOG (dialog), listbox);
+  adw_alert_dialog_set_extra_child (ADW_ALERT_DIALOG (dialog), listbox);
 
   g_signal_connect (dialog, "response", G_CALLBACK (certificate_selection_dialog_response_cb), self);
 
-  gtk_window_present (GTK_WINDOW (dialog));
+  adw_dialog_present (dialog, GTK_WIDGET (gtk_widget_get_root (GTK_WIDGET (self->web_view))));
 }
 
 static void

embed/ephy-download.c
@@ -648,7 +648,7 @@ suggested_filename_data_free (SuggestedFilenameData *data)
 }
 
 static void
-filename_suggested_dialog_cb (AdwMessageDialog      *dialog,
+filename_suggested_dialog_cb (AdwAlertDialog        *dialog,
                               const char            *response,
                               SuggestedFilenameData *data)
 {
@@ -729,7 +729,7 @@ open_download_confirmation_dialog (EphyDownload *download,
                                    const char   *suggested_filename)
 {
   GApplication *application;
-  GtkWidget *dialog = NULL;
+  AdwDialog *dialog = NULL;
   GtkWidget *grid;
   GtkWindow *toplevel;
   GtkWidget *icon;
@@ -755,13 +755,11 @@ open_download_confirmation_dialog (EphyDownload *download,
   application = G_APPLICATION (ephy_embed_shell_get_default ());
   toplevel = gtk_application_get_active_window (GTK_APPLICATION (application));
 
-  dialog = adw_message_dialog_new (GTK_WINDOW (toplevel),
-                                   _("Download Requested"),
-                                   NULL);
-  adw_message_dialog_add_responses (ADW_MESSAGE_DIALOG (dialog),
-                                    "cancel", _("_Cancel"),
-                                    "download", _("_Download"),
-                                    NULL);
+  dialog = adw_alert_dialog_new (_("Download Requested"), NULL);
+  adw_alert_dialog_add_responses (ADW_ALERT_DIALOG (dialog),
+                                  "cancel", _("_Cancel"),
+                                  "download", _("_Download"),
+                                  NULL);
 
   webkit_download = ephy_download_get_webkit_download (download);
   response = webkit_download_get_response (webkit_download);
@@ -769,7 +767,7 @@ open_download_confirmation_dialog (EphyDownload *download,
   grid = gtk_grid_new ();
   gtk_grid_set_column_spacing (GTK_GRID (grid), 6);
   gtk_widget_set_margin_top (grid, 6);
-  adw_message_dialog_set_extra_child (ADW_MESSAGE_DIALOG (dialog), grid);
+  adw_alert_dialog_set_extra_child (ADW_ALERT_DIALOG (dialog), grid);
 
   content_length = g_format_size (webkit_uri_response_get_content_length (response));
   content_type = ephy_download_get_content_type (download);
@@ -845,7 +843,7 @@ open_download_confirmation_dialog (EphyDownload *download,
   g_signal_connect (dialog, "response",
                     G_CALLBACK (filename_suggested_dialog_cb), data);
 
-  gtk_window_present (GTK_WINDOW (dialog));
+  adw_dialog_present (dialog, GTK_WIDGET (toplevel));
 }
 
 static void

commit 2b36bdf7891719a3dd3a0e75af193268aa997e81

Fix the unresponsive web process dialog
Unfortunately I broke this dialog in
2ae1feb13ba494d050e8182a48732740ebd97220. Simplify the code and remove
incorrect casts to GtkWindow.

Fixes #2386

Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1530>

embed/ephy-web-view.c
@@ -125,7 +125,6 @@ struct _EphyWebView {
   gboolean just_registered_message_handlers;
 
   guint unresponsive_process_timeout_id;
-  GtkWindow *unresponsive_process_dialog;
 
   guint64 uid;
 
@@ -771,32 +770,30 @@ on_unresponsive_dialog_response (AdwAlertDialog *dialog,
                                                                             (GSourceFunc)unresponsive_process_timeout_cb,
                                                                             web_view,
                                                                             NULL);
-
-  web_view->unresponsive_process_dialog = NULL;
 }
 
 static gboolean
 unresponsive_process_timeout_cb (gpointer user_data)
 {
   EphyWebView *web_view = EPHY_WEB_VIEW (user_data);
+  AdwDialog *dialog;
 
   if (!gtk_widget_get_mapped (GTK_WIDGET (web_view)))
     return G_SOURCE_CONTINUE;
 
-  web_view->unresponsive_process_dialog =
-    GTK_WINDOW (adw_alert_dialog_new (_("Page Unresponsive"), NULL));
+  dialog = adw_alert_dialog_new (_("Page Unresponsive"), NULL);
 
-  adw_alert_dialog_format_body (ADW_ALERT_DIALOG (web_view->unresponsive_process_dialog),
+  adw_alert_dialog_format_body (ADW_ALERT_DIALOG (dialog),
                                 _("The current page “%s” is not responding"),
                                 ephy_web_view_get_address (web_view));
 
-  adw_alert_dialog_add_responses (ADW_ALERT_DIALOG (web_view->unresponsive_process_dialog),
+  adw_alert_dialog_add_responses (ADW_ALERT_DIALOG (dialog),
                                   "wait", _("_Wait"),
                                   "stop", _("Force _Stop"),
                                   NULL);
 
-  g_signal_connect (web_view->unresponsive_process_dialog, "response", G_CALLBACK (on_unresponsive_dialog_response), web_view);
-  adw_dialog_present (ADW_DIALOG (web_view->unresponsive_process_dialog), GTK_WIDGET (gtk_widget_get_root (GTK_WIDGET (web_view))));
+  g_signal_connect_object (dialog, "response", G_CALLBACK (on_unresponsive_dialog_response), web_view, 0);
+  adw_dialog_present (ADW_DIALOG (dialog), GTK_WIDGET (gtk_widget_get_root (GTK_WIDGET (web_view))));
 
   web_view->unresponsive_process_timeout_id = 0;
 
@@ -808,16 +805,9 @@ is_web_process_responsive_changed_cb (EphyWebView *web_view,
                                       GParamSpec  *pspec,
                                       gpointer     user_data)
 {
-  gboolean responsive = webkit_web_view_get_is_web_process_responsive (WEBKIT_WEB_VIEW (web_view));
-
   g_clear_handle_id (&web_view->unresponsive_process_timeout_id, g_source_remove);
 
-  if (web_view->unresponsive_process_dialog && responsive) {
-    g_signal_handlers_disconnect_by_func (web_view->unresponsive_process_dialog, on_unresponsive_dialog_response, web_view);
-    g_clear_pointer (&web_view->unresponsive_process_dialog, gtk_window_destroy);
-  }
-
-  if (!responsive) {
+  if (!webkit_web_view_get_is_web_process_responsive (WEBKIT_WEB_VIEW (web_view))) {
     web_view->unresponsive_process_timeout_id = g_timeout_add_seconds_full (G_PRIORITY_HIGH,
                                                                             10,
                                                                             (GSourceFunc)unresponsive_process_timeout_cb,
@@ -3566,7 +3556,6 @@ ephy_web_view_dispose (GObject *object)
   g_clear_object (&view->certificate);
   g_clear_object (&view->file_monitor);
   g_clear_object (&view->icon);
-  g_clear_pointer (&view->unresponsive_process_dialog, gtk_window_destroy);
 
   if (view->cancellable) {
     g_cancellable_cancel (view->cancellable);