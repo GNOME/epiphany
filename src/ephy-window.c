3a6cc4c434a3759a28a007b21082ca207e909b52
Michael Catanzaro <mcatanzaro@redhat.com>
2024-10-09 10:52:04 -0500

    Never open unsupported URLs
    6c223bb8b3e75026eeecc61424e5a73649aca1f2 introduced a regression where
    we could accidentally attempt to load an unsupported URL type. This
    causes crashes when EphySession attempts to display the content.
    
    Fixes #2490
    
    (Note we have a related issue in this code, #2475, which I don't know
    how to fix.)
    
    
    (cherry picked from commit 65f5e886dbd07ffd2a89ae2633a368cb63184b3c)

--- src/ephy-window.c
+++ src/ephy-window.c
@@ -2079,10 +2079,13 @@ decide_navigation_policy (WebKitWebView            *web_view,
   request = webkit_navigation_action_get_request (navigation_action);
   uri = webkit_uri_request_get_uri (request);
 
-  if (!ephy_embed_utils_address_has_web_scheme (uri) && webkit_navigation_action_is_user_gesture (navigation_action)) {
-    g_autoptr (GtkUriLauncher) launcher = gtk_uri_launcher_new (uri);
+  if (!ephy_embed_utils_address_has_web_scheme (uri)) {
+    if (webkit_navigation_action_is_user_gesture (navigation_action)) {
+      g_autoptr (GtkUriLauncher) launcher = gtk_uri_launcher_new (uri);
 
-    gtk_uri_launcher_launch (launcher, GTK_WINDOW (window), NULL, NULL, NULL);
+      gtk_uri_launcher_launch (launcher, GTK_WINDOW (window), NULL, NULL, NULL);
+    }
+
     webkit_policy_decision_ignore (decision);
     return TRUE;
   }
