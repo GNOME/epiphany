d69b5d33d2c01910afd71743e92ace35638da389
Jamie Gravendeel <me@jamie.garden>
2025-03-19 17:18:52 +0100

    Reapply "preferences: Add toggle to always show full URL"
    This reverts commit 5e8f613c0b95077ef386db80f44d36a6765b0e49.
    
    Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1717>

--- src/ephy-location-entry.c
+++ src/ephy-location-entry.c
@@ -713,6 +713,9 @@ update_entry_style (EphyLocationEntry *self,
   color_normal->end_index = color_normal->start_index + strlen (base_domain);
   pango_attr_list_insert (attrs, color_normal);
 
+  if (g_settings_get_boolean (EPHY_SETTINGS_WEB, EPHY_PREFS_WEB_ALWAYS_SHOW_FULL_URL))
+    goto out;
+
   /* Scheme is hidden */
   start_hidden = pango_attr_size_new (0);
   start_hidden->start_index = 0;
@@ -745,8 +748,9 @@ focus_enter_cb (EphyLocationEntry *entry)
 static void
 focus_leave_cb (EphyLocationEntry *entry)
 {
-  /* Don't animate if the toplevel lost global input focus */
-  if (!gtk_widget_is_focus (GTK_WIDGET (entry->text)))
+  /* Don't animate if the toplevel lost global input focus or "Always Show Full URL" is set */
+  if (!gtk_widget_is_focus (GTK_WIDGET (entry->text)) &&
+      !g_settings_get_boolean (EPHY_SETTINGS_WEB, EPHY_PREFS_WEB_ALWAYS_SHOW_FULL_URL))
     animate_focus (entry, FALSE);
 
   ephy_location_entry_reset (entry);
@@ -1644,6 +1648,9 @@ ephy_location_entry_init (EphyLocationEntry *entry)
                    G_SETTINGS_BIND_GET | G_SETTINGS_BIND_INVERT_BOOLEAN);
 
   g_signal_connect (entry->bookmark_button, "clicked", G_CALLBACK (on_bookmark_button_clicked), entry);
+
+  if (g_settings_get_boolean (EPHY_SETTINGS_WEB, EPHY_PREFS_WEB_ALWAYS_SHOW_FULL_URL))
+    gtk_editable_set_alignment (GTK_EDITABLE (entry), 0.0);
 
   update_reader_icon (entry);
   g_signal_connect_object (gtk_settings_get_default (), "notify::gtk-icon-theme-name",
