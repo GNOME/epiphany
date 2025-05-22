GNOME/epiphany

commit 3d290ef92d4fcc2901caa5eeb9d7d9ea32f6062b

Move page menu to action bar in narrow mode
Fixes: https://gitlab.gnome.org/GNOME/epiphany/-/issues/2625
Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1823>

src/ephy-page-menu-button.c
@@ -1,0 +1,192 @@
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ *  Copyright © 2025 Jan-Michael Brummer <jan.brummer@tabos.org>
+ *
+ *  This file is part of Epiphany.
+ *
+ *  Epiphany is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Epiphany is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Epiphany.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ephy-page-menu-button.h"
+
+#include "ephy-desktop-utils.h"
+#include "ephy-embed-shell.h"
+#include "ephy-flatpak-utils.h"
+
+/* Translators: tooltip for the refresh button */
+static const char *REFRESH_BUTTON_TOOLTIP = N_("Reload the current page");
+
+struct _EphyPageMenuButton {
+  AdwBin parent_instance;
+
+  GtkWidget *menu_button;
+  GMenu *page_menu;
+  GtkWidget *button_box;
+  GtkWidget *page_menu_popover;
+  GtkWidget *zoom_level;
+  GtkWidget *combined_stop_reload_button;
+};
+
+G_DEFINE_FINAL_TYPE (EphyPageMenuButton, ephy_page_menu_button, ADW_TYPE_BIN)
+
+static gboolean
+remove_menu_item (GMenu      *menu,
+                  const char *action_name)
+{
+  int i, n;
+
+  n = g_menu_model_get_n_items (G_MENU_MODEL (menu));
+
+  for (i = 0; i < n; i++) {
+    g_autofree char *item_action = NULL;
+    g_autofree char *submenu_id = NULL;
+    g_autoptr (GMenuModel) section = NULL;
+
+    g_menu_model_get_item_attribute (G_MENU_MODEL (menu),
+                                     i,
+                                     G_MENU_ATTRIBUTE_ACTION,
+                                     "s",
+                                     &item_action);
+
+    if (!g_strcmp0 (action_name, item_action)) {
+      g_menu_remove (menu, i);
+      return TRUE;
+    }
+
+    /* FIXME: this isn't particularly great. Maybe we should have custom
+     * attributes for everything like show-in-app-mode etc? */
+    g_menu_model_get_item_attribute (G_MENU_MODEL (menu),
+                                     i,
+                                     "ephy-submenu-id",
+                                     "s",
+                                     &submenu_id);
+
+    if (!g_strcmp0 (action_name, submenu_id)) {
+      g_menu_remove (menu, i);
+      return TRUE;
+    }
+
+    section = g_menu_model_get_item_link (G_MENU_MODEL (menu),
+                                          i,
+                                          G_MENU_LINK_SECTION);
+
+    if (!G_IS_MENU (section))
+      continue;
+
+    if (remove_menu_item (G_MENU (section), action_name))
+      return TRUE;
+  }
+
+  return FALSE;
+}
+
+static void
+ephy_page_menu_button_class_init (EphyPageMenuButtonClass *klass)
+{
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/epiphany/gtk/page-menu-button.ui");
+
+  gtk_widget_class_bind_template_child (widget_class, EphyPageMenuButton, menu_button);
+  gtk_widget_class_bind_template_child (widget_class, EphyPageMenuButton, page_menu);
+  gtk_widget_class_bind_template_child (widget_class, EphyPageMenuButton, button_box);
+  gtk_widget_class_bind_template_child (widget_class, EphyPageMenuButton, page_menu_popover);
+  gtk_widget_class_bind_template_child (widget_class, EphyPageMenuButton, zoom_level);
+  gtk_widget_class_bind_template_child (widget_class, EphyPageMenuButton, combined_stop_reload_button);
+}
+
+static void
+ephy_page_menu_button_init (EphyPageMenuButton *self)
+{
+  EphyEmbedShell *embed_shell = ephy_embed_shell_get_default ();
+
+  gtk_widget_init_template (GTK_WIDGET (self));
+
+  if (ephy_embed_shell_get_mode (embed_shell) == EPHY_EMBED_SHELL_MODE_APPLICATION) {
+    remove_menu_item (self->page_menu, "app.new-incognito");
+    remove_menu_item (self->page_menu, "app.reopen-closed-tab");
+    remove_menu_item (self->page_menu, "win.save-as-application");
+    remove_menu_item (self->page_menu, "win.open-application-manager");
+    remove_menu_item (self->page_menu, "win.encoding");
+    remove_menu_item (self->page_menu, "app.shortcuts");
+    remove_menu_item (self->page_menu, "app.help");
+    remove_menu_item (self->page_menu, "app.firefox-sync-dialog");
+    remove_menu_item (self->page_menu, "import-export");
+    remove_menu_item (self->page_menu, "webapps");
+  } else if (ephy_is_running_inside_sandbox ()) {
+    remove_menu_item (self->page_menu, "app.run-in-background");
+    remove_menu_item (self->page_menu, "app.quit");
+
+    if (is_desktop_pantheon ())
+      remove_menu_item (self->page_menu, "app.help");
+  } else {
+    remove_menu_item (self->page_menu, "app.run-in-background");
+    remove_menu_item (self->page_menu, "app.quit");
+  }
+
+  if (!ephy_can_install_web_apps ()) {
+    remove_menu_item (self->page_menu, "win.save-as-application");
+    remove_menu_item (self->page_menu, "win.open-application-manager");
+  }
+
+  if (is_desktop_pantheon ()) {
+    remove_menu_item (self->page_menu, "app.about");
+
+    gtk_menu_button_set_icon_name (GTK_MENU_BUTTON (self->menu_button), "open-menu");
+    gtk_widget_add_css_class (self->menu_button, "toolbar-button");
+
+    gtk_widget_add_css_class (self->button_box, "linked");
+    gtk_box_set_spacing (GTK_BOX (self->button_box), 0);
+  }
+
+  gtk_menu_button_set_popover (GTK_MENU_BUTTON (self->menu_button), self->page_menu_popover);
+}
+
+EphyPageMenuButton *
+ephy_page_menu_button_new (void)
+{
+  return g_object_new (EPHY_TYPE_PAGE_MENU_BUTTON, NULL);
+}
+
+void
+ephy_page_menu_button_set_zoom_level (EphyPageMenuButton *self,
+                                      char               *zoom_level)
+{
+  gtk_label_set_text (GTK_LABEL (self->zoom_level), zoom_level);
+}
+
+void
+ephy_page_menu_button_change_combined_stop_reload_state (EphyPageMenuButton *self,
+                                                         gboolean            loading)
+{
+  if (loading) {
+    gtk_button_set_icon_name (GTK_BUTTON (self->combined_stop_reload_button),
+                              "process-stop-symbolic");
+    /* Translators: tooltip for the stop button */
+    gtk_widget_set_tooltip_text (self->combined_stop_reload_button,
+                                 _("Stop loading the current page"));
+  } else {
+    gtk_button_set_icon_name (GTK_BUTTON (self->combined_stop_reload_button),
+                              "view-refresh-symbolic");
+    gtk_widget_set_tooltip_text (self->combined_stop_reload_button,
+                                 _(REFRESH_BUTTON_TOOLTIP));
+  }
+}
+
+void
+ephy_page_menu_button_show_combined_stop_reload_button (EphyPageMenuButton *self,
+                                                        gboolean            show)
+{
+  gtk_widget_set_visible (self->combined_stop_reload_button, show);
+}

src/ephy-page-menu-button.h
@@ -1,0 +1,43 @@
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ *  Copyright © 2025 Jan-Michael Brummer <jan.brummer@tabos.org>
+ *
+ *  This file is part of Epiphany.
+ *
+ *  Epiphany is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Epiphany is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Epiphany.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <adwaita.h>
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define EPHY_TYPE_PAGE_MENU_BUTTON (ephy_page_menu_button_get_type())
+
+G_DECLARE_FINAL_TYPE (EphyPageMenuButton, ephy_page_menu_button, EPHY, PAGE_MENU_BUTTON, AdwBin)
+
+EphyPageMenuButton   *ephy_page_menu_button_new (void);
+
+void                  ephy_page_menu_button_set_zoom_level (EphyPageMenuButton *self,
+                                                            char               *zoom_level);
+
+void                  ephy_page_menu_button_change_combined_stop_reload_state (EphyPageMenuButton *self,
+                                                                               gboolean            loading);
+
+void                  ephy_page_menu_button_show_combined_stop_reload_button (EphyPageMenuButton *self,
+                                                                              gboolean            show);
+
+G_END_DECLS

po/POTFILES.in
@@ -52,6 +52,7 @@ src/ephy-header-bar.c
 src/ephy-history-dialog.c
 src/ephy-location-entry.c
 src/ephy-main.c
+src/ephy-page-menu-button.c
 src/ephy-passwords-view.c
 src/ephy-permission-popover.c
 src/ephy-privacy-report.c

src/ephy-action-bar.c
@@ -25,6 +25,7 @@
 #include "ephy-embed-prefs.h"
 #include "ephy-embed-utils.h"
 #include "ephy-favicon-helpers.h"
+#include "ephy-page-menu-button.h"
 #include "ephy-settings.h"
 #include "ephy-shell.h"
 #include "ephy-tab-view.h"
@@ -43,6 +44,7 @@ struct _EphyActionBar {
   EphyWindow *window;
   GtkWidget *toolbar;
   GtkWidget *bookmark_button;
+  GtkWidget *menu_button;
   AdwTabButton *tab_button;
   GtkWidget *navigation_back;
   GtkWidget *navigation_forward;
@@ -506,6 +508,7 @@ ephy_action_bar_class_init (EphyActionBarClass *klass)
   gtk_widget_class_bind_template_child (widget_class, EphyActionBar, bookmark_button);
   gtk_widget_class_bind_template_child (widget_class, EphyActionBar, navigation_back);
   gtk_widget_class_bind_template_child (widget_class, EphyActionBar, navigation_forward);
+  gtk_widget_class_bind_template_child (widget_class, EphyActionBar, menu_button);
 
   gtk_widget_class_bind_template_callback (widget_class,
                                            right_click_pressed_cb);
@@ -527,6 +530,8 @@ ephy_action_bar_init (EphyActionBar *action_bar)
   mode = ephy_embed_shell_get_mode (EPHY_EMBED_SHELL (ephy_shell_get_default ()));
   gtk_widget_set_visible (GTK_WIDGET (action_bar->tab_button),
                           mode != EPHY_EMBED_SHELL_MODE_APPLICATION);
+
+  ephy_page_menu_button_show_combined_stop_reload_button (EPHY_PAGE_MENU_BUTTON (action_bar->menu_button), TRUE);
 }
 
 EphyActionBar *
@@ -565,4 +570,20 @@ ephy_action_bar_set_bookmark_icon_state (EphyActionBar         *self,
     default:
       g_assert_not_reached ();
   }
+}
+
+void
+ephy_action_bar_set_zoom_level (EphyActionBar *self,
+                                gdouble        zoom)
+{
+  g_autofree gchar *zoom_level = g_strdup_printf ("%2.0f%%", zoom * 100);
+
+  ephy_page_menu_button_set_zoom_level (EPHY_PAGE_MENU_BUTTON (self->menu_button), zoom_level);
+}
+
+void
+ephy_action_bar_change_combined_stop_reload_state (EphyActionBar *self,
+                                                   gboolean       loading)
+{
+  ephy_page_menu_button_change_combined_stop_reload_state (EPHY_PAGE_MENU_BUTTON (self->menu_button), loading);
 }

src/ephy-action-bar.h
@@ -41,4 +41,10 @@ EphyActionBarEnd   *ephy_action_bar_get_action_bar_end   (EphyActionBar *action_
 void              ephy_action_bar_set_bookmark_icon_state (EphyActionBar         *self,
                                                            EphyBookmarkIconState  state);
 
+void               ephy_action_bar_set_zoom_level         (EphyActionBar *header_bar,
+                                                           gdouble        zoom);
+
+void               ephy_action_bar_change_combined_stop_reload_state (EphyActionBar *self,
+                                                                      gboolean       loading);
+
 G_END_DECLS

src/ephy-header-bar.c
@@ -28,6 +28,7 @@
 #include "ephy-file-helpers.h"
 #include "ephy-flatpak-utils.h"
 #include "ephy-location-entry.h"
+#include "ephy-page-menu-button.h"
 #include "ephy-settings.h"
 #include "ephy-shell.h"
 #include "ephy-title-box.h"
@@ -46,9 +47,6 @@ enum {
 };
 
 static GParamSpec *object_properties[N_PROPERTIES] = { NULL, };
-
-/* Translators: tooltip for the refresh button */
-static const char *REFRESH_BUTTON_TOOLTIP = N_("Reload the current page");
 
 struct _EphyHeaderBar {
   AdwBin parent_instance;
@@ -145,67 +143,13 @@ fullscreen_changed_cb (EphyHeaderBar *header_bar)
   }
 }
 
-static gboolean
-remove_menu_item (GMenu      *menu,
-                  const char *action_name)
-{
-  int i, n;
-
-  n = g_menu_model_get_n_items (G_MENU_MODEL (menu));
-
-  for (i = 0; i < n; i++) {
-    g_autofree char *item_action = NULL;
-    g_autofree char *submenu_id = NULL;
-    g_autoptr (GMenuModel) section = NULL;
-
-    g_menu_model_get_item_attribute (G_MENU_MODEL (menu),
-                                     i,
-                                     G_MENU_ATTRIBUTE_ACTION,
-                                     "s",
-                                     &item_action);
-
-    if (!g_strcmp0 (action_name, item_action)) {
-      g_menu_remove (menu, i);
-      return TRUE;
-    }
-
-    /* FIXME: this isn't particularly great. Maybe we should have custom
-     * attributes for everything like show-in-app-mode etc? */
-    g_menu_model_get_item_attribute (G_MENU_MODEL (menu),
-                                     i,
-                                     "ephy-submenu-id",
-                                     "s",
-                                     &submenu_id);
-
-    if (!g_strcmp0 (action_name, submenu_id)) {
-      g_menu_remove (menu, i);
-      return TRUE;
-    }
-
-    section = g_menu_model_get_item_link (G_MENU_MODEL (menu),
-                                          i,
-                                          G_MENU_LINK_SECTION);
-
-    if (!G_IS_MENU (section))
-      continue;
-
-    if (remove_menu_item (G_MENU (section), action_name))
-      return TRUE;
-  }
-
-  return FALSE;
-}
-
 static void
 ephy_header_bar_constructed (GObject *object)
 {
   EphyHeaderBar *header_bar = EPHY_HEADER_BAR (object);
-  GtkWidget *button;
   GtkWidget *event_box;
-  GtkBuilder *builder;
   EphyEmbedShell *embed_shell;
   GtkSizeGroup *downloads_size_group;
-  GMenu *menu;
 
   G_OBJECT_CLASS (ephy_header_bar_parent_class)->constructed (object);
 
@@ -266,62 +210,10 @@ ephy_header_bar_constructed (GObject *object)
                            GTK_WIDGET (header_bar->restore_button));
 
   /* Page Menu */
-  button = gtk_menu_button_new ();
-  header_bar->page_menu_button = button;
-  gtk_menu_button_set_icon_name (GTK_MENU_BUTTON (button), "open-menu-symbolic");
-  gtk_widget_set_tooltip_text (button, _("Main Menu"));
-  builder = gtk_builder_new_from_resource ("/org/gnome/epiphany/gtk/page-menu-popover.ui");
-  menu = G_MENU (gtk_builder_get_object (builder, "menu"));
-  header_bar->page_menu_popover = GTK_WIDGET (gtk_builder_get_object (builder, "page-menu-popover"));
-  header_bar->zoom_level_label = GTK_WIDGET (gtk_builder_get_object (builder, "zoom-level"));
+  header_bar->page_menu_button = GTK_WIDGET (ephy_page_menu_button_new ());
+  ephy_page_menu_button_show_combined_stop_reload_button (EPHY_PAGE_MENU_BUTTON (header_bar->page_menu_button), FALSE);
 
-  if (ephy_embed_shell_get_mode (embed_shell) == EPHY_EMBED_SHELL_MODE_APPLICATION) {
-    remove_menu_item (menu, "app.new-incognito");
-    remove_menu_item (menu, "app.reopen-closed-tab");
-    remove_menu_item (menu, "win.save-as-application");
-    remove_menu_item (menu, "win.open-application-manager");
-    remove_menu_item (menu, "win.encoding");
-    remove_menu_item (menu, "app.shortcuts");
-    remove_menu_item (menu, "app.help");
-    remove_menu_item (menu, "app.firefox-sync-dialog");
-    remove_menu_item (menu, "import-export");
-    remove_menu_item (menu, "webapps");
-  } else if (ephy_is_running_inside_sandbox ()) {
-    remove_menu_item (menu, "app.run-in-background");
-    remove_menu_item (menu, "app.quit");
-
-    if (is_desktop_pantheon ())
-      remove_menu_item (menu, "app.help");
-  } else {
-    remove_menu_item (menu, "app.run-in-background");
-    remove_menu_item (menu, "app.quit");
-  }
-
-  if (!ephy_can_install_web_apps ()) {
-    remove_menu_item (menu, "win.save-as-application");
-    remove_menu_item (menu, "win.open-application-manager");
-  }
-
-  header_bar->combined_stop_reload_button = GTK_WIDGET (gtk_builder_get_object (builder, "combined_stop_reload_button"));
-  gtk_widget_set_tooltip_text (header_bar->combined_stop_reload_button, _(REFRESH_BUTTON_TOOLTIP));
-
-  if (is_desktop_pantheon ()) {
-    GtkWidget *button_box;
-
-    remove_menu_item (menu, "app.about");
-
-    gtk_menu_button_set_icon_name (GTK_MENU_BUTTON (button), "open-menu");
-    gtk_widget_add_css_class (button, "toolbar-button");
-
-    button_box = GTK_WIDGET (gtk_builder_get_object (builder, "button-box"));
-    gtk_widget_add_css_class (button_box, "linked");
-    gtk_box_set_spacing (GTK_BOX (button_box), 0);
-  }
-
-  gtk_menu_button_set_popover (GTK_MENU_BUTTON (button), header_bar->page_menu_popover);
-  g_object_unref (builder);
-
-  adw_header_bar_pack_end (ADW_HEADER_BAR (header_bar->header_bar), button);
+  adw_header_bar_pack_end (ADW_HEADER_BAR (header_bar->header_bar), header_bar->page_menu_button);
 
   /* End action elements */
   header_bar->action_bar_end = ephy_action_bar_end_new ();
@@ -427,13 +319,15 @@ ephy_header_bar_set_adaptive_mode (EphyHeaderBar    *header_bar,
     case EPHY_ADAPTIVE_MODE_NORMAL:
       gtk_widget_set_visible (GTK_WIDGET (header_bar->action_bar_start), TRUE);
       gtk_widget_set_visible (GTK_WIDGET (header_bar->action_bar_end), TRUE);
-      gtk_widget_set_visible (header_bar->combined_stop_reload_button, FALSE);
+      gtk_widget_set_visible (header_bar->page_menu_button, TRUE);
+      adw_header_bar_set_show_end_title_buttons (ADW_HEADER_BAR (header_bar->header_bar), TRUE);
 
       break;
     case EPHY_ADAPTIVE_MODE_NARROW:
       gtk_widget_set_visible (GTK_WIDGET (header_bar->action_bar_start), FALSE);
       gtk_widget_set_visible (GTK_WIDGET (header_bar->action_bar_end), FALSE);
-      gtk_widget_set_visible (header_bar->combined_stop_reload_button, TRUE);
+      gtk_widget_set_visible (header_bar->page_menu_button, FALSE);
+      adw_header_bar_set_show_end_title_buttons (ADW_HEADER_BAR (header_bar->header_bar), FALSE);
 
       break;
   }
@@ -446,18 +340,7 @@ void
 ephy_header_bar_start_change_combined_stop_reload_state (EphyHeaderBar *header_bar,
                                                          gboolean       loading)
 {
-  if (loading) {
-    gtk_button_set_icon_name (GTK_BUTTON (header_bar->combined_stop_reload_button),
-                              "process-stop-symbolic");
-    /* Translators: tooltip for the stop button */
-    gtk_widget_set_tooltip_text (header_bar->combined_stop_reload_button,
-                                 _("Stop loading the current page"));
-  } else {
-    gtk_button_set_icon_name (GTK_BUTTON (header_bar->combined_stop_reload_button),
-                              "view-refresh-symbolic");
-    gtk_widget_set_tooltip_text (header_bar->combined_stop_reload_button,
-                                 _(REFRESH_BUTTON_TOOLTIP));
-  }
+  ephy_page_menu_button_change_combined_stop_reload_state (EPHY_PAGE_MENU_BUTTON (header_bar->page_menu_button), loading);
 }
 
 void
@@ -466,5 +349,5 @@ ephy_header_bar_set_zoom_level (EphyHeaderBar *header_bar,
 {
   g_autofree gchar *zoom_level = g_strdup_printf ("%2.0f%%", zoom * 100);
 
-  gtk_label_set_label (GTK_LABEL (header_bar->zoom_level_label), zoom_level);
+  ephy_page_menu_button_set_zoom_level (EPHY_PAGE_MENU_BUTTON (header_bar->page_menu_button), zoom_level);
 }

src/ephy-window.c
@@ -799,6 +799,8 @@ change_combined_stop_reload_state (GSimpleAction *action,
                                                            g_variant_get_boolean (loading));
   ephy_header_bar_start_change_combined_stop_reload_state (header_bar,
                                                            g_variant_get_boolean (loading));
+  ephy_action_bar_change_combined_stop_reload_state (EPHY_ACTION_BAR (window->action_bar),
+                                                     g_variant_get_boolean (loading));
 
   g_simple_action_set_state (action, loading);
 }
@@ -1099,6 +1101,7 @@ sync_tab_zoom (WebKitWebView *web_view,
   zoom = webkit_web_view_get_zoom_level (web_view);
 
   ephy_header_bar_set_zoom_level (EPHY_HEADER_BAR (window->header_bar), zoom);
+  ephy_action_bar_set_zoom_level (EPHY_ACTION_BAR (window->action_bar), zoom);
 
   if (zoom >= ZOOM_MAXIMAL)
     can_zoom_in = FALSE;

src/meson.build
@@ -41,6 +41,7 @@ libephymain_sources = [
   'ephy-lockdown.c',
   'ephy-mouse-gesture-controller.c',
   'ephy-permission-popover.c',
+  'ephy-page-menu-button.c',
   'ephy-password-popover.c',
   'ephy-passwords-view.c',
   'ephy-privacy-report.c',
@@ -94,7 +95,7 @@ blueprints = custom_target('blueprints',
     'resources/gtk/lang-row.blp',
     'resources/gtk/location-entry.blp',
     'resources/gtk/notebook-context-menu.blp',
-    'resources/gtk/page-menu-popover.blp',
+    'resources/gtk/page-menu-button.blp',
     'resources/gtk/password-popover.blp',
     'resources/gtk/passwords-view.blp',
     'resources/gtk/permission-popover.blp',

commit 5df27894446288faa23cb68d05dc3bf2c03d5df6

Fix crash when opening Find bar
The find bar dismisses the page menu popover, but this is broken because
the return value of ephy_header_bar_get_page_menu_button() has changed
from GtkMenuButton to EphyPageMenuButton.

This fixes a regression from 3d290ef92d4fcc2901caa5eeb9d7d9ea32f6062b
(!1823).

Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1842>

src/ephy-page-menu-button.c
@@ -190,3 +190,15 @@ ephy_page_menu_button_show_combined_stop_reload_button (EphyPageMenuButton *self
 {
   gtk_widget_set_visible (self->combined_stop_reload_button, show);
 }
+
+void
+ephy_page_menu_button_popup (EphyPageMenuButton *self)
+{
+  gtk_menu_button_popup (GTK_MENU_BUTTON (self->menu_button));
+}
+
+void
+ephy_page_menu_button_popdown (EphyPageMenuButton *self)
+{
+  gtk_menu_button_popdown (GTK_MENU_BUTTON (self->menu_button));
+}

src/ephy-page-menu-button.h
@@ -40,4 +40,7 @@ void                  ephy_page_menu_button_change_combined_stop_reload_state (E
 void                  ephy_page_menu_button_show_combined_stop_reload_button (EphyPageMenuButton *self,
                                                                               gboolean            show);
 
+void                  ephy_page_menu_button_popup          (EphyPageMenuButton *self);
+void                  ephy_page_menu_button_popdown        (EphyPageMenuButton *self);
+
 G_END_DECLS

src/window-commands.c
@@ -46,6 +46,7 @@
 #include "ephy-history-dialog.h"
 #include "ephy-link.h"
 #include "ephy-location-entry.h"
+#include "ephy-page-menu-button.h"
 #include "ephy-password-export.h"
 #include "ephy-password-import.h"
 #include "ephy-passwords-view.h"
@@ -1546,11 +1547,11 @@ window_cmd_page_menu (GSimpleAction *action,
 {
   EphyWindow *window = EPHY_WINDOW (user_data);
   EphyHeaderBar *header_bar;
-  GtkMenuButton *button;
+  EphyPageMenuButton *button;
 
   header_bar = EPHY_HEADER_BAR (ephy_window_get_header_bar (window));
-  button = GTK_MENU_BUTTON (ephy_header_bar_get_page_menu_button (header_bar));
-  gtk_menu_button_popup (button);
+  button = EPHY_PAGE_MENU_BUTTON (ephy_header_bar_get_page_menu_button (header_bar));
+  ephy_page_menu_button_popup (button);
 }
 
 void
@@ -2770,11 +2771,11 @@ static void
 dismiss_page_popover (EphyWindow *window)
 {
   EphyHeaderBar *header_bar;
-  GtkMenuButton *button;
+  EphyPageMenuButton *button;
 
   header_bar = EPHY_HEADER_BAR (ephy_window_get_header_bar (window));
-  button = GTK_MENU_BUTTON (ephy_header_bar_get_page_menu_button (header_bar));
-  gtk_menu_button_popdown (button);
+  button = EPHY_PAGE_MENU_BUTTON (ephy_header_bar_get_page_menu_button (header_bar));
+  ephy_page_menu_button_popdown (button);
 }
 
 void