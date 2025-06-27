GNOME/epiphany

commit 2566164ebc25e10f33ac8bf48806fc65d6f9c67b

Allow to set reader mode zoom level
Fixes: https://gitlab.gnome.org/GNOME/epiphany/-/issues/2492
Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1799>

embed/ephy-reader-handler.c
@@ -256,6 +256,7 @@ ephy_reader_request_begin_get_source_from_web_view (EphyReaderRequest *request,
   }
 
   script = (const char *)g_bytes_get_data (bytes, &length);
+  request->web_view = web_view;
   webkit_web_view_evaluate_javascript (web_view, script, length, NULL,
                                        "resource:///org/gnome/epiphany/readability/Readability.js",
                                        request->cancellable,

data/org.gnome.epiphany.gschema.xml
@@ -233,6 +233,11 @@
 			<default>1.0</default>
 			<summary>Default zoom level for new pages</summary>
 		</key>
+		<key type="d" name="reader-mode-zoom-level">
+			<default>1.0</default>
+			<summary>Default reader mode zoom level.</summary>
+			<description>Sets the zoom level for reader mode pages.</description>
+		</key>
 		<key type="b" name="enable-autosearch">
 			<default>true</default>
 			<summary>Enable autosearch</summary>

embed/ephy-web-view.c
@@ -1532,6 +1532,11 @@ load_changed_cb (WebKitWebView   *web_view,
 
       if (view->entering_reader_mode) {
         view->entering_reader_mode = FALSE;
+
+        view->is_setting_zoom = TRUE;
+        webkit_web_view_set_zoom_level (WEBKIT_WEB_VIEW (view), g_settings_get_double (EPHY_SETTINGS_WEB, EPHY_PREFS_WEB_READER_MODE_ZOOM_LEVEL));
+        view->is_setting_zoom = FALSE;
+
         g_object_notify_by_pspec (G_OBJECT (web_view), obj_properties[PROP_ENTERING_READER_MODE]);
       }
 
@@ -2276,6 +2281,11 @@ zoom_changed_cb (WebKitWebView *web_view,
 
   if (EPHY_WEB_VIEW (web_view)->is_setting_zoom)
     return;
+
+  if (g_str_has_prefix (ephy_web_view_get_display_address (EPHY_WEB_VIEW (web_view)), "ephy-reader:")) {
+    g_settings_set_double (EPHY_SETTINGS_WEB, EPHY_PREFS_WEB_READER_MODE_ZOOM_LEVEL, zoom);
+    return;
+  }
 
   address = ephy_web_view_get_address (EPHY_WEB_VIEW (web_view));
   if (ephy_embed_utils_address_has_web_scheme (address)) {

lib/ephy-prefs.h
@@ -114,6 +114,7 @@ static const char * const ephy_prefs_state_schema[] = {
 #define EPHY_PREFS_WEB_ENABLE_NAVIGATION_GESTURES   "enable-navigation-gestures"
 #define EPHY_PREFS_WEB_AUTOFILL_DATA                "autofill-data"
 #define EPHY_PREFS_WEB_AUTO_OPEN_SCHEMES            "auto-open-schemes"
+#define EPHY_PREFS_WEB_READER_MODE_ZOOM_LEVEL       "reader-mode-zoom-level"
 
 static const char * const ephy_prefs_web_schema[] = {
   EPHY_PREFS_WEB_FONT_MIN_SIZE,
@@ -145,6 +146,7 @@ static const char * const ephy_prefs_web_schema[] = {
   EPHY_PREFS_WEB_SHOW_DEVELOPER_ACTIONS,
   EPHY_PREFS_WEB_ALWAYS_SHOW_FULL_URL,
   EPHY_PREFS_WEB_AUTOFILL_DATA,
+  EPHY_PREFS_WEB_READER_MODE_ZOOM_LEVEL,
 };
 
 #define EPHY_PREFS_SCHEMA                             "org.gnome.Epiphany"

commit e0c0e00cc50a2b731a9ee365d961eee78ef80560

reader-handler: fix crash when destroying web view
2566164ebc25e10f33ac8bf48806fc65d6f9c67b introduced a refcounting error
by assigning to request->web_view, which is owned, without calling
g_object_ref(). We could just ref it, but this looks to be unneeded.
It's surely a leftover from an earlier iteration of the merge request.

Fixes: https://bugs.webkit.org/show_bug.cgi?id=294692
Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1901>

embed/ephy-reader-handler.c
@@ -256,7 +256,6 @@ ephy_reader_request_begin_get_source_from_web_view (EphyReaderRequest *request,
   }
 
   script = (const char *)g_bytes_get_data (bytes, &length);
-  request->web_view = web_view;
   webkit_web_view_evaluate_javascript (web_view, script, length, NULL,
                                        "resource:///org/gnome/epiphany/readability/Readability.js",
                                        request->cancellable,