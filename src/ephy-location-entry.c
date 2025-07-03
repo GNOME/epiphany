GNOME/epiphany

commit c4fb95328aa338b40455a7bcfc832b5e60b6cac5

Keep track of focus and position of location entry
Fixes: https://gitlab.gnome.org/GNOME/epiphany/-/issues/2639
Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1820>

src/ephy-location-entry.c
@@ -401,8 +401,17 @@ on_focus_enter (GtkEventControllerFocus *controller,
     EphyEmbed *active_embed = ephy_window_get_active_embed (window);
     const char *typed_input = ephy_embed_get_typed_input (active_embed);
 
-    if (typed_input)
+    if (typed_input) {
       ephy_location_entry_title_widget_set_address (EPHY_TITLE_WIDGET (self), typed_input);
+    } else {
+      EphyWebView *web_view = ephy_embed_get_web_view (active_embed);
+      const char *text = ephy_web_view_get_address (web_view);
+
+      if (!ephy_embed_utils_is_no_show_address (text))
+        gtk_editable_set_text (GTK_EDITABLE (self->text), ephy_web_view_get_address (web_view));
+      else
+        gtk_editable_set_text (GTK_EDITABLE (self->text), "");
+    }
   }
 }
 
@@ -1319,6 +1328,9 @@ ephy_location_entry_title_widget_set_address (EphyTitleWidget *widget,
   const char *text = "";
   const char *final_text;
 
+  if (ephy_location_entry_has_focus (self))
+    return;
+
   if (address != NULL) {
     if (g_str_has_prefix (address, EPHY_ABOUT_SCHEME))
       effective_text = g_strdup_printf ("about:%s", address + strlen (EPHY_ABOUT_SCHEME) + 1);
@@ -1717,6 +1729,7 @@ ephy_location_entry_page_action_clear (EphyLocationEntry *self)
 void
 ephy_location_entry_grab_focus_without_selecting (EphyLocationEntry *self)
 {
+  gtk_stack_set_visible_child_name (GTK_STACK (self->stack), "edit");
   gtk_entry_grab_focus_without_selecting (GTK_ENTRY (self->text));
 }
 
@@ -1793,4 +1806,19 @@ GtkWidget *
 ephy_location_entry_get_opensearch_button (EphyLocationEntry *entry)
 {
   return entry->opensearch_button;
+}
+
+gboolean
+ephy_location_entry_has_focus (EphyLocationEntry *self)
+{
+  GtkWidget *delegate = GTK_WIDGET (gtk_editable_get_delegate (GTK_EDITABLE (self->text)));
+
+  return gtk_widget_has_focus (delegate);
+}
+
+void
+ephy_location_entry_set_position (EphyLocationEntry *self,
+                                  int                position)
+{
+  gtk_editable_set_position (GTK_EDITABLE (self->text), position);
 }

embed/ephy-web-view.c
@@ -132,6 +132,8 @@ struct _EphyWebView {
   guint unresponsive_process_timeout_id;
 
   guint64 uid;
+  int location_entry_position;
+  gboolean location_entry_has_focus;
 
   EphyClientCertificateManager *client_certificate_manager;
 
@@ -4426,4 +4428,30 @@ ephy_web_view_get_web_app_manifest_url_finish (EphyWebView   *view,
   g_assert (g_task_is_valid (result, view));
 
   return g_task_propagate_pointer (G_TASK (result), error);
+}
+
+void
+ephy_web_view_set_location_entry_position (EphyWebView *self,
+                                           int          position)
+{
+  self->location_entry_position = position;
+}
+
+int
+ephy_web_view_get_location_entry_position (EphyWebView *self)
+{
+  return self->location_entry_position;
+}
+
+void
+ephy_web_view_set_location_entry_has_focus (EphyWebView *self,
+                                            gboolean     focus)
+{
+  self->location_entry_has_focus = focus;
+}
+
+gboolean
+ephy_web_view_get_location_entry_has_focus (EphyWebView *self)
+{
+  return self->location_entry_has_focus;
 }

embed/ephy-web-view.h
@@ -208,4 +208,14 @@ void ephy_web_view_autofill_disable_popup                         (EphyWebView  
 
 GListModel *               ephy_web_view_get_opensearch_engines   (EphyWebView               *view);
 
+void ephy_web_view_set_location_entry_position                    (EphyWebView *self,
+                                                                   int          position);
+
+int ephy_web_view_get_location_entry_position                     (EphyWebView *self);
+
+void ephy_web_view_set_location_entry_has_focus                   (EphyWebView *self,
+                                                                   gboolean     focus);
+
+gboolean ephy_web_view_get_location_entry_has_focus               (EphyWebView *self);
+
 G_END_DECLS

src/ephy-location-entry.h
@@ -106,4 +106,9 @@ void            ephy_location_entry_start_change_combined_stop_reload_state (Eph
                                                                              gboolean           loading);
 
 GtkWidget      *ephy_location_entry_get_opensearch_button      (EphyLocationEntry *entry);
+
+gboolean        ephy_location_entry_has_focus                  (EphyLocationEntry *self);
+
+void            ephy_location_entry_set_position               (EphyLocationEntry *self,
+                                                                int                position);
 G_END_DECLS

src/ephy-window.c
@@ -2649,6 +2649,15 @@ ephy_window_connect_active_embed (EphyWindow *window)
     ephy_location_entry_set_reader_mode_state (EPHY_LOCATION_ENTRY (title_widget), ephy_web_view_get_reader_mode_state (view));
   }
 
+  if (ephy_web_view_get_location_entry_has_focus (view)) {
+    /* Grab view to not interfere with location entry signals clearing position */
+    gtk_widget_grab_focus (GTK_WIDGET (view));
+    ephy_location_entry_grab_focus_without_selecting (EPHY_LOCATION_ENTRY (title_widget));
+    ephy_location_entry_set_position (EPHY_LOCATION_ENTRY (title_widget), ephy_web_view_get_location_entry_position (view));
+  } else {
+    gtk_widget_grab_focus (GTK_WIDGET (view));
+  }
+
   sync_tab_security (view, NULL, window);
   sync_tab_document_type (view, NULL, window);
   sync_tab_load_status (view, WEBKIT_LOAD_STARTED, window);
@@ -2732,12 +2741,20 @@ ephy_window_disconnect_active_embed (EphyWindow *window)
   EphyEmbed *embed;
   WebKitWebView *web_view;
   EphyWebView *view;
+  EphyTitleWidget *title_widget;
 
   g_assert (window->active_embed != NULL);
 
   embed = window->active_embed;
   web_view = EPHY_GET_WEBKIT_WEB_VIEW_FROM_EMBED (embed);
   view = EPHY_WEB_VIEW (web_view);
+
+  title_widget = ephy_header_bar_get_title_widget (EPHY_HEADER_BAR (window->header_bar));
+
+  if (EPHY_IS_LOCATION_ENTRY (title_widget)) {
+    ephy_web_view_set_location_entry_has_focus (view, ephy_location_entry_has_focus (EPHY_LOCATION_ENTRY (title_widget)));
+    ephy_web_view_set_location_entry_position (view, gtk_editable_get_position (GTK_EDITABLE (title_widget)));
+  }
 
   ephy_embed_detach_notification_container (window->active_embed);
 

commit 063bd6e2dba651e22ba05254b3f90df16494a9fc

location-entry: fix reset function
c4fb95328aa338b40455a7bcfc832b5e60b6cac5 broke
ephy_location_entry_reset() when the location entry is focused. This
needs to always reset, so let's bypass the focus check.

Fixes #2720

src/ephy-location-entry.c
@@ -1333,15 +1333,16 @@ ephy_location_entry_title_widget_get_address (EphyTitleWidget *widget)
 }
 
 static void
-ephy_location_entry_title_widget_set_address (EphyTitleWidget *widget,
-                                              const char      *address)
+ephy_location_entry_do_set_address (EphyTitleWidget *widget,
+                                    const char      *address,
+                                    gboolean         force)
 {
   EphyLocationEntry *self = EPHY_LOCATION_ENTRY (widget);
   g_autofree char *effective_text = NULL;
   const char *text = "";
   const char *final_text;
 
-  if (ephy_location_entry_has_focus (self))
+  if (!force && ephy_location_entry_has_focus (self))
     return;
 
   if (address != NULL) {
@@ -1356,6 +1357,13 @@ ephy_location_entry_title_widget_set_address (EphyTitleWidget *widget,
   update_url_button_style (self);
 
   gtk_popover_popdown (GTK_POPOVER (self->suggestions_popover));
+}
+
+static void
+ephy_location_entry_title_widget_set_address (EphyTitleWidget *widget,
+                                              const char      *address)
+{
+  ephy_location_entry_do_set_address (widget, address, FALSE);
 }
 
 static EphySecurityLevel
@@ -1493,7 +1501,7 @@ ephy_location_entry_reset (EphyLocationEntry *self)
 
   offset = strlen (text) - strlen (old_text);
   position = gtk_editable_get_position (GTK_EDITABLE (self));
-  ephy_title_widget_set_address (EPHY_TITLE_WIDGET (self), text);
+  ephy_location_entry_do_set_address (EPHY_TITLE_WIDGET (self), text, TRUE);
   gtk_editable_set_position (GTK_EDITABLE (self), position + offset);
 
   return g_strcmp0 (text, old_text);