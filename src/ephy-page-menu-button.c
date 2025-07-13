c8186b10c9493809f109525ae91b46f933bb5411
ARAKHNID <arakhnid@protonmail.com>
2025-07-13 18:26:08 -0500

    Hide run in background web app setting if unsandboxed
    The setting is broken outside of Flatpak due to no sandboxing. This just
    hides the option along with the explicit quit in that scenario.
    
    Closes #2724
    
    Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1926>

--- src/ephy-page-menu-button.c
+++ src/ephy-page-menu-button.c
@@ -115,7 +115,10 @@ ephy_page_menu_button_init (EphyPageMenuButton *self)
     remove_menu_item (self->page_menu, "app.firefox-sync-dialog");
     remove_menu_item (self->page_menu, "import-export");
     remove_menu_item (self->page_menu, "webapps");
-  } else {
+  }
+
+  if (ephy_embed_shell_get_mode (embed_shell) != EPHY_EMBED_SHELL_MODE_APPLICATION
+      || !ephy_is_running_inside_sandbox ()) {
     remove_menu_item (self->page_menu, "app.run-in-background");
     remove_menu_item (self->page_menu, "app.quit");
   }
