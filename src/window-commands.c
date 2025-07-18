5f3f3f6a9a4a089065f9b64e81c92a2a565f8237
Michael Catanzaro <mcatanzaro@redhat.com>
2025-07-18 16:54:57 -0500

    window-commands: fix crash when taking screenshot
    This is a regression from f87d5f9eee23b8b5e56b5390ff2ba34e207a93c0. I
    changed save_dialog_cb() to receive an async data struct as user data
    instead of an EphyEmbed, but turns out this callback is used in two
    places and I only updated one of the two call sites. Oops.
    
    Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/1927>

--- src/window-commands.c
+++ src/window-commands.c
@@ -2593,7 +2593,7 @@ window_cmd_screenshot (GSimpleAction *action,
                         GTK_WINDOW (window),
                         NULL,
                         (GAsyncReadyCallback)save_dialog_cb,
-                        embed);
+                        save_dialog_async_data_new (embed, suggested_filename));
 }
 
 void
