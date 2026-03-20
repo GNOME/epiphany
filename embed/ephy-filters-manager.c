GNOME/epiphany

commit 7362c2abc20004ff58b88dab7b6d9bd3a971d909

Use an own user content manager for each webview

embed/ephy-filters-manager.c
@@ -44,6 +44,7 @@ struct _EphyFiltersManager {
   gint64 update_time;
   guint update_timeout_id;
   GCancellable *cancellable;
+  WebKitUserContentFilter *wk_filter;
   WebKitUserContentFilterStore *store;
 };
 
@@ -471,7 +472,6 @@ filter_saved_cb (WebKitUserContentFilterStore *store,
                  FilterInfo                   *self)
 {
   g_autoptr (GError) error = NULL;
-  g_autoptr (WebKitUserContentFilter) wk_filter = NULL;
 
   if (!self->manager)
     return;
@@ -481,12 +481,13 @@ filter_saved_cb (WebKitUserContentFilterStore *store,
   g_assert (self);
   g_assert (self->manager->store == store);
 
-  wk_filter = webkit_user_content_filter_store_save_finish (self->manager->store,
-                                                            result,
-                                                            &error);
-  if (wk_filter) {
+  g_clear_object (&self->manager->wk_filter);
+  self->manager->wk_filter = webkit_user_content_filter_store_save_finish (self->manager->store,
+                                                                           result,
+                                                                           &error);
+  if (self->manager->wk_filter) {
     LOG ("Filter %s compiled successfully.", filter_info_get_identifier (self));
-    filter_info_setup_enable_compiled_filter (self, wk_filter);
+    filter_info_setup_enable_compiled_filter (self, self->manager->wk_filter);
     filter_info_save_sidecar (self,
                               self->manager->cancellable,
                               (GAsyncReadyCallback)sidecar_saved_cb,
@@ -623,7 +624,6 @@ filter_load_cb (WebKitUserContentFilterStore *store,
                 FilterInfo                   *self)
 {
   g_autoptr (GError) error = NULL;
-  g_autoptr (WebKitUserContentFilter) wk_filter = NULL;
   g_autoptr (GFile) source_file = NULL;
   g_autoptr (GFile) json_file = NULL;
   g_autofree char *json_file_uri = NULL;
@@ -637,14 +637,15 @@ filter_load_cb (WebKitUserContentFilterStore *store,
   g_assert (self);
   g_assert (store == self->manager->store);
 
-  wk_filter = webkit_user_content_filter_store_load_finish (self->manager->store,
-                                                            result,
-                                                            &error);
-  self->found = (wk_filter != NULL);
+  g_clear_object (&self->manager->wk_filter);
+  self->manager->wk_filter = webkit_user_content_filter_store_load_finish (self->manager->store,
+                                                                           result,
+                                                                           &error);
+  self->found = (self->manager->wk_filter != NULL);
 
-  if (wk_filter) {
+  if (self->manager->wk_filter) {
     LOG ("Found compiled filter %s.", filter_info_get_identifier (self));
-    filter_info_setup_enable_compiled_filter (self, wk_filter);
+    filter_info_setup_enable_compiled_filter (self, self->manager->wk_filter);
     LOG ("Update %sneeded for filter %s (last %" PRIu64 "s ago, interval %us)",
          filter_info_needs_updating_from_source (self) ? "" : "not ",
          filter_info_get_identifier (self),
@@ -916,6 +917,7 @@ ephy_filters_manager_dispose (GObject *object)
     g_cancellable_cancel (manager->cancellable);
     g_clear_object (&manager->cancellable);
   }
+  g_clear_object (&manager->wk_filter);
   g_clear_object (&manager->store);
 
   G_OBJECT_CLASS (ephy_filters_manager_parent_class)->dispose (object);
@@ -1092,4 +1094,18 @@ ephy_filters_manager_get_is_initialized (EphyFiltersManager *manager)
 {
   g_return_val_if_fail (EPHY_IS_FILTERS_MANAGER (manager), FALSE);
   return manager->is_initialized;
+}
+
+void
+ephy_filters_manager_set_ucm_forbids_ads (EphyFiltersManager       *manager,
+                                          WebKitUserContentManager *ucm,
+                                          gboolean                  forbids_ads)
+{
+  if (!manager->wk_filter)
+    return;
+
+  if (forbids_ads)
+    webkit_user_content_manager_add_filter (ucm, manager->wk_filter);
+  else
+    webkit_user_content_manager_remove_filter (ucm, manager->wk_filter);
 }

embed/ephy-embed-prefs.c
@@ -42,6 +42,23 @@ typedef struct {
 /* FIXME: Refactor this code to remove the need of those globals */
 static WebKitSettings *webkit_settings = NULL;
 static GFileMonitor *user_style_sheet_monitor = NULL;
+static WebKitUserStyleSheet *style_sheet = NULL;
+static GList *ucm_list = NULL;
+
+static void
+update_user_style_on_all_ucm (void)
+{
+  GList *list = NULL;
+
+  for (list = ucm_list; list != NULL; list = list->next) {
+    WebKitUserContentManager *ucm = list->data;
+
+    if (style_sheet)
+      webkit_user_content_manager_add_style_sheet (ucm, style_sheet);
+    else
+      webkit_user_content_manager_remove_all_style_sheets (ucm);
+  }
+}
 
 static void
 user_style_sheet_output_stream_splice_cb (GOutputStream *output_stream,
@@ -52,14 +69,13 @@ user_style_sheet_output_stream_splice_cb (GOutputStream *output_stream,
 
   bytes = g_output_stream_splice_finish (output_stream, result, NULL);
   if (bytes > 0) {
-    WebKitUserStyleSheet *style_sheet;
+    g_clear_pointer (&style_sheet, webkit_user_style_sheet_unref);
 
     style_sheet = webkit_user_style_sheet_new (g_memory_output_stream_get_data (G_MEMORY_OUTPUT_STREAM (output_stream)),
                                                WEBKIT_USER_CONTENT_INJECT_ALL_FRAMES, WEBKIT_USER_STYLE_LEVEL_USER,
                                                NULL, NULL);
-    webkit_user_content_manager_add_style_sheet (WEBKIT_USER_CONTENT_MANAGER (ephy_embed_shell_get_user_content_manager (ephy_embed_shell_get_default ())),
-                                                 style_sheet);
-    webkit_user_style_sheet_unref (style_sheet);
+
+    update_user_style_on_all_ucm ();
   }
 }
 
@@ -95,8 +111,6 @@ user_style_sheet_file_changed (GFileMonitor      *monitor,
                                gpointer           user_data)
 {
   if (event_type == G_FILE_MONITOR_EVENT_CHANGES_DONE_HINT) {
-    webkit_user_content_manager_remove_all_style_sheets (WEBKIT_USER_CONTENT_MANAGER (ephy_embed_shell_get_user_content_manager (ephy_embed_shell_get_default ())));
-
     g_file_read_async (file, G_PRIORITY_DEFAULT, NULL,
                        (GAsyncReadyCallback)user_style_sheet_read_cb, NULL);
   }
@@ -113,7 +127,9 @@ webkit_pref_callback_user_stylesheet (GSettings  *settings,
 
   if (!value) {
     g_clear_object (&user_style_sheet_monitor);
-    webkit_user_content_manager_remove_all_style_sheets (WEBKIT_USER_CONTENT_MANAGER (ephy_embed_shell_get_user_content_manager (ephy_embed_shell_get_default ())));
+    g_clear_pointer (&style_sheet, webkit_user_style_sheet_unref);
+
+    update_user_style_on_all_ucm ();
   } else {
     GFile *file;
     GError *error = NULL;
@@ -550,4 +566,23 @@ ephy_embed_prefs_get_settings (void)
   static GOnce once_init = G_ONCE_INIT;
 
   return g_once (&once_init, ephy_embed_prefs_init, NULL);
+}
+
+void
+ephy_embed_prefs_apply_user_style (WebKitUserContentManager *ucm)
+{
+  if (style_sheet)
+    webkit_user_content_manager_add_style_sheet (ucm, style_sheet);
+}
+
+void
+ephy_embed_prefs_register_ucm (WebKitUserContentManager *ucm)
+{
+  ucm_list = g_list_append (ucm_list, ucm);
+}
+
+void
+ephy_embed_prefs_unregister_ucm (WebKitUserContentManager *ucm)
+{
+  ucm_list = g_list_remove (ucm_list, ucm);
 }

embed/ephy-embed-prefs.h
@@ -30,8 +30,13 @@
 
 G_BEGIN_DECLS
 
-WebKitSettings *ephy_embed_prefs_get_settings (void);
-void ephy_embed_prefs_set_cookie_accept_policy          (WebKitCookieManager *cookie_manager,
-                                                         const char          *settings_policy);
+WebKitSettings *ephy_embed_prefs_get_settings  (void);
+void ephy_embed_prefs_set_cookie_accept_policy (WebKitCookieManager      *cookie_manager,
+                                                const char               *settings_policy);
+void ephy_embed_prefs_apply_user_style         (WebKitUserContentManager *ucm);
+
+void ephy_embed_prefs_register_ucm             (WebKitUserContentManager *ucm);
+void ephy_embed_prefs_unregister_ucm           (WebKitUserContentManager *ucm);
+
 
 G_END_DECLS

commit ae8f28b2cdd10647b64530c8bc707cded15e3e42

filters-manager: store and apply a list of filters
The filters manager has been pretty broken since
7362c2abc20004ff58b88dab7b6d9bd3a971d909, which stored a single
WebKitUserContentFilter* wk_filter in the filters manager. But the
filters manager is supposed to process a list of many filters, not
just one. Currently the filter that gets loaded last wins, and all
others get dropped and ignored. Oops. Instead, let's store a list
of all filters, and use them all.

Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/2064>

embed/ephy-filters-manager.c
@@ -41,11 +41,11 @@ struct _EphyFiltersManager {
   gboolean is_initialized;
 
   char *filters_dir;
-  GHashTable *filter_infos;  /* (identifier, FilterInfo) */
+  GHashTable *filter_infos;  /* (unowned char *identifier, owned FilterInfo) */
+  GList *filters;            /* (owned WebKitUserContentFilter *) */
   gint64 update_time;
   guint update_timeout_id;
   GCancellable *cancellable;
-  WebKitUserContentFilter *wk_filter;
   WebKitUserContentFilterStore *store;
   gboolean metered;
 };
@@ -463,6 +463,7 @@ filter_saved_cb (WebKitUserContentFilterStore *store,
                  FilterInfo                   *self)
 {
   g_autoptr (GError) error = NULL;
+  g_autoptr (WebKitUserContentFilter) filter = NULL;
 
   if (!self->manager)
     return;
@@ -472,16 +473,16 @@ filter_saved_cb (WebKitUserContentFilterStore *store,
   g_assert (self);
   g_assert (self->manager->store == store);
 
-  g_clear_pointer (&self->manager->wk_filter, webkit_user_content_filter_unref);
-  self->manager->wk_filter = webkit_user_content_filter_store_save_finish (self->manager->store,
-                                                                           result,
-                                                                           &error);
-  if (self->manager->wk_filter) {
+  filter = webkit_user_content_filter_store_save_finish (self->manager->store,
+                                                         result,
+                                                         &error);
+  if (filter) {
     LOG ("Filter %s compiled successfully.", filter_info_get_identifier (self));
     filter_info_save_sidecar (self,
                               self->manager->cancellable,
                               (GAsyncReadyCallback)sidecar_saved_cb,
                               self);
+    self->manager->filters = g_list_append (self->manager->filters, g_steal_pointer (&filter));
   } else if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED)) {
     g_warning ("Filter %s <%s> cannot be compiled: %s.",
                filter_info_get_identifier (self), self->source_uri,
@@ -671,6 +672,7 @@ filter_load_cb (WebKitUserContentFilterStore *store,
   g_autoptr (GError) error = NULL;
   g_autoptr (GFile) source_file = NULL;
   g_autoptr (GFile) json_file = NULL;
+  g_autoptr (WebKitUserContentFilter) filter = NULL;
   EphyDownload *download;
 
   if (!self->manager)
@@ -681,19 +683,19 @@ filter_load_cb (WebKitUserContentFilterStore *store,
   g_assert (self);
   g_assert (store == self->manager->store);
 
-  g_clear_pointer (&self->manager->wk_filter, webkit_user_content_filter_unref);
-  self->manager->wk_filter = webkit_user_content_filter_store_load_finish (self->manager->store,
-                                                                           result,
-                                                                           &error);
-  self->found = !!self->manager->wk_filter;
+  filter = webkit_user_content_filter_store_load_finish (self->manager->store,
+                                                         result,
+                                                         &error);
+  self->found = !!filter;
 
-  if (self->manager->wk_filter) {
+  if (filter) {
     LOG ("Found compiled filter %s.", filter_info_get_identifier (self));
     LOG ("Update %sneeded for filter %s (last %" PRIu64 "s ago, interval %us)",
          filter_info_needs_updating_from_source (self) ? "" : "not ",
          filter_info_get_identifier (self),
          (self->manager->update_time - self->last_update),
          self->manager->metered ? ADBLOCK_FILTER_UPDATE_FREQUENCY_METERED : ADBLOCK_FILTER_UPDATE_FREQUENCY);
+    self->manager->filters = g_list_append (self->manager->filters, g_steal_pointer (&filter));
   } else if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED)) {
     filter_info_setup_done (self);
     return;
@@ -973,8 +975,9 @@ ephy_filters_manager_dispose (GObject *object)
     g_cancellable_cancel (manager->cancellable);
     g_clear_object (&manager->cancellable);
   }
-  g_clear_pointer (&manager->wk_filter, webkit_user_content_filter_unref);
   g_clear_object (&manager->store);
+
+  g_clear_list (&manager->filters, (GDestroyNotify)webkit_user_content_filter_unref);
 
   G_OBJECT_CLASS (ephy_filters_manager_parent_class)->dispose (object);
 }
@@ -1142,16 +1145,27 @@ ephy_filters_manager_get_is_initialized (EphyFiltersManager *manager)
   return manager->is_initialized;
 }
 
+static void
+add_filter (WebKitUserContentFilter  *filter,
+            WebKitUserContentManager *ucm)
+{
+  webkit_user_content_manager_add_filter (ucm, filter);
+}
+
+static void
+remove_filter (WebKitUserContentFilter  *filter,
+               WebKitUserContentManager *ucm)
+{
+  webkit_user_content_manager_remove_filter (ucm, filter);
+}
+
 void
 ephy_filters_manager_set_ucm_forbids_ads (EphyFiltersManager       *manager,
                                           WebKitUserContentManager *ucm,
                                           gboolean                  forbids_ads)
 {
-  if (!manager->wk_filter)
-    return;
-
   if (forbids_ads)
-    webkit_user_content_manager_add_filter (ucm, manager->wk_filter);
+    g_list_foreach (manager->filters, (GFunc)add_filter, ucm);
   else
-    webkit_user_content_manager_remove_filter (ucm, manager->wk_filter);
+    g_list_foreach (manager->filters, (GFunc)remove_filter, ucm);
 }