f91593a15acb9f8914984396bc84aed781546fdb
Jan-Michael Brummer <jan.brummer@tabos.org>
2026-08-05 21:19:39 +0200

    Linter: Apply property_enum_convention

--- lib/ephy-notification.c
+++ lib/ephy-notification.c
@@ -38,8 +38,10 @@ struct _EphyNotification {
 
 typedef enum {
   PROP_HEAD = 1,
-  PROP_BODY
+  PROP_BODY,
 } EphyNotificationProps;
+
+static GParamSpec *props[PROP_BODY + 1] = { NULL, };
 
 enum {
   CLOSE,
@@ -160,18 +162,16 @@ ephy_notification_class_init (EphyNotificationClass *klass)
   object_class->set_property = ephy_notification_set_property;
   object_class->get_property = ephy_notification_get_property;
 
-  g_object_class_install_property (object_class,
-                                   PROP_HEAD,
-                                   g_param_spec_string ("head",
-                                                        NULL, NULL,
-                                                        "",
-                                                        G_PARAM_STATIC_STRINGS | G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE));
-  g_object_class_install_property (object_class,
-                                   PROP_BODY,
-                                   g_param_spec_string ("body",
-                                                        NULL, NULL,
-                                                        "",
-                                                        G_PARAM_STATIC_STRINGS | G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE));
+  props[PROP_HEAD] = g_param_spec_string ("head",
+                                          NULL, NULL,
+                                          "",
+                                          G_PARAM_STATIC_STRINGS | G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
+  props[PROP_BODY] = g_param_spec_string ("body",
+                                          NULL, NULL,
+                                          "",
+                                          G_PARAM_STATIC_STRINGS | G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
+
+  g_object_class_install_properties (object_class, G_N_ELEMENTS (props), props);
 
   signals[CLOSE] =
     g_signal_new ("close",
--- src/ephy-window.c
+++ src/ephy-window.c
@@ -217,6 +217,8 @@ typedef enum {
   PROP_ADAPTIVE_MODE,
 } EphyWindowProps;
 
+static GParamSpec *props[PROP_ADAPTIVE_MODE + 1] = { NULL, };
+
 /* Make sure not to overlap with those in ephy-lockdown.c */
 enum {
   SENS_FLAG_CHROME        = 1 << 0,
@@ -4720,15 +4722,15 @@ ephy_window_class_init (EphyWindowClass *klass)
                                                        G_PARAM_READWRITE |
                                                        G_PARAM_STATIC_STRINGS));
 
-  g_object_class_install_property (object_class,
-                                   PROP_ADAPTIVE_MODE,
-                                   g_param_spec_enum ("adaptive-mode",
-                                                      NULL,
-                                                      NULL,
-                                                      EPHY_TYPE_ADAPTIVE_MODE,
-                                                      EPHY_ADAPTIVE_MODE_NORMAL,
-                                                      G_PARAM_READWRITE |
-                                                      G_PARAM_STATIC_STRINGS));
+  props[PROP_ADAPTIVE_MODE] = g_param_spec_enum ("adaptive-mode",
+                                                 NULL,
+                                                 NULL,
+                                                 EPHY_TYPE_ADAPTIVE_MODE,
+                                                 EPHY_ADAPTIVE_MODE_NORMAL,
+                                                 G_PARAM_READWRITE |
+                                                 G_PARAM_STATIC_STRINGS);
+
+  g_object_class_install_properties (object_class, G_N_ELEMENTS (props), props);
 
   shell = ephy_shell_get_default ();
   manager = ephy_embed_shell_get_downloads_manager (EPHY_EMBED_SHELL (shell));
