GNOME/epiphany

commit 2bd9720f21c8c7d9493846d92ae85f09f554d64a

Port async operations to GTask
Refactor EphyHistoryService async functions to use standard GTask-based
async and finish signatures, replacing the custom EphyHistoryJobCallback

Part-of: <https://gitlab.gnome.org/GNOME/epiphany/-/merge_requests/2116>

embed/ephy-web-view.c
@@ -474,14 +474,21 @@ take_snapshot (EphyWebView *view)
 
 static void
 history_service_query_urls_cb (EphyHistoryService *service,
-                               gboolean            success,
-                               GList              *urls,
+                               GAsyncResult       *result,
                                EphyWebView        *view)
 {
   const char *url = webkit_web_view_get_uri (WEBKIT_WEB_VIEW (view));
+  g_autoptr (GError) error = NULL;
+  g_autolist (EphyHistoryURL) urls = NULL;
 
-  if (!success)
+  urls = ephy_history_service_query_urls_finish (service, result, &error);
+  if (error) {
+    if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+      return;
+
+    g_warning ("Failed to query overview URLs: %s", error->message);
     goto out;
+  }
 
   /* Have we already started a new load? */
   if (g_strcmp0 (url, view->pending_snapshot_uri) != 0)
@@ -499,7 +506,6 @@ history_service_query_urls_cb (EphyHistoryService *service,
 
 out:
   g_clear_pointer (&view->pending_snapshot_uri, g_free);
-  g_object_unref (view);
 }
 
 static gboolean
@@ -523,9 +529,9 @@ maybe_take_snapshot (EphyWebView *view)
    */
   query = ephy_history_query_new_for_overview ();
   query->limit += 5;
-  ephy_history_service_query_urls (service, query, NULL,
-                                   (EphyHistoryJobCallback)history_service_query_urls_cb,
-                                   g_object_ref (view));
+  ephy_history_service_query_urls (service, query, view->cancellable,
+                                   (GAsyncReadyCallback)history_service_query_urls_cb,
+                                   view);
   ephy_history_query_free (query);
 
   return G_SOURCE_REMOVE;
@@ -1013,21 +1019,24 @@ out:
 }
 
 static void
-get_host_for_url_cb (gpointer service,
-                     gboolean success,
-                     gpointer result_data,
-                     gpointer user_data)
+get_host_for_url_cb (EphyHistoryService *service,
+                     GAsyncResult       *result,
+                     EphyWebView        *view)
 {
-  EphyHistoryHost *host;
-  EphyWebView *view;
+  g_autoptr (EphyHistoryHost) host = NULL;
+  g_autoptr (GError) error = NULL;
   double current_zoom;
   double set_zoom;
 
-  if (!success)
+  host = ephy_history_service_get_host_for_url_finish (service, result, &error);
+  if (error) {
+    if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+      g_warning ("Failed to get host zoom level: %s", error->message);
     return;
+  }
 
-  view = EPHY_WEB_VIEW (user_data);
-  host = (EphyHistoryHost *)result_data;
+  if (!host)
+    return;
 
   current_zoom = webkit_web_view_get_zoom_level (WEBKIT_WEB_VIEW (view));
 
@@ -1055,7 +1064,8 @@ restore_zoom_level (EphyWebView *view,
   if (ephy_embed_utils_address_has_web_scheme (address))
     ephy_history_service_get_host_for_url (view->history_service,
                                            address, view->cancellable,
-                                           (EphyHistoryJobCallback)get_host_for_url_cb, view);
+                                           (GAsyncReadyCallback)get_host_for_url_cb,
+                                           view);
 }
 
 static void

embed/ephy-about-handler.c
@@ -408,17 +408,26 @@ ephy_about_handler_handle_applications (EphyAboutHandler       *handler,
 
 static void
 history_service_query_urls_cb (EphyHistoryService     *history,
-                               gboolean                success,
-                               GList                  *urls,
-                               WebKitURISchemeRequest *request)
+                               GAsyncResult           *result,
+                               WebKitURISchemeRequest *request_param)
 {
+  g_autoptr (WebKitURISchemeRequest) request = request_param;
   EphySnapshotService *snapshot_service;
   EphyEmbedShell *shell;
   GString *data_str;
   gsize data_length;
   char *lang;
   GList *l;
+  g_autolist (EphyHistoryURL) urls = NULL;
+  g_autoptr (GError) error = NULL;
   guint list_length;
+
+  urls = ephy_history_service_query_urls_finish (history, result, &error);
+  if (error) {
+    if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+      g_warning ("Failed to query overview URLs: %s", error->message);
+    return;
+  }
 
   snapshot_service = ephy_snapshot_service_get_default ();
   shell = ephy_embed_shell_get_default ();
@@ -445,7 +454,7 @@ history_service_query_urls_cb (EphyHistoryService     *history,
 
   list_length = g_list_length (urls);
 
-  if (list_length == 0 || !success) {
+  if (list_length == 0) {
     GtkIconTheme *icon_theme;
     g_autoptr (GtkIconPaintable) paintable = NULL;
     g_autofree char *path = NULL;
@@ -517,15 +526,14 @@ history_service_query_urls_cb (EphyHistoryService     *history,
                             entity_encoded_title);
   }
 
-  data_str = g_string_append (data_str,
-                              "  </div>\n"
-                              "  </div>\n"
-                              "</body></html>\n");
+  g_string_append (data_str,
+                   "  </div>\n"
+                   "  </div>\n"
+                   "</body></html>\n");
 
 out:
   data_length = data_str->len;
   ephy_about_handler_finish_request (request, g_string_free_and_steal (data_str), data_length);
-  g_object_unref (request);
 }
 
 static gboolean
@@ -568,7 +576,7 @@ ephy_about_handler_handle_html_overview (EphyAboutHandler       *handler,
   history = ephy_embed_shell_get_global_history_service (ephy_embed_shell_get_default ());
   query = ephy_history_query_new_for_overview ();
   ephy_history_service_query_urls (history, query, NULL,
-                                   (EphyHistoryJobCallback)history_service_query_urls_cb,
+                                   (GAsyncReadyCallback)history_service_query_urls_cb,
                                    g_object_ref (request));
   ephy_history_query_free (query);
 

embed/ephy-embed-shell.c
@@ -227,16 +227,21 @@ web_process_extension_password_form_focused_message_received_cb (WebKitUserConte
 
 static void
 history_service_query_urls_cb (EphyHistoryService *service,
-                               gboolean            success,
-                               GList              *urls,
+                               GAsyncResult       *result,
                                EphyEmbedShell     *shell)
 {
   EphyEmbedShellPrivate *priv = ephy_embed_shell_get_instance_private (shell);
+  g_autolist (EphyHistoryURL) urls = NULL;
+  g_autoptr (GError) error = NULL;
   GList *l;
   GVariantBuilder builder;
 
-  if (!success)
+  urls = ephy_history_service_query_urls_finish (service, result, &error);
+  if (error) {
+    if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+      g_warning ("Failed to query overview URLs: %s", error->message);
     return;
+  }
 
   g_variant_builder_init (&builder, G_VARIANT_TYPE ("a(ssb)"));
   for (l = urls; l; l = g_list_next (l)) {
@@ -258,8 +263,8 @@ ephy_embed_shell_update_overview_urls (EphyEmbedShell *shell)
   g_autoptr (EphyHistoryQuery) query = NULL;
 
   query = ephy_history_query_new_for_overview ();
-  ephy_history_service_query_urls (priv->global_history_service, query, NULL,
-                                   (EphyHistoryJobCallback)history_service_query_urls_cb,
+  ephy_history_service_query_urls (priv->global_history_service, query, priv->cancellable,
+                                   (GAsyncReadyCallback)history_service_query_urls_cb,
                                    shell);
 }
 
@@ -272,12 +277,16 @@ history_service_urls_visited_cb (EphyHistoryService *history,
 
 static void
 history_set_url_hidden_cb (EphyHistoryService *service,
-                           gboolean            success,
-                           gpointer            result_data,
+                           GAsyncResult       *result,
                            EphyEmbedShell     *shell)
 {
-  if (!success)
+  g_autoptr (GError) error = NULL;
+
+  if (!ephy_history_service_set_url_hidden_finish (service, result, &error)) {
+    if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+      g_warning ("Failed to hide URL: %s", error->message);
     return;
+  }
 
   ephy_embed_shell_update_overview_urls (shell);
 }
@@ -294,8 +303,8 @@ web_process_extension_overview_message_received_cb (WebKitUserContentManager *ma
   url_to_remove = jsc_value_to_string (message);
 
   ephy_history_service_set_url_hidden (priv->global_history_service,
-                                       url_to_remove, TRUE, NULL,
-                                       (EphyHistoryJobCallback)history_set_url_hidden_cb,
+                                       url_to_remove, TRUE, priv->cancellable,
+                                       (GAsyncReadyCallback)history_set_url_hidden_cb,
                                        shell);
 
   window = gtk_application_get_active_window (GTK_APPLICATION (shell));
@@ -353,12 +362,16 @@ web_process_extension_autofill_askuser_received_cb (WebKitUserContentManager *ma
 
 static void
 history_set_url_pinned_cb (EphyHistoryService *service,
-                           gboolean            success,
-                           gpointer            result_data,
+                           GAsyncResult       *result,
                            EphyEmbedShell     *shell)
 {
-  if (!success)
+  g_autoptr (GError) error = NULL;
+
+  if (!ephy_history_service_set_url_pinned_finish (service, result, &error)) {
+    if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+      g_warning ("Failed to pin/unpin URL: %s", error->message);
     return;
+  }
 
   ephy_embed_shell_update_overview_urls (shell);
 }
@@ -374,8 +387,8 @@ web_process_extension_overview_pin_message_received_cb (WebKitUserContentManager
 
   if (url) {
     ephy_history_service_set_url_pinned (priv->global_history_service,
-                                         url, pinned, NULL,
-                                         (EphyHistoryJobCallback)history_set_url_pinned_cb,
+                                         url, pinned, priv->cancellable,
+                                         (GAsyncReadyCallback)history_set_url_pinned_cb,
                                          shell);
   }
 }

lib/sync/ephy-history-manager.c
@@ -475,17 +475,26 @@ ephy_history_manager_handle_regular_merge (EphyHistoryManager *self,
 }
 
 static void
-merge_history_cb (EphyHistoryService    *service,
-                  gboolean               success,
-                  GList                 *urls,
-                  MergeHistoryAsyncData *data)
+merge_history_cb (EphyHistoryService *service,
+                  GAsyncResult       *result,
+                  gpointer            user_data)
 {
+  MergeHistoryAsyncData *data = user_data;
   GHashTable *records_ht_id = NULL;
   GHashTable *records_ht_url = NULL;
   GPtrArray *to_upload = NULL;
+  g_autolist (EphyHistoryURL) urls = NULL;
+  g_autoptr (GError) error = NULL;
 
-  if (!success) {
-    g_warning ("Failed to retrieve URLs in history");
+  urls = ephy_history_service_find_urls_finish (service, result, &error);
+  if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED)) {
+    data->callback (NULL, data->user_data);
+    merge_history_async_data_free (data);
+    return;
+  }
+
+  if (error) {
+    g_warning ("Failed to retrieve URLs in history: %s", error->message);
     goto out;
   }
 
@@ -539,7 +548,7 @@ synchronizable_manager_merge (EphySynchronizableManager              *manager,
 
   ephy_history_service_find_urls (self->service, -1, -1, -1, 0, NULL,
                                   EPHY_HISTORY_SORT_MOST_RECENTLY_VISITED, NULL,
-                                  (EphyHistoryJobCallback)merge_history_cb,
+                                  (GAsyncReadyCallback)merge_history_cb,
                                   merge_history_async_data_new (self,
                                                                 is_initial,
                                                                 remotes_deleted,

src/ephy-history-dialog.c
@@ -287,19 +287,24 @@ on_select_all_button_clicked (GtkButton         *button,
 }
 
 static void
-on_find_urls_cb (gpointer service,
-                 gboolean success,
-                 gpointer result_data,
-                 gpointer user_data)
+on_find_urls_cb (EphyHistoryService *service,
+                 GAsyncResult       *result,
+                 gpointer            user_data)
 {
   EphyHistoryDialog *self = EPHY_HISTORY_DIALOG (user_data);
+  g_autoptr (GError) error = NULL;
+  GList *urls;
 
-  if (!success)
+  urls = ephy_history_service_find_urls_finish (service, result, &error);
+  if (error) {
+    if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+      g_warning ("Failed to find URLs: %s", error->message);
     return;
+  }
 
   if (self->urls)
     ephy_history_url_list_free (self->urls);
-  self->urls = ephy_history_url_list_copy (result_data);
+  self->urls = urls;
 
   gtk_list_box_remove_all (GTK_LIST_BOX (self->listbox));
 
@@ -359,7 +364,7 @@ filter_now (EphyHistoryDialog *self)
                                   substrings,
                                   type,
                                   self->cancellable,
-                                  (EphyHistoryJobCallback)on_find_urls_cb, self);
+                                  (GAsyncReadyCallback)on_find_urls_cb, self);
 }
 
 static GList *
@@ -381,12 +386,20 @@ get_checked_rows (EphyHistoryDialog *self)
 }
 
 static void
-on_browse_history_deleted_cb (gpointer service,
-                              gboolean success,
-                              gpointer result_data,
-                              gpointer user_data)
+on_browse_history_deleted_cb (EphyHistoryService *service,
+                              GAsyncResult       *result,
+                              gpointer            user_data)
 {
   EphyHistoryDialog *self = EPHY_HISTORY_DIALOG (user_data);
+  g_autoptr (GError) error = NULL;
+  gboolean success;
+
+  success = ephy_history_service_delete_urls_finish (service, result, &error);
+  if (error) {
+    if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+      g_warning ("Failed to delete URLs: %s", error->message);
+    return;
+  }
 
   if (success) {
     g_autoptr (GList) checked_rows = get_checked_rows (self);
@@ -418,7 +431,7 @@ delete_checked_rows (EphyHistoryDialog *self)
   }
 
   ephy_history_service_delete_urls (self->history_service, deleted_urls, self->cancellable,
-                                    (EphyHistoryJobCallback)on_browse_history_deleted_cb, self);
+                                    (GAsyncReadyCallback)on_browse_history_deleted_cb, self);
 
   for (iter = deleted_urls; iter; iter = g_list_next (iter))
     ephy_snapshot_service_delete_snapshot_for_url (self->snapshot_service, ((EphyHistoryURL *)iter->data)->url);
@@ -635,7 +648,7 @@ confirmation_dialog_response_cb (EphyHistoryDialog *self)
     }
 
     ephy_history_service_delete_urls (self->history_service, deleted_urls, self->cancellable,
-                                      (EphyHistoryJobCallback)on_browse_history_deleted_cb, self);
+                                      (GAsyncReadyCallback)on_browse_history_deleted_cb, self);
 
     for (iter = deleted_urls; iter; iter = g_list_next (iter))
       ephy_snapshot_service_delete_snapshot_for_url (self->snapshot_service, ((EphyHistoryURL *)iter->data)->url);

src/ephy-suggestion-model.c
@@ -610,18 +610,25 @@ bookmarks_query (EphySuggestionModel *self,
 
 static void
 history_query_completed_cb (EphyHistoryService *service,
-                            gboolean            success,
-                            gpointer            result_data,
+                            GAsyncResult       *result,
                             gpointer            user_data)
 {
   GTask *task = user_data;
   EphySuggestionModel *self;
   QueryData *data;
-  GList *urls;
+  g_autolist (EphyHistoryURL) urls = NULL;
+  g_autoptr (GError) error = NULL;
 
   self = g_task_get_source_object (task);
   data = g_task_get_task_data (task);
-  urls = (GList *)result_data;
+
+  urls = ephy_history_service_find_urls_finish (service, result, &error);
+  if (error) {
+    if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+      g_warning ("Failed to query history suggestions: %s", error->message);
+    query_collection_done (self, g_steal_pointer (&task));
+    return;
+  }
 
   if (strlen (data->query) > 0) {
     for (const GList *p = urls; p; p = p->next) {
@@ -757,7 +764,7 @@ ephy_suggestion_model_query_async (EphySuggestionModel *self,
                                     qlist,
                                     EPHY_HISTORY_SORT_MOST_VISITED,
                                     cancellable,
-                                    (EphyHistoryJobCallback)history_query_completed_cb,
+                                    (GAsyncReadyCallback)history_query_completed_cb,
                                     task);
   }
 

commit bc57d40be41bb0799457531e9ec04f2d07179fda

web-view: fix use after free in query URLs callback
It's not allowed to use the web view before first checking to see
whether the async call was canceled, since if the call has been
canceled, the source object has been disposed. This was a regression
from 2bd9720f21c8c7d9493846d92ae85f09f554d64a.

embed/ephy-web-view.c
@@ -477,7 +477,7 @@ history_service_query_urls_cb (EphyHistoryService *service,
                                GAsyncResult       *result,
                                EphyWebView        *view)
 {
-  const char *url = webkit_web_view_get_uri (WEBKIT_WEB_VIEW (view));
+  const char *url;
   g_autoptr (GError) error = NULL;
   g_autolist (EphyHistoryURL) urls = NULL;
 
@@ -489,6 +489,8 @@ history_service_query_urls_cb (EphyHistoryService *service,
     g_warning ("Failed to query overview URLs: %s", error->message);
     goto out;
   }
+
+  url = webkit_web_view_get_uri (WEBKIT_WEB_VIEW (view));
 
   /* Have we already started a new load? */
   if (g_strcmp0 (url, view->pending_snapshot_uri) != 0)