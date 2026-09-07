09ea3f6234d59502840fa221c17af79ccab70908
Michael Catanzaro <mcatanzaro@gnome.org>
2026-09-07 14:12:37 -0500

    Revert "shell: Open links in new tab in incognito window when "Start in Incognito Mode" is enabled"
    This reverts commit 24a56a9da0f7e4a931db44c40c8eebc4ca8bcb97

--- src/ephy-shell.c
+++ src/ephy-shell.c
@@ -949,8 +949,7 @@ static void
 ephy_shell_constructed (GObject *object)
 {
   if (ephy_embed_shell_get_mode (EPHY_EMBED_SHELL (object)) != EPHY_EMBED_SHELL_MODE_BROWSER &&
-      ephy_embed_shell_get_mode (EPHY_EMBED_SHELL (object)) != EPHY_EMBED_SHELL_MODE_APPLICATION &&
-      ephy_embed_shell_get_mode (EPHY_EMBED_SHELL (object)) != EPHY_EMBED_SHELL_MODE_INCOGNITO) {
+      ephy_embed_shell_get_mode (EPHY_EMBED_SHELL (object)) != EPHY_EMBED_SHELL_MODE_APPLICATION) {
     GApplicationFlags flags;
 
     flags = g_application_get_flags (G_APPLICATION (object));
